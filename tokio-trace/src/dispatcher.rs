dff3af4310b93e3a8c24f349454cdfc084804bba
Eliza Weisman <eliza@buoyant.io>
2018-10-05 15:53:18 -0700

    Remove global dispatcher (#40)
    Fixes #30. Fixes #29.
    
    Currently, the `tokio_trace` crate provides a global dispatcher through
    which all trace events are set. This was based primarily on the design
    of the `log` crate. 
    
    As described in #30, this is not an ideal design. Instead, there should
    be no global subscriber state. The Tokio runtime will handle setting up
    the subscriber at the root of the task. When operating outside of a
    Tokio runtime, the user will be responsible for setting up the necessary
    subscriber. There should be a way to get a handle to the current
    subscriber and probably easy ways to get into new execution while
    carrying over the subscriber.
    
    This branch removes the global dispatcher, and replaces it with an
    explicit contextual dispatcher. In doing so, it also removes the fanout
    functionality from the dispatcher. We will be able to implement fanout
    subscribers in other crates, using the new API.
    
    Also, this branch changes span enter and exit behavior, so that when a
    span is entered or exited, that event is broadcast to the subscriber
    _that provided that span with its ID_, rather than to the _current_
    subscriber. The need for this change was described in
    https://github.com/hawkw/tokio-trace-prototype/pull/37#commitcomment-30777184
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace/src/dispatcher.rs
+++ tokio-trace/src/dispatcher.rs
@@ -1,111 +1,87 @@
 use {span, subscriber::Subscriber, Event, SpanData, Meta};
 
-use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::{
+    cell::RefCell,
+    collections::{
+        HashSet,
+        hash_map::DefaultHasher,
+    },
+    hash::{Hash, Hasher},
+    fmt,
+    sync::Arc,
+};
 
-static STATE: AtomicUsize = ATOMIC_USIZE_INIT;
-
-// There are three different states that we care about: the logger's
-// uninitialized, the logger's initializing (set_logger's been called but
-// LOGGER hasn't actually been set yet), or the logger's active.
-const UNINITIALIZED: usize = 0;
-const INITIALIZING: usize = 1;
-const INITIALIZED: usize = 2;
-static mut DISPATCHER: &'static Subscriber = &NoDispatcher;
-
-#[derive(Default)]
-pub struct Builder {
-    subscribers: Vec<Box<dyn Subscriber>>,
+thread_local! {
+    static CURRENT_DISPATCH: RefCell<Current> = RefCell::new(Dispatch::none().with_invalidate());
 }
 
-impl Builder {
-    pub fn new() -> Self {
-        Self::default()
+struct Current {
+    dispatch: Dispatch,
+    seen: HashSet<u64>,
+}
+
+#[derive(Clone)]
+pub struct Dispatch(Arc<dyn Subscriber + Send + Sync>);
+
+impl Dispatch {
+    pub fn none() -> Self {
+        Dispatch(Arc::new(NoSubscriber))
     }
 
-    pub fn add_subscriber<T: Subscriber + 'static>(mut self, subscriber: T) -> Self {
-        self.subscribers.push(Box::new(subscriber));
-        self
+    pub fn current() -> Dispatch {
+        CURRENT_DISPATCH.with(|current| {
+            current.borrow().dispatch.clone()
+        })
     }
 
-    pub fn try_init(self) -> Result<(), InitError> {
-        unsafe {
-            match STATE.compare_and_swap(UNINITIALIZED, INITIALIZING, Ordering::SeqCst) {
-                UNINITIALIZED => {
-                    DISPATCHER = &*Box::into_raw(Box::new(self));
-                    STATE.store(INITIALIZED, Ordering::SeqCst);
-                    Ok(())
-                }
-                INITIALIZING => {
-                    while STATE.load(Ordering::SeqCst) == INITIALIZING {}
-                    Err(InitError)
-                }
-                _ => Err(InitError),
+    pub fn to<S>(subscriber: S) -> Self
+    // TODO: Add some kind of `UnsyncDispatch`?
+    where
+        S: Subscriber + Send + Sync + 'static,
+    {
+        Dispatch(Arc::new(subscriber))
+    }
+
+    pub fn with<T>(&self, f: impl FnOnce() -> T) -> T {
+        CURRENT_DISPATCH.with(|current| {
+            let prior = current.replace(self.with_invalidate()).dispatch;
+            let result = f();
+            *current.borrow_mut() = prior.with_invalidate();
+            result
+        })
+    }
+
+    fn with_invalidate(&self) -> Current {
+        Current {
+            dispatch: self.clone(),
+            seen: HashSet::new(),
+        }
+    }
+}
+
+impl fmt::Debug for Dispatch {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.pad("Dispatch(...)")
+    }
+}
+
+impl Subscriber for Dispatch {
+    fn should_invalidate_filter(&self, metadata: &Meta) -> bool {
+        CURRENT_DISPATCH.with(|current| {
+            let mut hasher = DefaultHasher::new();
+            metadata.hash(&mut hasher);
+            let hash = hasher.finish();
+
+            if current.borrow().seen.contains(&hash) {
+                self.0.should_invalidate_filter(metadata)
+            } else {
+                current.borrow_mut().seen.insert(hash);
+                true
             }
-        }
+        })
+
     }
 
-    pub fn init(self) {
-        self.try_init().unwrap()
-    }
-}
-
-impl Subscriber for Builder {
-    fn enabled(&self, metadata: &Meta) -> bool {
-        self.subscribers.iter()
-            .any(|subscriber| subscriber.enabled(metadata))
-    }
-
-    fn observe_event<'event, 'meta: 'event>(&self, event: &'event Event<'event, 'meta>) {
-        for subscriber in &self.subscribers {
-            subscriber.observe_event(event)
-        }
-    }
-
-    fn new_span(&self, new_span: &span::NewSpan) -> span::Id {
-        // TODO: this shouldn't let the first attached subscriber always be in
-        // control of the span ID, but the dispatcher type is going away soon
-        // anyway, so for now, it just needs to compile.
-        let id = self.subscribers.get(0)
-            .map(|subscriber| subscriber.new_span(&new_span))
-            .unwrap_or_else(|| span::Id::from_u64(0));
-        // Show the new span to all the attached subscribers anyway, so they can
-        // register its creation.
-        // TODO: this means their IDs will silently be ignored. Figure out a
-        // better way to handle this!
-        for subscriber in &self.subscribers[1..] {
-            subscriber.new_span(&new_span);
-        }
-        id
-    }
-
-    #[inline]
-    fn enter(&self, span: &SpanData) {
-        for subscriber in &self.subscribers {
-            subscriber.enter(span)
-        }
-    }
-
-    #[inline]
-    fn exit(&self, span: &SpanData) {
-        for subscriber in &self.subscribers {
-            subscriber.exit(span)
-        }
-    }
-}
-
-pub struct Dispatcher(&'static Subscriber);
-
-impl Dispatcher {
-    pub fn current() -> Dispatcher {
-        Dispatcher(unsafe { DISPATCHER })
-    }
-
-    pub fn builder() -> Builder {
-        Builder::new()
-    }
-}
-
-impl Subscriber for Dispatcher {
     #[inline]
     fn enabled(&self, metadata: &Meta) -> bool {
         self.0.enabled(metadata)
@@ -132,12 +108,9 @@ impl Subscriber for Dispatcher {
     }
 }
 
-struct NoDispatcher;
+struct NoSubscriber;
 
-#[derive(Debug)]
-pub struct InitError;
-
-impl Subscriber for NoDispatcher {
+impl Subscriber for NoSubscriber {
     fn enabled(&self, _metadata: &Meta) -> bool {
         false
     }
