b8f67af2f6b4e5b03f9f0af420f7fefbda365eb5
Eliza Weisman <eliza@buoyant.io>
2018-10-10 16:12:15 -0700

    Instrument future executors as well as futures (#43)
    This branch adds functionality to the `tokio-trace-futures`
    compatibility crate for instrumenting future _executors_ as well as
    futures. Instrumenting a future with a span works slightly differently
    than instrumenting a future --- rather than taking a span which is
    entered by all futures spawned on the executor, it takes a _function_
    returning a span, so that all top-level futures spawned are inside of
    their own unique span. This seemed like the correct behaviour for
    executors.
    
    Possibly more valuable is the ability to attach a `Subscriber` to an
    executor, so that whenever futures spawned on that executor are polled,
    the subscriber context switches to the attached subscriber.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ tokio-trace-futures/src/executor.rs
@@ -0,0 +1,65 @@
+use futures::{future::{Executor, ExecuteError}, Future};
+use tokio_trace::Span;
+use ::{Instrumented, Instrument, WithDispatch};
+
+pub trait InstrumentExecutor<F>
+where
+    Self: Executor<Instrumented<F>>,
+    F: Future<Item = (), Error = ()>,
+{
+    fn instrument<G>(self, mk_span: G) -> InstrumentedExecutor<Self, G>
+    where
+        G: Fn() -> Span,
+        Self: Sized,
+    {
+        InstrumentedExecutor {
+            inner: self,
+            mk_span,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct InstrumentedExecutor<T, G> {
+    inner: T,
+    mk_span: G,
+}
+
+impl<T, F> InstrumentExecutor<F> for T
+where
+    T: Executor<Instrumented<F>>,
+    F: Future<Item = (), Error = ()>,
+{ }
+
+macro_rules! deinstrument_err {
+    ($e:expr) => {
+        $e.map_err(|e| {
+            let kind = e.kind();
+            let future = e.into_future().inner;
+            ExecuteError::new(kind, future)
+        })
+    }
+}
+
+impl<T, F, N> Executor<F> for InstrumentedExecutor<T, N>
+where
+    T: Executor<Instrumented<F>>,
+    F: Future<Item = (), Error = ()>,
+    N: Fn() -> Span,
+{
+    fn execute(&self, future: F) -> Result<(), ExecuteError<F>> {
+        let future = future.instrument((self.mk_span)());
+        deinstrument_err!(self.inner.execute(future))
+    }
+}
+
+impl<T, F> Executor<F> for WithDispatch<T>
+where
+    T: Executor<WithDispatch<F>>,
+    F: Future<Item = (), Error = ()>,
+{
+    fn execute(&self, future: F) -> Result<(), ExecuteError<F>> {
+        let future = self.with_dispatch(future);
+        deinstrument_err!(self.inner.execute(future))
+    }
+}
