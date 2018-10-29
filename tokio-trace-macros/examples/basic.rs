ce6b7c308453c2bf08bba53171a625489c262366
David Barsky <me@davidbarsky.com>
2018-10-29 16:29:17 -0400

    First pass at #[trace] macro. (#16)
    First pass at `#[trace]` proc macro.

---
+++ tokio-trace-macros/examples/basic.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate tokio_trace;
+#[macro_use]
+extern crate tokio_trace_macros;
+extern crate env_logger;
+extern crate tokio_trace_log;
+
+use tokio_trace::Level;
+
+fn main() {
+    env_logger::Builder::new().parse("trace").init();
+    let subscriber = tokio_trace_log::TraceLogger::new();
+
+    tokio_trace::Dispatch::to(subscriber).with(|| {
+        let num = 1;
+
+        let span = span!("Getting rec from another function.", number_of_recs = &num);
+        span.enter(|| {
+            let band = suggest_band();
+            event!(Level::Info, { band_recommendation = &band }, "Got a rec.");
+        });
+    });
+}
+
+#[trace]
+#[inline]
+fn suggest_band() -> String {
+    format!("Wild Pink")
+}
---
+++ tokio-trace-macros/src/lib.rs
@@ -0,0 +1,45 @@
+extern crate proc_macro;
+#[macro_use]
+extern crate syn;
+#[macro_use]
+extern crate quote;
+extern crate proc_macro2;
+
+use proc_macro::TokenStream;
+use proc_macro2::Span;
+use syn::token::{Async, Const, Unsafe};
+use syn::{Abi, Attribute, Block, Ident, ItemFn, Visibility};
+
+#[proc_macro_attribute]
+pub fn trace(_args: TokenStream, item: TokenStream) -> TokenStream {
+    let input: ItemFn = parse_macro_input!(item as ItemFn);
+    let call_site = Span::call_site();
+
+    // these are needed ahead of time, as ItemFn contains the function body _and_
+    // isn't representable inside a quote!/quote_spanned! macro
+    // (Syn's ToTokens isn't implemented for ItemFn)
+    let attrs: Vec<Attribute> = input.clone().attrs;
+    let vis: Visibility = input.clone().vis;
+    let constness: Option<Const> = input.clone().constness;
+    let unsafety: Option<Unsafe> = input.clone().unsafety;
+    let asyncness: Option<Async> = input.clone().asyncness;
+    let abi: Option<Abi> = input.clone().abi;
+
+    // function body
+    let block: Box<Block> = input.clone().block;
+    // function name
+    let ident: Ident = input.clone().ident;
+    let ident_str = ident.to_string();
+
+    let return_type = input.clone().decl.output;
+    let params = input.clone().decl.inputs;
+
+    quote_spanned!(call_site=>
+        #(#attrs) *
+        #vis #constness #unsafety #asyncness #abi fn #ident(#params) #return_type {
+            span!(#ident_str, traced_function = &#ident_str).enter(move || {
+                #block
+            })
+        }
+    ).into()
+}
