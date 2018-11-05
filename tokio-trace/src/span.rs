2771f0fd69f88dd7bd9188310f42bb696d8e1965
Eliza Weisman <eliza@buoyant.io>
2018-11-05 12:55:03 -0800

    Re-implement cloneable `Shared` spans on top of new Span API (#71)
    This branch re-implements the reimplements the previous (`Clone`-based)
    span API on top of the new `Span` API added in #70.
    
    The cloneable span handle is a new type, `span::Shared`, in the
    `tokio-trace` crate. The `Shared` span handle is constructed from a
    `core::Span` handle, so users who need to share multiple entering
    handles to a span, or enter a span from more than one thread, can now
    explicitly opt in to the allocation overhead.
    
    I've updated the documentation to describe the differences between the
    two types of span handles. Additionally, I've improved the interfaces
    for the `Enter` and `Entered` internal types that are used for 
    implementing both types of span handle, and added some documentation.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace/src/span.rs
+++ tokio-trace/src/span.rs
@@ -127,6 +127,61 @@
 //! a subscriber never observes an inconsistant state; namely, a span being
 //! entered after it has closed.
 //!
+//! ## Shared spans
+//!
+//! In general, it is rarely necessary to create multiple handles to a span that
+//! persist outside of the period during which that span is executing. Users who
+//! wish to have multiple handles capable of entering a span, or enter the same
+//! span from multiple threads, may wish to use the [shared span] type instead.
+//!
+//! A `Shared` handle behaves similarly to a `Span` handle, but may be cloned
+//! freely. Any `Shared` handle may be used to enter the span it corresponds to,
+//! and (since `Shared` handles are `Send + Sync`) they may be used to allow
+//! multiple threads to enter the same span.
+//!
+//! Shared handles may be created from a span using the [`IntoShared`] trait:
+//! ```
+//! # #[macro_use] extern crate tokio_trace;
+//! # fn main() {
+//! use tokio_trace::span::IntoShared;
+//! // Convert a regular `Span` handle into a cloneable `Shared` handle.
+//! let span = span!("foo").into_shared();
+//!
+//! // Entering a `Shared` span handle *consumes* the handle:
+//! span.clone().enter(|| {
+//!     // ...
+//! });
+//!
+//! // When all `Shared` handles to the span have been consumed, it will close.
+//! span.enter(|| {
+//!     // ...
+//! }) // --> Subscriber::close(span)
+//! # }
+//! ```
+//!
+//! Unlike `Span` handles, `Shared` spans are represented by `Arc` pointers, so
+//! constructing them will allocate memory, if the span is enabled.
+//!
+//! **Note**: When _not_ using shared spans, it is possible to cause a span to
+//! _never_ be dropped, by entering it, creating a second handle using
+//! `Span::current`, and returning that handle from the closure which executes
+//! inside the span, and then dropping that handle while the span is not
+//! currently executing. Since the span is not currently executing, it has no
+//! way to observe the second handle being dropped, and it cannot determine if
+//! it is safe to close.  However, if all such handles are dropped while the
+//! span is executing, the span will still be able to close normally. Spans will
+//! only  fail to close in situations where the second handle is dropped while
+//! the span is _not_ executing.
+//!
+//! This is possible because the logic for determining if a span can close is
+//! intentionally cautious. Spans will only close if they _know_ they cannot be
+//! entered; although a span failing to close does result in potentially
+//! incorrect trace data, this is less serious of a problem than the
+//! inconsistant state that arises when a span is closed and then re-entered.
+//! The latter violates the assumptions that a reasonable subscriber
+//! implementation could be expected to make.
+//!
+//!
 //! # Accessing a Span's Data
 //!
 //! The [`Data`] type represents a *non-entering* reference to a `Span`'s data
@@ -139,14 +194,142 @@
 //! [`Subscriber`]: ::Subscriber
 //! [`State`]: ::span::State
 //! [`Data`]: ::span::Data
-pub use tokio_trace_core::span::*;
-pub use tokio_trace_core::Span; // TODO: auto-close
-                                // use tokio_trace_core::span::Span as Inner;
+//! [shared span]: ::span::Shared
+//! [`IntoShared`]: ::span::IntoShared
+pub use tokio_trace_core::span::{AsId, Data, Id, Span};
 
-// #[derive(Clone, Debug)]
-// pub struct Span {
+#[cfg(any(test, feature = "test-support"))]
+pub use tokio_trace_core::span::{mock, MockSpan};
 
-// }
+use std::sync::Arc;
+use tokio_trace_core::span::Enter;
+use {subscriber, IntoValue};
+
+/// Trait for converting a `Span` into a cloneable `Shared` span.
+pub trait IntoShared {
+    /// Returns a `Shared` span handle that can be cloned.
+    ///
+    /// This will allocate memory to store the span if the span is enabled.
+    fn into_shared(self) -> Shared;
+}
+
+impl IntoShared for Span {
+    fn into_shared(self) -> Shared {
+        Shared::from_span(self)
+    }
+}
+
+/// A shared, heap-allocated span handle, which may be cloned.
+///
+/// Unlike a `Span` handle, entering a `Shared` span handle consumes the handle.
+/// This is used to determine when the span may be closed. Thus, each `Shared`
+/// handle may be thought of as representing a *single* attempt to enter the
+/// span.
+///
+/// However, they may be cloned inexpensively (as they are internally
+/// reference-counted).
+// NOTE: It may also be worthwhile to provide an `UnsyncShared` type, that uses
+// `Rc` rather than `Arc` for performance reasons? I'm not sure what the
+// use-case for that would be, though.
+#[derive(Clone, Debug)]
+pub struct Shared {
+    inner: Option<Arc<Enter>>,
+}
+
+impl Shared {
+    /// Returns a `Shared` span handle that can be cloned.
+    ///
+    /// This function allocates memory to store the shared span, if the span is
+    /// enabled.
+    pub fn from_span(span: Span) -> Self {
+        Self {
+            inner: Enter::from_span(span).map(Arc::new),
+        }
+    }
+
+    /// Executes the given function in the context of this span.
+    ///
+    /// Unlike `Span::enter`, this *consumes* the shared span handle.
+    ///
+    /// If this span is enabled, then this function enters the span, invokes
+    /// and then exits the span. If the span is disabled, `f` will still be
+    /// invoked, but in the context of the currently-executing span (if there is
+    /// one).
+    ///
+    /// Returns the result of evaluating `f`.
+    pub fn enter<F: FnOnce() -> T, T>(self, f: F) -> T {
+        if let Some(inner) = self.inner {
+            let guard = inner.enter();
+            let result = f();
+            inner.exit_and_join(guard);
+            if Arc::strong_count(&inner) == 1 {
+                inner.close();
+            }
+            result
+        } else {
+            f()
+        }
+    }
+
+    /// Returns the `Id` of the parent of this span, if one exists.
+    pub fn parent(&self) -> Option<Id> {
+        self.inner.as_ref().and_then(|inner| inner.parent())
+    }
+
+    /// Returns the `Id` of the span, or `None` if it is disabled.
+    pub fn id(&self) -> Option<Id> {
+        self.inner.as_ref().map(|inner| inner.id())
+    }
+
+    /// Returns `true` if this span is enabled.
+    pub fn is_enabled(&self) -> bool {
+        self.inner.is_some()
+    }
+
+    /// Sets the field on this span named `name` to the given `value`.
+    ///
+    /// `name` must name a field already defined by this span's metadata, and
+    /// the field must not already have a value. If this is not the case, this
+    /// function returns an [`AddValueError`](::subscriber::AddValueError).
+    pub fn add_value(
+        &self,
+        field: &'static str,
+        value: &dyn IntoValue,
+    ) -> Result<(), subscriber::AddValueError> {
+        self.inner
+            .as_ref()
+            .map(|inner| inner.add_value(field, value))
+            .unwrap_or(Ok(()))
+    }
+
+    /// Indicates that the span with the given ID has an indirect causal
+    /// relationship with this span.
+    ///
+    /// This relationship differs somewhat from the parent-child relationship: a
+    /// span may have any number of prior spans, rather than a single one; and
+    /// spans are not considered to be executing _inside_ of the spans they
+    /// follow from. This means that a span may close even if subsequent spans
+    /// that follow from it are still open, and time spent inside of a
+    /// subsequent span should not be included in the time its precedents were
+    /// executing. This is used to model causal relationships such as when a
+    /// single future spawns several related background tasks, et cetera.
+    ///
+    /// If this span is disabled, this function will do nothing. Otherwise, it
+    /// returns `Ok(())` if the other span was added as a precedent of this
+    /// span, or an error if this was not possible.
+    pub fn follows_from<I: AsId>(&self, from: I) -> Result<(), subscriber::FollowsError> {
+        self.inner
+            .as_ref()
+            .map(move |inner| inner.follows_from(from))
+            .unwrap_or(Ok(()))
+    }
+}
+
+impl AsId for Shared {
+    fn as_id(&self) -> Option<Id> {
+        self.id()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -193,68 +376,6 @@ mod tests {
             });
         });
     }
-
-    // This test doesn't make sense in the context of non-cloneable spans.
-    /*
-   #[test]
-    fn exit_doesnt_finish_concurrently_executing_spans() {
-        // Test that exiting a span only marks it as "done" when no other
-        // threads are still executing inside that span.
-        use std::sync::{Arc, Barrier};
-
-        let subscriber = subscriber::mock()
-            .enter(span::mock().named(Some("baz")))
-            // Main thread enters "quux".
-            .enter(span::mock().named(Some("quux")))
-            // Spawned thread also enters "quux".
-            .enter(span::mock().named(Some("quux")))
-            // When the main thread exits "quux", it will still be running in the
-            // spawned thread.
-            .exit(span::mock().named(Some("quux")))
-            // Now, when this thread exits "quux", there is no handle to re-enter it, so
-            // it should become "done".
-            .exit(span::mock().named(Some("quux")))
-            .close(span::mock().named(Some("quux")))
-            // "baz" never had more than one handle, so it should also become
-            // "done" when we exit it.
-            .exit(span::mock().named(Some("baz")))
-            .close(span::mock().named(Some("baz")))
-            .run();
-
-        Dispatch::to(subscriber).as_default(|| {
-            let barrier1 = Arc::new(Barrier::new(2));
-            let barrier2 = Arc::new(Barrier::new(2));
-            // Make copies of the barriers for thread 2 to wait on.
-            let t2_barrier1 = barrier1.clone();
-            let t2_barrier2 = barrier2.clone();
-
-            span!("baz",).enter(move || {
-                let quux = span!("quux",);
-                let quux2 = quux.clone();
-                let handle = thread::Builder::new()
-                    .name("thread-2".to_string())
-                    .spawn(move || {
-                        quux2.enter(|| {
-                            // Once this thread has entered "quux", allow thread 1
-                            // to exit.
-                            t2_barrier1.wait();
-                            // Wait for the main thread to allow us to exit.
-                            t2_barrier2.wait();
-                        })
-                    }).expect("spawn test thread");
-                quux.enter(|| {
-                    // Wait for thread 2 to enter "quux". When we exit "quux", it
-                    // should stay running, since it's running in the other thread.
-                    barrier1.wait();
-                });
-                // After we exit "quux", wait for the second barrier, so the other
-                // thread unblocks and exits "quux".
-                barrier2.wait();
-                handle.join().unwrap();
-            });
-        });
-    }
-    */
 
     #[test]
     fn handles_to_the_same_span_are_equal() {
@@ -420,5 +541,123 @@ mod tests {
             let span = span!("foo");
             drop(span);
         })
+    }
+
+    mod shared {
+        use super::*;
+
+        #[test]
+        fn span_closes_on_drop() {
+            let subscriber = subscriber::mock()
+                .enter(span::mock().named(Some("foo")))
+                .exit(span::mock().named(Some("foo")))
+                .close(span::mock().named(Some("foo")))
+                .done()
+                .run();
+            Dispatch::to(subscriber).as_default(|| span!("foo").into_shared().enter(|| {}))
+        }
+
+        #[test]
+        fn span_doesnt_close_if_it_never_opened() {
+            let subscriber = subscriber::mock().done().run();
+            Dispatch::to(subscriber).as_default(|| {
+                let span = span!("foo").into_shared();
+                drop(span);
+            })
+        }
+
+        #[test]
+        fn exit_doesnt_finish_concurrently_executing_spans() {
+            // Test that exiting a span only marks it as "done" when no other
+            // threads are still executing inside that span.
+            use std::sync::{Arc, Barrier};
+
+            let subscriber = subscriber::mock()
+                .enter(span::mock().named(Some("baz")))
+                // Main thread enters "quux".
+                .enter(span::mock().named(Some("quux")))
+                // Spawned thread also enters "quux".
+                .enter(span::mock().named(Some("quux")))
+                // When the main thread exits "quux", it will still be running in the
+                // spawned thread.
+                .exit(span::mock().named(Some("quux")))
+                // Now, when this thread exits "quux", there is no handle to re-enter it, so
+                // it should become "done".
+                .exit(span::mock().named(Some("quux")))
+                .close(span::mock().named(Some("quux")))
+                // "baz" never had more than one handle, so it should also become
+                // "done" when we exit it.
+                .exit(span::mock().named(Some("baz")))
+                .close(span::mock().named(Some("baz")))
+                .done()
+                .run();
+
+            Dispatch::to(subscriber).as_default(|| {
+                let barrier1 = Arc::new(Barrier::new(2));
+                let barrier2 = Arc::new(Barrier::new(2));
+                // Make copies of the barriers for thread 2 to wait on.
+                let t2_barrier1 = barrier1.clone();
+                let t2_barrier2 = barrier2.clone();
+
+                span!("baz",).enter(move || {
+                    let quux = span!("quux",).into_shared();
+                    let quux2 = quux.clone();
+                    let handle = thread::Builder::new()
+                        .name("thread-2".to_string())
+                        .spawn(move || {
+                            quux2.enter(|| {
+                                // Once this thread has entered "quux", allow thread 1
+                                // to exit.
+                                t2_barrier1.wait();
+                                // Wait for the main thread to allow us to exit.
+                                t2_barrier2.wait();
+                            })
+                        }).expect("spawn test thread");
+                    quux.enter(|| {
+                        // Wait for thread 2 to enter "quux". When we exit "quux", it
+                        // should stay running, since it's running in the other thread.
+                        barrier1.wait();
+                    });
+                    // After we exit "quux", wait for the second barrier, so the other
+                    // thread unblocks and exits "quux".
+                    barrier2.wait();
+                    handle.join().unwrap();
+                });
+            });
+        }
+
+        #[test]
+        fn exit_doesnt_finish_while_handles_still_exist() {
+            // Test that exiting a span only marks it as "done" when no handles
+            // that can re-enter the span exist.
+            let subscriber = subscriber::mock()
+                .enter(span::mock().named(Some("foo")))
+                .enter(span::mock().named(Some("bar")))
+                // The first time we exit "bar", there will be another handle with
+                // which we could potentially re-enter bar.
+                .exit(span::mock().named(Some("bar")))
+                // Re-enter "bar", using the cloned handle.
+                .enter(span::mock().named(Some("bar")))
+                // Now, when we exit "bar", there is no handle to re-enter it, so
+                // it should become "done".
+                .exit(span::mock().named(Some("bar")))
+                .close(span::mock().named(Some("bar")))
+                .exit(span::mock().named(Some("foo")))
+                .done()
+                .run();
+
+            Dispatch::to(subscriber).as_default(|| {
+                span!("foo").enter(|| {
+                    let bar = span!("bar").into_shared();
+                    bar.clone().enter(|| {
+                        // do nothing. exiting "bar" should leave it idle, since it can
+                        // be re-entered.
+                    });
+                    // Enter "bar" again. consuming the last handle to `bar`
+                    // close the span automatically.
+                    bar.enter(|| {});
+                });
+            });
+        }
     }
 }
