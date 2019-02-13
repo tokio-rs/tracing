a3dfa3b78a575bdad10aa684dcea78660853e0a6
Eliza Weisman <eliza@buoyant.io>
2019-02-13 10:47:40 -0800

    Make "yak_shave" example show off more stuff

--- tokio-trace-fmt/examples/yak_shave.rs
+++ tokio-trace-fmt/examples/yak_shave.rs
@@ -8,16 +8,20 @@ fn main() {
         .finish();
 
     tokio_trace::subscriber::with_default(subscriber, || {
-        span!("yak_shave").enter(|| {
-            let number_of_yaks = 3;
-            info!(yaks_to_shave = number_of_yaks);
-            info!({ yak = number_of_yaks, excitement = "yay!" }, "hello! I'm gonna shave a yak.");
+        let number_of_yaks = 3;
+
+        trace!("preparing to shave {} yaks", number_of_yaks);
+
+        span!("yak_shave", yaks_to_shave = number_of_yaks).enter(|| {
+            info!("shaving yaks");
+            debug!({ yak = number_of_yaks, excitement = "yay!" }, "hello! I'm gonna shave a yak.");
 
             let mut span = span!("my_great_span", foo = &4, baz = &5);
             span.enter(|| {
                 error!({ yak_shaved = false }, "Huh, no yaks to be found...");
             });
-        })
+        });
 
+        trace!("yak shaving completed");
     });
 }
