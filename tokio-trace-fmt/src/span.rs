0e04567bde3141bb35b0b22761f9dc2d3c89cc57
Eliza Weisman <eliza@buoyant.io>
2019-02-12 17:45:35 -0800

    Rename `with_spans` -> `visit_spans`
    This seems more descriptive IMO.

--- tokio-trace-fmt/src/span.rs
+++ tokio-trace-fmt/src/span.rs
@@ -13,6 +13,7 @@ pub use tokio_trace_core::Span as Id;
 
 #[derive(Debug)]
 pub struct Data {
+    parent: Option<Id>,
     pub(crate) name: &'static str,
     pub(crate) fields: String,
     ref_count: AtomicUsize,
@@ -31,6 +32,7 @@ thread_local! {
 impl Data {
     pub(crate) fn new(name: &'static str, fields: String) -> Self {
         Self {
+            parent: Context::current(),
             name,
             fields,
             ref_count: AtomicUsize::new(1),
@@ -53,6 +55,18 @@ impl Data {
     #[inline]
     pub(crate) fn drop_ref(&self) -> bool {
         self.ref_count.fetch_sub(1, Ordering::AcqRel) == 1
+    }
+
+    fn with_parent<'store, F, E>(&self, my_id: &Id, f: &mut F, store: &'store HashMap<Id, Data>) -> Result<(), E>
+    where
+        F: FnMut(&Id, &Data) -> Result<(), E>
+    {
+        if let Some(parent_id) = self.parent.as_ref() {
+            if let Some(parent) = store.get(parent_id) {
+                parent.with_parent(parent_id, f, store)?;
+            }
+        }
+        f(my_id, self)
     }
 }
 
@@ -98,21 +112,29 @@ impl<'a> Context<'a> {
         }).ok()?
     }
 
-    pub fn with_spans<F, E>(&self, mut f: F) -> Result<(), E>
+    /// Applies a function to each span in the current trace context.
+    ///
+    /// The function is applied in order, beginning with the root of the trace,
+    /// and ending with the current span. If the function returns an error,
+    /// this will short-circuit.
+    ///
+    /// If invoked from outside of a span, the function will not be applied.
+    ///
+    /// Note that if we are currently unwinding, this will do nothing, rather
+    /// than potentially causing a double panic.
+    pub fn visit_spans<F, E>(&self, mut f: F) -> Result<(), E>
     where
-        F: FnMut((&Id, &Data)) -> Result<(), E>
+        F: FnMut(&Id, &Data) -> Result<(), E>
     {
-        // If the lock is poisoned or the thread local has already been
-        // destroyed, we might be in the middle of unwinding, so this
-        // will just do nothing rather than cause a double panic.
         CONTEXT.try_with(|current| {
-            if let Ok(lock) = self.lock.read() {
-                let stack = current.borrow();
-                let spans = stack.iter().filter_map(|id| {
-                    lock.get(id).map(|span| (id, span))
-                });
-                for span in spans {
-                    f(span)?;
+            if let Some(id) = current.borrow().last() {
+                if let Ok(store) = self.lock.read() {
+                    if let Some(span) = store.get(id) {
+                        // with_parent uses the call stack to visit the span
+                        // stack in reverse order, without having to allocate
+                        // a buffer.
+                        return span.with_parent(id, &mut f, &store);
+                    }
                 }
             }
             Ok(())
@@ -123,6 +145,9 @@ impl<'a> Context<'a> {
     where
         F: FnOnce((&Id, &Data)) -> R,
     {
+        // If the lock is poisoned or the thread local has already been
+        // destroyed, we might be in the middle of unwinding, so this
+        // will just do nothing rather than cause a double panic.
         CONTEXT.try_with(|current| {
             if let Some(id) = current.borrow().last() {
                 let spans = self.lock.read().ok()?;
