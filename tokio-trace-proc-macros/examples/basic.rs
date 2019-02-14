b930cd7b0ad433b36ce4b28d908393a5ad2582a7
Eliza Weisman <eliza@buoyant.io>
2019-02-13 17:09:53 -0800

    First pass on filtering!
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace-proc-macros/examples/basic.rs
+++ tokio-trace-proc-macros/examples/basic.rs
@@ -3,17 +3,15 @@ extern crate tokio_trace;
 #[macro_use]
 extern crate tokio_trace_proc_macros;
 extern crate env_logger;
-extern crate tokio_trace_log;
+extern crate tokio_trace_fmt;
 
 use tokio_trace::field;
 
 fn main() {
     env_logger::Builder::new().parse("trace").init();
-    let subscriber = tokio_trace_log::TraceLogger::builder()
-        .with_span_entry(true)
-        .with_span_exits(true)
+    let subscriber = tokio_trace_fmt::FmtSubscriber::builder()
+        .full()
         .finish();
-
     tokio_trace::subscriber::with_default(subscriber, || {
         let num: u64 = 1;
 
