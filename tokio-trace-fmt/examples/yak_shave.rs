5c0126b46ccf650ae11fa7e4b0521472bb39456c
Eliza Weisman <eliza@buoyant.io>
2019-02-15 10:44:49 -0800

    Make yak-shaving example a little fancier
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace-fmt/examples/yak_shave.rs
+++ tokio-trace-fmt/examples/yak_shave.rs
@@ -2,6 +2,19 @@
 extern crate tokio_trace;
 extern crate tokio_trace_fmt;
 
+fn shave(yak: usize) -> bool {
+    span!("shave", yak = yak).enter(|| {
+        debug!(message = "hello! I'm gonna shave a yak.", excitement = "yay!");
+        if yak == 3 {
+            warn!(target: "yak_events", "could not locate yak!");
+            false
+        } else {
+            trace!(target: "yak_events", "yak shaved successfully");
+            true
+        }
+    })
+}
+
 fn main() {
     let subscriber = tokio_trace_fmt::FmtSubscriber::builder()
         .full()
@@ -9,19 +22,29 @@ fn main() {
 
     tokio_trace::subscriber::with_default(subscriber, || {
         let number_of_yaks = 3;
+        let mut number_shaved = 0;
+        debug!("preparing to shave {} yaks", number_of_yaks);
 
-        trace!("preparing to shave {} yaks", number_of_yaks);
-
-        span!("yak_shave", yaks_to_shave = number_of_yaks).enter(|| {
+        span!("shaving_yaks", yaks_to_shave = number_of_yaks).enter(|| {
             info!("shaving yaks");
-            debug!({ yak = number_of_yaks, excitement = "yay!" }, "hello! I'm gonna shave a yak.");
 
-            let mut span = span!("my_great_span", foo = &4, baz = &5);
-            span.enter(|| {
-                error!({ yak_shaved = false }, "Huh, no yaks to be found...");
-            });
+            for yak in 1..=number_of_yaks {
+                let shaved = shave(yak);
+                trace!(target: "yak_events", yak = yak, shaved = shaved);
+
+                if !shaved {
+                    error!(message = "failed to shave yak!", yak = yak);
+                } else {
+                    number_shaved += 1;
+                }
+
+                trace!(target: "yak_events", yaks_shaved = number_shaved);
+            }
         });
 
-        trace!("yak shaving completed");
+        debug!(
+            message = "yak shaving completed.",
+            all_yaks_shaved = number_shaved == number_of_yaks,
+        );
     });
 }
