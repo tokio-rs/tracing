dbaa787ce017b42a3307ef086904ddbe266a841c
Eliza Weisman <eliza@buoyant.io>
2019-03-01 12:20:18 -0800

    fix 1.26.0 build
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace-futures/src/test_support/subscriber.rs
+++ tokio-trace-futures/src/test_support/subscriber.rs
@@ -12,7 +12,11 @@ use std::{
         Arc, Mutex,
     },
 };
-use tokio_trace::{field, Event, Id, Metadata, Subscriber};
+use tokio_trace::{
+    field,
+    span::{self, Id},
+    Event, Metadata, Subscriber,
+};
 
 #[derive(Debug, Eq, PartialEq)]
 enum Expect {
@@ -172,7 +176,7 @@ impl<F: Fn(&Metadata) -> bool> Subscriber for Running<F> {
         // TODO: it should be possible to expect spans to follow from other spans
     }
 
-    fn new_span(&self, attrs: &tokio_trace::span::Attributes) -> Id {
+    fn new_span(&self, attrs: &span::Attributes) -> Id {
         let meta = attrs.metadata();
         let values = attrs.values();
         let id = self.ids.fetch_add(1, Ordering::SeqCst);
