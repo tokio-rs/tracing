0cff2edf5d8318530c26c99410346f9195555ccb
Eliza Weisman <eliza@buoyant.io>
2019-03-12 10:12:49 -0700

    chore: Update for upstream changes (#26)
    This branch updates the nursery to ensure compatibility with
    tokio-rs/tokio#971 and tokio-rs/tokio#973.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace-subscriber/src/registry.rs
+++ tokio-trace-subscriber/src/registry.rs
@@ -160,7 +160,7 @@ pub fn increasing_counter() -> IncreasingCounter {
 impl Default for IncreasingCounter {
     fn default() -> Self {
         Self {
-            next_id: AtomicUsize::new(0),
+            next_id: AtomicUsize::new(1),
             spans: Mutex::new(HashMap::new()),
         }
     }
@@ -172,9 +172,9 @@ impl RegisterSpan for IncreasingCounter {
     fn new_span(&self, _new_span: &Attributes) -> Id {
         let id = self.next_id.fetch_add(1, Ordering::SeqCst);
         let id = Id::from_u64(id as u64);
-        if let Ok(spans) = self.spans.lock() {
-            // spans.insert(id.clone(), new_span);
-        }
+        // if let Ok(spans) = self.spans.lock() {
+        //     // spans.insert(id.clone(), new_span);
+        // }
         id
     }
 
