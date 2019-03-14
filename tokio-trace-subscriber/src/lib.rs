46fd1dff2c13a196055287e559229d30ae891800
Eliza Weisman <eliza@buoyant.io>
2019-03-14 14:46:50 -0700

    wip

--- tokio-trace-subscriber/src/lib.rs
+++ tokio-trace-subscriber/src/lib.rs
@@ -1,57 +1,6 @@
 //! Utilities and helpers for implementing and composing subscribers.
 
-extern crate tokio_trace;
-pub use tokio_trace::{Event, Id};
+extern crate tokio_trace_core;
 
-mod compose;
-pub use compose::Composed;
-
-pub mod filter;
-pub mod observe;
 pub mod registry;
-
-pub use filter::{Filter, FilterExt};
-pub use observe::{Observe, ObserveExt};
-pub use registry::{RegisterSpan, SpanRef};
-
-use std::{cell::RefCell, default::Default, thread};
-
-/// Tracks the currently executing span on a per-thread basis.
-#[derive(Clone)]
-pub struct CurrentSpanPerThread {
-    current: &'static thread::LocalKey<RefCell<Vec<Id>>>,
-}
-
-impl CurrentSpanPerThread {
-    pub fn new() -> Self {
-        thread_local! {
-            static CURRENT: RefCell<Vec<Id>> = RefCell::new(vec![]);
-        };
-        Self { current: &CURRENT }
-    }
-
-    /// Returns the [`Id`](::Id) of the span in which the current thread is
-    /// executing, or `None` if it is not inside of a span.
-    pub fn id(&self) -> Option<Id> {
-        self.current
-            .with(|current| current.borrow().last().cloned())
-    }
-
-    pub fn enter(&self, span: Id) {
-        self.current.with(|current| {
-            current.borrow_mut().push(span);
-        })
-    }
-
-    pub fn exit(&self) {
-        self.current.with(|current| {
-            let _ = current.borrow_mut().pop();
-        })
-    }
-}
-
-impl Default for CurrentSpanPerThread {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+pub mod observe;
--- tokio-trace-subscriber/src/observe.rs
+++ tokio-trace-subscriber/src/observe.rs
@@ -1,28 +1,75 @@
 use {
-    filter::{self, Filter},
+    // filter::{self, Filter},
     registry::SpanRef,
 };
 
-use tokio_trace::{Event, Metadata};
+use std::any::{Any, TypeId};
+use tokio_trace_core::{Event, Metadata};
 
 /// The notification processing portion of the [`Subscriber`] trait.
 ///
 /// Implementations of this trait describe the logic needed to process envent
 /// and span notifications, but don't implement span registration.
-pub trait Observe {
-    fn observe_event<'a>(&self, event: &'a Event<'a>);
-    fn enter(&self, span: &SpanRef);
-    fn exit(&self, span: &SpanRef);
-    fn close(&self, span: &SpanRef);
+pub trait Observe<S: SpanRef>: 'static {
+    fn event(&self, event: &Event);
+    fn enter(&self, span: &S);
+    fn exit(&self, span: &S);
+    fn close(&self, span: &S);
 
-    fn filter(&self) -> &Filter {
-        &filter::NoFilter
+    // fn filter(&self) -> &Filter {
+    //     &filter::NoFilter
+    // }
+
+    // === Downcasting methods ================================================
+
+    /// Gets the `TypeId` of `Self`.
+    ///
+    /// Implementations of `Subscriber` are **not** expected to override this!
+    #[doc(hidden)]
+    fn type_id(&self) -> TypeId {
+        TypeId::of::<Self>()
+    }
+
+    /// If `self` is the same type as the provided `TypeId`, returns an untyped
+    /// `*const` pointer to that type. Otherwise, returns `None`.
+    ///
+    /// If you wish to downcast a `Subscriber`, it is strongly advised to use
+    /// the safe API provided by [`downcast_ref`] instead.
+    ///
+    /// This API is required for `downcast_raw` to be a trait method; a method
+    /// signature like [`downcast_ref`] (with a generic type parameter) is not
+    /// object-safe, and thus cannot be a trait method for `Subscriber`. This
+    /// means that if we only exposed `downcast_ref`, `Subscriber`
+    /// implementations could not override the downcasting behavior
+    ///
+    /// This method may be overridden by "fan out" or "chained" subscriber
+    /// implementations which consist of multiple composed types. Such
+    /// subscribers might allow `downcast_raw` by returning references to those
+    /// component if they contain components with the given `TypeId`.
+    fn downcast_raw(&self, id: TypeId) -> Option<*const ()> {
+        if id == self.type_id() {
+            Some(self as *const Self as *const ())
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if this `Subscriber` is the same type as `T`.
+    fn is<T: Any>(&self) -> bool {
+        self.downcast_raw(TypeId::of::<T>()).is_some()
+    }
+
+    /// Returns some reference to this `Subscriber` value if it is of type `T`,
+    /// or `None` if it isn't.
+    fn downcast_ref<T: Any>(&self) -> Option<&T> {
+        self.downcast_raw(TypeId::of::<T>())
+            .map(|raw| unsafe { &*(raw as *const _) })
     }
 }
 
 /// Extension trait providing combinators and helper methods for working with
 /// instances of `Observe`.
-pub trait ObserveExt: Observe {
+pub trait ObserveExt<S: SpanRef>: Observe<S> {
     /// Construct a new observer that sends events to both `self` and `other`.
     ///
     /// For example:
@@ -80,7 +127,7 @@ pub trait ObserveExt: Observe {
     /// ```
     fn tee_to<I>(self, other: I) -> Tee<Self, I::Observer>
     where
-        I: IntoObserver,
+        I: IntoObserver<S>,
         Self: Sized,
     {
         Tee {
@@ -89,66 +136,66 @@ pub trait ObserveExt: Observe {
         }
     }
 
-    /// Composes `self` with a [`Filter`].
-    ///
-    /// This function is intended to be used with composing observers from
-    /// external crates with user-defined filters, so that the resulting
-    /// observer is [`enabled`] only for a subset of the events and spans for
-    /// which the original observer would be enabled.
-    ///
-    ///
-    /// For example:
-    // TODO: this needs to be fixed since it uses the `tokio-trace-log` crate,
-    // which  doesn't work with `Observer` yet.
-    /// ```ignore
-    /// #[macro_use]
-    /// extern crate tokio_trace;
-    /// extern crate tokio_trace_log;
-    /// extern crate tokio_trace_subscriber;
-    /// use tokio_trace_subscriber::{registry, filter, Observe, ObserveExt, SpanRef};
-    /// # use tokio_trace::{Level, Metadata, Span};
-    /// # fn main() {
-    ///
-    /// let observer = tokio_trace_log::TraceLogger::new()
-    ///     // Subscribe *only* to spans named "foo".
-    ///     .with_filter(|meta: &Metadata| {
-    ///         meta.name == Some("foo")
-    ///     });
-    ///
-    /// let subscriber = tokio_trace_subscriber::Composed::builder()
-    ///     .with_observer(observer)
-    ///     .with_registry(registry::increasing_counter());
-    ///
-    /// tokio_trace::Dispatch::new(subscriber).as_default(|| {
-    ///     /// // This span will be logged.
-    ///     span!("foo", enabled = &true) .enter(|| {
-    ///         // do work;
-    ///     });
-    ///     // This span will *not* be logged.
-    ///     span!("bar", enabled = &false).enter(|| {
-    ///         // This event also will not be logged.
-    ///         event!(Level::Debug, { enabled = false },"this won't be logged");
-    ///     });
-    /// });
-    /// # }
-    /// ```
-    ///
-    /// [`Filter`]: ../trait.Filter.html
-    /// [`enabled`]: ../trait.Filter.html#tymethod.enabled
-    fn with_filter<F>(self, filter: F) -> WithFilter<Self, F>
-    where
-        F: Filter,
-        Self: Sized,
-    {
-        WithFilter {
-            inner: self,
-            filter,
-        }
-    }
+    // /// Composes `self` with a [`Filter`].
+    // ///
+    // /// This function is intended to be used with composing observers from
+    // /// external crates with user-defined filters, so that the resulting
+    // /// observer is [`enabled`] only for a subset of the events and spans for
+    // /// which the original observer would be enabled.
+    // ///
+    // ///
+    // /// For example:
+    // // TODO: this needs to be fixed since it uses the `tokio-trace-log` crate,
+    // // which  doesn't work with `Observer` yet.
+    // /// ```ignore
+    // /// #[macro_use]
+    // /// extern crate tokio_trace;
+    // /// extern crate tokio_trace_log;
+    // /// extern crate tokio_trace_subscriber;
+    // /// use tokio_trace_subscriber::{registry, filter, Observe, ObserveExt, SpanRef};
+    // /// # use tokio_trace::{Level, Metadata, Span};
+    // /// # fn main() {
+    // ///
+    // /// let observer = tokio_trace_log::TraceLogger::new()
+    // ///     // Subscribe *only* to spans named "foo".
+    // ///     .with_filter(|meta: &Metadata| {
+    // ///         meta.name == Some("foo")
+    // ///     });
+    // ///
+    // /// let subscriber = tokio_trace_subscriber::Composed::builder()
+    // ///     .with_observer(observer)
+    // ///     .with_registry(registry::increasing_counter());
+    // ///
+    // /// tokio_trace::Dispatch::new(subscriber).as_default(|| {
+    // ///     /// // This span will be logged.
+    // ///     span!("foo", enabled = &true) .enter(|| {
+    // ///         // do work;
+    // ///     });
+    // ///     // This span will *not* be logged.
+    // ///     span!("bar", enabled = &false).enter(|| {
+    // ///         // This event also will not be logged.
+    // ///         event!(Level::Debug, { enabled = false },"this won't be logged");
+    // ///     });
+    // /// });
+    // /// # }
+    // /// ```
+    // ///
+    // /// [`Filter`]: ../trait.Filter.html
+    // /// [`enabled`]: ../trait.Filter.html#tymethod.enabled
+    // fn with_filter<F>(self, filter: F) -> WithFilter<Self, F>
+    // where
+    //     F: Filter,
+    //     Self: Sized,
+    // {
+    //     WithFilter {
+    //         inner: self,
+    //         filter,
+    //     }
+    // }
 }
 
-pub trait IntoObserver {
-    type Observer: Observe;
+pub trait IntoObserver<S: SpanRef> {
+    type Observer: Observe<S>;
     fn into_observer(self) -> Self::Observer;
 }
 
@@ -234,62 +281,62 @@ pub struct WithFilter<O, F> {
     filter: F,
 }
 
-impl<O, F> Filter for WithFilter<O, F>
+// impl<O, F> Filter for WithFilter<O, F>
+// where
+//     O: Observe,
+//     F: Filter,
+// {
+//     #[inline]
+//     fn enabled(&self, metadata: &Metadata) -> bool {
+//         self.filter.enabled(metadata) && self.inner.filter().enabled(metadata)
+//     }
+
+//     #[inline]
+//     fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
+//         self.filter.should_invalidate_filter(metadata)
+//             || self.inner.filter().should_invalidate_filter(metadata)
+//     }
+// }
+
+// impl<O, F> Observe for WithFilter<O, F>
+// where
+//     O: Observe,
+//     F: Filter,
+// {
+//     #[inline]
+//     fn observe_event<'a>(&self, event: &'a Event<'a>) {
+//         self.inner.observe_event(event)
+//     }
+
+//     #[inline]
+//     fn enter(&self, span: &SpanRef) {
+//         self.inner.enter(span)
+//     }
+
+//     #[inline]
+//     fn exit(&self, span: &SpanRef) {
+//         self.inner.exit(span)
+//     }
+
+//     #[inline]
+//     fn close(&self, span: &SpanRef) {
+//         self.inner.close(span)
+//     }
+
+//     fn filter(&self) -> &Filter {
+//         self
+//     }
+// }
+
+// pub fn none() -> NoObserver {
+//     NoObserver
+// }
+
+impl<T, S: SpanRef> ObserveExt<S> for T where T: Observe<S> {}
+
+impl<T, S: SpanRef> IntoObserver<S> for T
 where
-    O: Observe,
-    F: Filter,
-{
-    #[inline]
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        self.filter.enabled(metadata) && self.inner.filter().enabled(metadata)
-    }
-
-    #[inline]
-    fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
-        self.filter.should_invalidate_filter(metadata)
-            || self.inner.filter().should_invalidate_filter(metadata)
-    }
-}
-
-impl<O, F> Observe for WithFilter<O, F>
-where
-    O: Observe,
-    F: Filter,
-{
-    #[inline]
-    fn observe_event<'a>(&self, event: &'a Event<'a>) {
-        self.inner.observe_event(event)
-    }
-
-    #[inline]
-    fn enter(&self, span: &SpanRef) {
-        self.inner.enter(span)
-    }
-
-    #[inline]
-    fn exit(&self, span: &SpanRef) {
-        self.inner.exit(span)
-    }
-
-    #[inline]
-    fn close(&self, span: &SpanRef) {
-        self.inner.close(span)
-    }
-
-    fn filter(&self) -> &Filter {
-        self
-    }
-}
-
-pub fn none() -> NoObserver {
-    NoObserver
-}
-
-impl<T> ObserveExt for T where T: Observe {}
-
-impl<T> IntoObserver for T
-where
-    T: Observe,
+    T: Observe<S>,
 {
     type Observer = Self;
     fn into_observer(self) -> Self::Observer {
@@ -298,125 +345,135 @@ where
 }
 
 // XXX: maybe this should just be an impl of `Observe` for tuples of `(Observe, Observe)`...?
-impl<A, B> Observe for Tee<A, B>
+impl<A, B, S: SpanRef> Observe<S> for Tee<A, B>
 where
-    A: Observe,
-    B: Observe,
+    A: Observe<S>,
+    B: Observe<S>,
 {
-    fn observe_event<'a>(&self, event: &'a Event<'a>) {
-        self.a.observe_event(event);
-        self.b.observe_event(event);
+
+    fn event(&self, event: &Event) {
+        self.a.event(event);
+        self.b.event(event);
     }
 
-    fn enter(&self, span: &SpanRef) {
+    fn enter(&self, span: &S) {
         self.a.enter(span);
         self.b.enter(span);
     }
 
-    fn exit(&self, span: &SpanRef) {
+    fn exit(&self, span: &S) {
         self.a.exit(span);
         self.b.exit(span);
     }
 
-    fn close(&self, span: &SpanRef) {
+    fn close(&self, span: &S) {
         self.a.close(span);
         self.b.close(span);
     }
 
-    fn filter(&self) -> &Filter {
-        self
-    }
-}
+    // fn filter(&self) -> &Filter {
+    //     self
+    // }
 
-impl<A, B> Filter for Tee<A, B>
-where
-    A: Observe,
-    B: Observe,
-{
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        self.a.filter().enabled(metadata) || self.b.filter().enabled(metadata)
-    }
-
-    fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
-        self.a.filter().should_invalidate_filter(metadata)
-            || self.b.filter().should_invalidate_filter(metadata)
-    }
-}
-
-impl<A, B> Observe for Either<A, B>
-where
-    A: Observe,
-    B: Observe,
-{
-    fn observe_event<'a>(&self, event: &'a Event<'a>) {
-        match self {
-            Either::A(a) => a.observe_event(event),
-            Either::B(b) => b.observe_event(event),
-        }
-    }
-
-    fn enter(&self, span: &SpanRef) {
-        match self {
-            Either::A(a) => a.enter(span),
-            Either::B(b) => b.enter(span),
-        }
-    }
-
-    fn exit(&self, span: &SpanRef) {
-        match self {
-            Either::A(a) => a.exit(span),
-            Either::B(b) => b.exit(span),
-        }
-    }
-
-    fn close(&self, span: &SpanRef) {
-        match self {
-            Either::A(a) => a.close(span),
-            Either::B(b) => b.close(span),
+    fn downcast_raw(&self, id: TypeId) -> Option<*const ()> {
+        match id {
+            id if id == self.type_id() => Some(self as *const _ as *const ()),
+            id if id == self.a.type_id() => Some(&self.a as *const _ as *const ()),
+            id if id == self.b.type_id() => Some(&self.b as *const _ as *const ()),
+            _ => None,
         }
     }
 }
 
-impl<A, B> Filter for Either<A, B>
-where
-    A: Observe,
-    B: Observe,
-{
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        match self {
-            Either::A(a) => a.filter().enabled(metadata),
-            Either::B(b) => b.filter().enabled(metadata),
-        }
-    }
+// impl<A, B> Filter for Tee<A, B>
+// where
+//     A: Observe,
+//     B: Observe,
+// {
+//     fn enabled(&self, metadata: &Metadata) -> bool {
+//         self.a.filter().enabled(metadata) || self.b.filter().enabled(metadata)
+//     }
 
-    fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
-        match self {
-            Either::A(a) => a.filter().should_invalidate_filter(metadata),
-            Either::B(b) => b.filter().should_invalidate_filter(metadata),
-        }
-    }
-}
+//     fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
+//         self.a.filter().should_invalidate_filter(metadata)
+//             || self.b.filter().should_invalidate_filter(metadata)
+//     }
+// }
 
-impl Observe for NoObserver {
-    fn observe_event<'a>(&self, _event: &'a Event<'a>) {}
+// impl<A, B> Observe for Either<A, B>
+// where
+//     A: Observe,
+//     B: Observe,
+// {
+//     fn observe_event<'a>(&self, event: &'a Event<'a>) {
+//         match self {
+//             Either::A(a) => a.observe_event(event),
+//             Either::B(b) => b.observe_event(event),
+//         }
+//     }
 
-    fn enter(&self, _span: &SpanRef) {}
+//     fn enter(&self, span: &SpanRef) {
+//         match self {
+//             Either::A(a) => a.enter(span),
+//             Either::B(b) => b.enter(span),
+//         }
+//     }
 
-    fn exit(&self, _span: &SpanRef) {}
+//     fn exit(&self, span: &SpanRef) {
+//         match self {
+//             Either::A(a) => a.exit(span),
+//             Either::B(b) => b.exit(span),
+//         }
+//     }
 
-    fn close(&self, _span: &SpanRef) {}
+//     fn close(&self, span: &SpanRef) {
+//         match self {
+//             Either::A(a) => a.close(span),
+//             Either::B(b) => b.close(span),
+//         }
+//     }
+// }
 
-    fn filter(&self) -> &Filter {
-        self
-    }
-}
+// impl<A, B> Filter for Either<A, B>
+// where
+//     A: Observe,
+//     B: Observe,
+// {
+//     fn enabled(&self, metadata: &Metadata) -> bool {
+//         match self {
+//             Either::A(a) => a.filter().enabled(metadata),
+//             Either::B(b) => b.filter().enabled(metadata),
+//         }
+//     }
 
-impl Filter for NoObserver {
-    fn enabled(&self, _metadata: &Metadata) -> bool {
-        false
-    }
+//     fn should_invalidate_filter(&self, metadata: &Metadata) -> bool {
+//         match self {
+//             Either::A(a) => a.filter().should_invalidate_filter(metadata),
+//             Either::B(b) => b.filter().should_invalidate_filter(metadata),
+//         }
+//     }
+// }
 
-    fn should_invalidate_filter(&self, _metadata: &Metadata) -> bool {
-        false
-    }
-}
+// impl<S: SpanRef> Observe<S> for NoObserver {
+//     fn event<'a>(&self, _event: &'a Event<'a>) {}
+
+//     fn enter(&self, _span: &SpanRef) {}
+
+//     fn exit(&self, _span: &SpanRef) {}
+
+//     fn close(&self, _span: &SpanRef) {}
+
+//     fn filter(&self) -> &Filter {
+//         self
+//     }
+// }
+
+// impl Filter for NoObserver {
+//     fn enabled(&self, _metadata: &Metadata) -> bool {
+//         false
+//     }
+
+//     fn should_invalidate_filter(&self, _metadata: &Metadata) -> bool {
+//         false
+//     }
+// }
