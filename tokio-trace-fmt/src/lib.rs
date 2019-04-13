1387618281d21eb640213ae6b18ca35d5e935665
Jane Lusby <jlusby42@gmail.com>
2019-04-13 11:37:40 -0700

    fix: address clippy warnings

--- tokio-trace-fmt/src/lib.rs
+++ tokio-trace-fmt/src/lib.rs
@@ -277,6 +277,7 @@ impl<N, E, F> Builder<N, E, F> {
 
     /// Sets the subscriber being built to use the default full span formatter.
     // TODO: this should probably just become the default.
+    #[allow(clippy::type_complexity)]
     pub fn full(
         self,
     ) -> Builder<N, fn(&span::Context<N>, &mut fmt::Write, &Event) -> fmt::Result, F>
