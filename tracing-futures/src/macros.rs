508c2218a8d0818e326805870700f0f5637c7349
Eliza Weisman <eliza@buoyant.io>
2019-05-24 11:18:06 -0700

    futures: Support all metadata combinations in `spawn!`
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-futures/src/macros.rs
+++ tracing-futures/src/macros.rs
@@ -12,20 +12,43 @@ pub use tracing::{span as __span, Level as __Level};
 #[cfg(any(feature = "tokio", feature = "tokio-executor"))]
 #[macro_export(inner_local_macros)]
 macro_rules! spawn {
-    (level: $lvl:expr, target: $tgt:expr, name: $name:expr, $fut:expr) => {
-        spawn!(level: $lvl, target: $tgt, name: $name, $fut,)
-    };
     (level: $lvl:expr, target: $tgt:expr, name: $name:expr, $fut:expr, $($field:tt)*) => {{
         use $crate::macros::__spawn;
         use $crate::Instrument;
         let fut = Box::new($fut.instrument($crate::macros::__span!($lvl, target: $tgt, $name, $($field)*)));
         __spawn(fut)
     }};
+    (level: $lvl:expr, name: $name:expr, $fut:expr, $($field:tt)*) => {
+        spawn!(
+            level: $lvl,
+            target: __tokio_trace_futures_module_path!(),
+            name: $name,
+            $fut,
+            $($field)*
+        )
+    };
+    (level: $lvl:expr, target: $tgt:expr, $fut:expr, $($field:tt)*) => {
+        spawn!(
+            level: $lvl,
+            target: $tgt,
+            name: __tokio_trace_futures_stringify!($fut),
+            $fut,
+            $($field)*
+        )
+    };
     (target: $tgt:expr, name: $name:expr, $fut:expr, $($field:tt)*) => {
         spawn!(
             level: $crate::macros::__Level::TRACE,
             target: $tgt,
             name: $name,
+            $fut,
+            $($field)*
+        )
+    };
+    (target: $tgt:expr, $fut:expr, $($field:tt)*) => {
+        spawn!(
+            level: $crate::macros::__Level::TRACE,
+            target: $tgt,
             $fut,
             $($field)*
         )
@@ -41,12 +64,23 @@ macro_rules! spawn {
     ($fut:expr, $($field:tt)*) => {
         spawn!(name: __tracing_futures_stringify!($fut), $fut, $($field)*)
     };
+    (level: $lvl:expr, target: $tgt:expr, name: $name:expr, $fut:expr) => {
+        spawn!(level: $lvl, target: $tgt, name: $name, $fut,)
+    };
+    (level: $lvl:expr, name: $name:expr, $fut:expr) => {
+        spawn!(level: $lvl, name: $name, $fut,)
+    };
+    (level: $lvl:expr, target: $tgt:expr, $fut:expr) => {
+        spawn!(level: $lvl, target: $tgt, $fut,)
+    };
     (target: $tgt:expr, name: $name:expr, $fut:expr) => {
         spawn!(target: $tgt, name: $name, $fut,)
     };
+    (target: $tgt:expr, $fut:expr) => {
+        spawn!(target: $tgt, $fut,)
+    };
     (name: $name:expr, $fut:expr) => {
-        spawn!(name: $name, $fut
-        )
+        spawn!(name: $name, $fut,)
     };
     ($fut:expr) => {
         spawn!($fut,)
