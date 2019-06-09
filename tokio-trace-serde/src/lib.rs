38076a901ab29a83e2c19231d49ace9e66aa325d
DebugSteven <debugsteven@gmail.com>
2019-06-09 08:30:33 -0600

    Add/fix 'Serialize' impls for 'Field', 'FieldSet', 'Metadata', 'Event', 'Attributes', & 'Level'

--- tokio-trace-serde/src/lib.rs
+++ tokio-trace-serde/src/lib.rs
@@ -4,27 +4,93 @@ use serde::{
     Serialize,
     ser::{
         SerializeMap,
+        SerializeSeq,
         SerializeStruct,
         Serializer,
     },
 };
 
-use tokio_trace::{
+use tokio_trace_core::{
     event::Event,
-    field::{Field, Visit},
-    metadata::Metadata,
-    span::{Attributes, Record},
+    field::{Field, FieldSet, Visit},
+    metadata::{Level, Metadata},
+    span::{Attributes, Id, Record},
 };
 
 #[derive(Debug)]
-pub struct FieldVisitor(pub (crate) Field);
+pub struct SerializeField(pub (crate) Field);
 
-impl Serialize for FieldVisitor {
+impl Serialize for SerializeField {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
-        let mut state = serializer.serialize_struct("Field", 1)?;
-        state.serialize_field("name", self.0.name())?;
-        state.end()
+        serializer.serialize_str(self.0.name())
+    }
+}
+
+#[derive(Debug)]
+pub struct SerializeFieldSet<'a>(pub (crate) &'a FieldSet);
+
+impl<'a> Serialize for SerializeFieldSet<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+        let mut seq = serializer.serialize_seq(Some(self.0.len()))?;
+        for element in self.0 {
+            seq.serialize_element(element.name())?;
+        }
+        seq.end()
+    }
+}
+
+#[derive(Debug)]
+pub struct SerializeLevel<'a>(pub (crate) &'a Level);
+
+impl<'a> Serialize for SerializeLevel<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer {
+
+            if self.0 == &Level::ERROR {
+                serializer.serialize_str("ERROR")
+            }
+
+            else if self.0 == &Level::WARN {
+                serializer.serialize_str("WARN")
+            }
+
+            else if self.0 == &Level::INFO {
+                serializer.serialize_str("INFO")
+            }
+
+            else if self.0 == &Level::DEBUG {
+                serializer.serialize_str("DEBUG")
+            }
+
+            else if self.0 == &Level::TRACE {
+                serializer.serialize_str("TRACE")
+            }
+
+            else {
+                unreachable!()
+            }
+    }
+}
+
+#[derive(Debug)]
+pub struct SerializeId<'a>(pub (crate) &'a Id);
+
+impl<'a> Serialize for SerializeId<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+        unimplemented!()
+        //let mut state = serializer.serialize_struct("Metadata", 7)?;
+        //state.serialize_field("name", self.0.name())?;
+        //state.serialize_field("target", self.0.target())?;
+        //state.serialize_field("level", &SerializeLevel(self.0.level()))?;
+        //state.serialize_field("module_path", &self.0.module_path())?;
+        //state.serialize_field("fields", &SerializeFieldSet(self.0.fields()))?;
+        //state.serialize_field("file", &self.0.file())?;
+        //state.serialize_field("line", &self.0.line())?;
+        //// TODO `is_span()` and `is_event()`
+        //state.end()
     }
 }
 
@@ -34,23 +100,33 @@ pub struct SerializeMetadata<'a>(pub (crate) &'a Metadata<'a>);
 impl<'a> Serialize for SerializeMetadata<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
-        let mut state = serializer.serialize_struct("Metadata", 2)?;
-        state.serialize_field("name", self.0.name)?;
-        state.serialize_field("target", self.0.target)?;
+        let mut state = serializer.serialize_struct("Metadata", 7)?;
+        state.serialize_field("name", self.0.name())?;
+        state.serialize_field("target", self.0.target())?;
+        state.serialize_field("level", &SerializeLevel(self.0.level()))?;
+        state.serialize_field("module_path", &self.0.module_path())?;
+        state.serialize_field("fields", &SerializeFieldSet(self.0.fields()))?;
+        state.serialize_field("file", &self.0.file())?;
+        state.serialize_field("line", &self.0.line())?;
+        // TODO `is_span()` and `is_event()`
         state.end()
     }
 }
 
-/// A Serde visitor to pull `Event` data out of a serialized stream
+/// Implements `serde::Serialize` to write `Event` data to a serializer.
 #[derive(Debug)]
 pub struct SerializeEvent<'a>(pub (crate) Event<'a>);
 
 impl<'a> Serialize for SerializeEvent<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
-        let mut state = serializer.serialize_struct("SerializeEvent", 2)?;
-        state.serialize_field("metadata", &SerializeMetadata(self.0.metadata()))?;
-        state.end()
+        let serializer = serializer.serialize_struct("Event", 2)?;
+        let mut visitor = SerdeStructVisitor {
+                            serializer,
+                            state: Ok(()),
+                          };
+        self.0.record(&mut visitor);
+        visitor.finish()
     }
 }
 
@@ -61,22 +137,17 @@ pub struct SerializeAttributes<'a>(pub (crate) Attributes<'a>);
 impl<'a> Serialize for SerializeAttributes<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
-        let mut state = serializer.serialize_struct("SerializeAttributes", 3)?;
-        state.serialize_field("metadata", &SerializeMetadata(self.0.metadata()))?;
-        if let Some(id) = self.0.parent() {
-            // TODO this probably isn't how we want it
-            state.serialize_field("parent", &format!("Explicit: {:?}", id))?;
-        }
+        let mut serializer = serializer.serialize_struct("Attributes", 3)?;
+        serializer.serialize_field("metadata", &SerializeMetadata(self.0.metadata()))?;
+        serializer.serialize_field("parent", &self.0.parent().map(SerializeId))?;
+        serializer.serialize_field("is_root", &self.0.is_root())?;
 
-        else if self.0.is_root() {
-            state.serialize_field("parent", "Root")?;
-        }
-
-        else {
-            state.serialize_field("parent", "Current")?;
-        }
-
-        state.end()
+        let mut visitor = SerdeStructVisitor {
+                            serializer,
+                            state: Ok(()),
+                          };
+        self.0.record(&mut visitor);
+        visitor.finish()
     }
 }
 
