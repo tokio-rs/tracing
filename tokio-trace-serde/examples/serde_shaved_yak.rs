45383d86d3c3651bf7f9874cff8ac339aaf36cab
DebugSteven <debugsteven@gmail.com>
2019-06-19 12:52:50 -0600

    move subscriber to top

--- tokio-trace-serde/examples/serde_shaved_yak.rs
+++ tokio-trace-serde/examples/serde_shaved_yak.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate serde_json;
 
 #[macro_use]
@@ -18,56 +19,6 @@ use tokio_trace_core::{
 };
 
 use tokio_trace_serde::AsSerde;
-
-fn shave(yak: usize) -> bool {
-    span!(Level::TRACE, "shave", yak = yak).enter(|| {
-        debug!(
-            message = "hello! I'm gonna shave a yak.",
-            excitement = "yay!"
-        );
-        if yak == 3 {
-            warn!(target: "yak_events", "could not locate yak!");
-            false
-        } else {
-            trace!(target: "yak_events", "yak shaved successfully");
-            true
-        }
-    })
-}
-
-fn main() {
-    let subscriber = JsonSubscriber {
-        next_id: AtomicUsize::new(1),
-    };
-
-    tokio_trace::subscriber::with_default(subscriber, || {
-        let number_of_yaks = 3;
-        let mut number_shaved = 0;
-        debug!("preparing to shave {} yaks", number_of_yaks);
-
-        span!(Level::TRACE, "shaving_yaks", yaks_to_shave = number_of_yaks).enter(|| {
-            info!("shaving yaks");
-
-            for yak in 1..=number_of_yaks {
-                let shaved = shave(yak);
-                trace!(target: "yak_events", yak = yak, shaved = shaved);
-
-                if !shaved {
-                    error!(message = "failed to shave yak!", yak = yak);
-                } else {
-                    number_shaved += 1;
-                }
-
-                trace!(target: "yak_events", yaks_shaved = number_shaved);
-            }
-        });
-
-        debug!(
-            message = "yak shaving completed.",
-            all_yaks_shaved = number_shaved == number_of_yaks,
-        );
-    });
-}
 
 pub struct JsonSubscriber {
     next_id: AtomicUsize, // you need to assign span IDs, so you need a counter
@@ -133,4 +84,54 @@ impl Subscriber for JsonSubscriber {
         });
         println!("{}", json);
     }
+}
+
+fn shave(yak: usize) -> bool {
+    span!(Level::TRACE, "shave", yak = yak).enter(|| {
+        debug!(
+            message = "hello! I'm gonna shave a yak.",
+            excitement = "yay!"
+        );
+        if yak == 3 {
+            warn!(target: "yak_events", "could not locate yak!");
+            false
+        } else {
+            trace!(target: "yak_events", "yak shaved successfully");
+            true
+        }
+    })
+}
+
+fn main() {
+    let subscriber = JsonSubscriber {
+        next_id: AtomicUsize::new(1),
+    };
+
+    tokio_trace::subscriber::with_default(subscriber, || {
+        let number_of_yaks = 3;
+        let mut number_shaved = 0;
+        debug!("preparing to shave {} yaks", number_of_yaks);
+
+        span!(Level::TRACE, "shaving_yaks", yaks_to_shave = number_of_yaks).enter(|| {
+            info!("shaving yaks");
+
+            for yak in 1..=number_of_yaks {
+                let shaved = shave(yak);
+                trace!(target: "yak_events", yak = yak, shaved = shaved);
+
+                if !shaved {
+                    error!(message = "failed to shave yak!", yak = yak);
+                } else {
+                    number_shaved += 1;
+                }
+
+                trace!(target: "yak_events", yaks_shaved = number_shaved);
+            }
+        });
+
+        debug!(
+            message = "yak shaving completed.",
+            all_yaks_shaved = number_shaved == number_of_yaks,
+        );
+    });
 }
