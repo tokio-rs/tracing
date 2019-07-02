4cbe3c9037c32ec2f8cba7d4639b41378319b4f7
Eliza Weisman <eliza@buoyant.io>
2019-07-02 11:20:49 -0700

    add filter combinators
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/filter.rs
+++ tracing-subscriber/src/filter.rs
@@ -1,6 +1,7 @@
 
 use crate::layer::Layer;
 use tracing_core::{subscriber::Interest, Metadata};
+
 pub trait Filter: 'static {
     fn callsite_enabled(&self, metadata: &'static Metadata<'static>) -> Interest {
         if self.enabled(metadata) {
@@ -13,11 +14,39 @@ pub trait Filter: 'static {
     fn enabled(&self, metadata: &Metadata) -> bool;
 }
 
+pub trait FilterExt: Filter + crate::sealed::Sealed {
+    fn or<B>(self, b: B) -> Or<Self, B>
+    where
+        Self: Sized,
+    {
+        Or { a: self, b }
+    }
+
+    fn and<B>(self, b: B) -> And<Self, B>
+    where
+        Self: Sized,
+    {
+        And { a: self, b }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct EnabledFn<F>(F);
 
 #[derive(Clone, Debug)]
 pub struct InterestFn<F>(F);
+
+#[derive(Clone, Debug)]
+pub struct Or<A, B> {
+    a: A,
+    b: B,
+}
+
+#[derive(Clone, Debug)]
+pub struct And<A, B> {
+    a: A,
+    b: B,
+}
 
 pub fn enabled_fn<F>(f: F) -> EnabledFn<F>
 where
@@ -92,5 +121,56 @@ where
 {
     fn from(f: F) -> Self {
         Self(f)
+    }
+}
+
+// === impl FilterExt ===
+
+impl<F: Filter> crate::sealed::Sealed for F {}
+
+impl<F: Filter + crate::sealed::Sealed> FilterExt for F {}
+
+// === impl And ===
+
+impl<A, B> Filter for And<A, B>
+where
+    A: Filter,
+    B: Filter,
+{
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.a.enabled(metadata) && self.b.enabled(metadata)
+    }
+
+    fn callsite_enabled(&self, metadata: &'static Metadata<'static>) -> Interest {
+        let a = self.a.callsite_enabled(metadata);
+        let b = self.b.callsite_enabled(metadata);
+        match () {
+            _ if a.is_sometimes() && b.is_sometimes() => Interest::sometimes(),
+            _ if a.is_always() && !b.is_never() => Interest::always(),
+            _ if b.is_always() && !a.is_never() => Interest::always(),
+            _ => Interest::never(),
+        }
+    }
+}
+
+// === impl Or ===
+
+impl<A, B> Filter for Or<A, B>
+where
+    A: Filter,
+    B: Filter,
+{
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.a.enabled(metadata) || self.b.enabled(metadata)
+    }
+
+    fn callsite_enabled(&self, metadata: &'static Metadata<'static>) -> Interest {
+        let a = self.a.callsite_enabled(metadata);
+        let b = self.b.callsite_enabled(metadata);
+        match () {
+            _ if a.is_always() || b.is_always() => Interest::always(),
+            _ if a.is_sometimes() || b.is_sometimes() => Interest::sometimes(),
+            _ => Interest::never(),
+        }
     }
 }
