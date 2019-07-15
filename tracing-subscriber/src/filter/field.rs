5b4f12c76861984cfb004b563808cfdca4503849
Eliza Weisman <eliza@buoyant.io>
2019-07-15 16:58:39 -0700

    better field filters
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/filter/field.rs
+++ tracing-subscriber/src/filter/field.rs
@@ -1,8 +1,9 @@
 use matchers::Pattern;
-use std::str::FromStr;
+use std::{error::Error, fmt, str::FromStr};
 
-pub struct Directive {
-    span: Option<&'static str>,
+pub struct FieldMatch {
+    name: String, // TODO: allow match patterns for names?
+    value: Option<ValueMatch>,
 }
 
 enum ValueMatch {
@@ -12,14 +13,46 @@ enum ValueMatch {
     Pat(Pattern),
 }
 
+#[derive(Clone, Debug)]
+struct BadName {
+    name: String,
+}
+
+// === impl FieldMatch ===
+
+impl FromStr for FieldMatch {
+    type Err = Box<dyn Error + Send + Sync>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split('=');
+        let name = parts
+            .next()
+            .ok_or_else(|| BadName {
+                name: "".to_string(),
+            })?
+            // TODO: validate field name
+            .to_string();
+        let value = parts.next().map(ValueMatch::from_str).transpose()?;
+        Ok(FieldMatch { name, value })
+    }
+}
+
 // === impl ValueMatch ===
 
 impl FromStr for ValueMatch {
     type Err = matchers::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.parse::<bool>().map(ValueMatch::Bool)
+        s.parse::<bool>()
+            .map(ValueMatch::Bool)
             .or_else(|_| s.parse::<u64>().map(ValueMatch::U64))
             .or_else(|_| s.parse::<i64>().map(ValueMatch::I64))
             .or_else(|_| s.parse::<Pattern>().map(ValueMatch::Pat))
+    }
+}
+
+impl Error for BadName {}
+
+impl fmt::Display for BadName {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid field name `{}`", self.name)
     }
 }
