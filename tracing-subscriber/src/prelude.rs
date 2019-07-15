081faf0fcb890f6252baef35a99b90cf18d8828e
Eliza Weisman <eliza@buoyant.io>
2019-07-15 14:33:46 -0700

    remove filter trait from this branch
    it needs to be reworked & will be added separately
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/prelude.rs
+++ tracing-subscriber/src/prelude.rs
@@ -1,4 +1,3 @@
-pub use crate::filter::FilterExt as __tracing_subscriber_FilterExt;
 pub use crate::layer::{
     Layer as __tracing_subscriber_Layer, SubscriberExt as __tracing_subscriber_SubscriberExt,
 };
