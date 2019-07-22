d1ab9dc39332df70b0615c79149edb216433337e
Eliza Weisman <eliza@buoyant.io>
2019-07-22 10:54:46 -0700

    tower: Rewrite the tower support crate (#198)
    ## Motivation
    
    The `tracing-tower` crate for compatibility with `tower` is currently
    pretty half-baked and incomplete, and doesn't conver a majority of
    use-cases.
    
    ## Solution
    
    This branch completely rewrites the `tracing-tower` support crate,
    hopefully making it more useful. 
    
    We now provide a set of `request_span` middleware for instrumenting a
    service so that each request passing through it is instrumented with its
    own span, and a set of `service_span` metadata that instruments the
    entire service with a single span. Additionally, there are now
    implementations of `Layer` and `MakeService`, when the `tower-layer` and
    `tower-util` crates are also in use. Finally, I've reworked the
    `InstrumentableService` extension trait a bit to make it easier to
    implement services in different ways, although the old behaviour of
    `InstrumentableService::instrument` is unchanged (I've heard
    @LucioFranco is using this in prod...).
    
    Most of the functionality of the `tracing-tower-http` crate can now be
    reimplemented using the new `tracing-tower` crate (and a feature-flagged
    `http` dependency), so that crate should _probably_ now be deprecated.
    I've reimplemented its example using `tracing-tower`.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ tracing-tower/examples/h2-server.rs
@@ -0,0 +1,150 @@
+use bytes::{Bytes, IntoBuf};
+use futures::*;
+use http::Request;
+use tokio::executor::DefaultExecutor;
+use tokio::net::TcpListener;
+use tower_h2::{Body, RecvBody, Server};
+use tower_service::Service;
+use tracing_futures::Instrument;
+use tracing_tower::InstrumentMake;
+
+type Response = http::Response<RspBody>;
+
+struct RspBody(Option<Bytes>);
+
+impl RspBody {
+    fn new(body: Bytes) -> Self {
+        RspBody(Some(body))
+    }
+
+    fn empty() -> Self {
+        RspBody(None)
+    }
+}
+
+impl Body for RspBody {
+    type Data = <Bytes as IntoBuf>::Buf;
+    type Error = h2::Error;
+
+    fn poll_data(&mut self) -> Poll<Option<Self::Data>, Self::Error> {
+        let data = self.0.take().and_then(|b| {
+            if b.is_empty() {
+                None
+            } else {
+                Some(b.into_buf())
+            }
+        });
+        Ok(Async::Ready(data))
+    }
+
+    fn poll_trailers(&mut self) -> Poll<Option<http::HeaderMap>, Self::Error> {
+        Ok(None.into())
+    }
+}
+
+const ROOT: &'static str = "/";
+
+#[derive(Debug)]
+struct Svc;
+impl Service<Request<RecvBody>> for Svc {
+    type Response = Response;
+    type Error = h2::Error;
+    type Future = future::FutureResult<Response, Self::Error>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        Ok(Async::Ready(()))
+    }
+
+    fn call(&mut self, req: Request<RecvBody>) -> Self::Future {
+        tracing::trace!(message = "received request", request.headers = ?req.headers());
+        let mut rsp = http::Response::builder();
+        rsp.version(http::Version::HTTP_2);
+
+        let uri = req.uri();
+        let rsp = if uri.path() != ROOT {
+            let body = RspBody::empty();
+            tracing::warn!(rsp.error = %"unrecognized path", request.path = ?uri.path());
+            rsp.status(404).body(body).unwrap()
+        } else {
+            let body = RspBody::new("heyo!".into());
+            rsp.status(200).body(body).unwrap()
+        };
+
+        tracing::debug!(rsp.status = %rsp.status(), message = "sending response...");
+        future::ok(rsp)
+    }
+}
+
+#[derive(Debug)]
+struct NewSvc;
+impl tower_service::Service<()> for NewSvc {
+    type Response = Svc;
+    type Error = ::std::io::Error;
+    type Future = future::FutureResult<Svc, Self::Error>;
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        Ok(Async::Ready(()))
+    }
+
+    fn call(&mut self, _target: ()) -> Self::Future {
+        future::ok(Svc)
+    }
+}
+
+fn main() {
+    // Set the default subscriber to record all traces emitted by this example
+    // and by the `tracing_tower` library's helpers.
+    let subscriber = tracing_fmt::FmtSubscriber::builder()
+        .with_filter(tracing_fmt::filter::EnvFilter::from(
+            "h2_server=trace,tracing_tower=trace",
+        ))
+        .finish();
+    let _ = tracing::subscriber::set_global_default(subscriber);
+
+    let addr = "[::1]:8888".parse().unwrap();
+    let bind = TcpListener::bind(&addr).expect("bind");
+
+    // Construct a span for the server task, annotated with the listening IP
+    // address and port.
+    let span = tracing::trace_span!("server", ip = %addr.ip(), port = addr.port());
+
+    let server = lazy(|| {
+        let executor = DefaultExecutor::current();
+
+        // Enrich the `MakeService` with a wrapper so that each request is
+        // traced with its own span.
+        let new_svc = NewSvc.with_traced_requests(tracing_tower::http::debug_request);
+        let h2 = Server::new(new_svc, Default::default(), executor);
+
+        tracing::info!("listening");
+
+        bind.incoming()
+            .fold(h2, |mut h2, sock| {
+                // Construct a new span for each accepted connection.
+                let addr = sock.peer_addr().expect("can't get addr");
+                let span = tracing::trace_span!("conn", ip = %addr.ip(), port = addr.port());
+                let _enter = span.enter();
+
+                tracing::debug!("accepted connection");
+
+                if let Err(e) = sock.set_nodelay(true) {
+                    return Err(e);
+                }
+
+                let serve = h2
+                    .serve(sock)
+                    .map_err(|error| tracing::error!(message = "h2 error", %error))
+                    .map(|_| {
+                        tracing::trace!("finished serving connection");
+                    })
+                    .instrument(span.clone());
+                tokio::spawn(serve);
+
+                Ok(h2)
+            })
+            .map_err(|error| tracing::error!(message = "serve error", %error))
+            .map(|_| {})
+    })
+    .instrument(span);
+
+    tokio::run(server);
+}
--- tracing-tower/src/lib.rs
+++ tracing-tower/src/lib.rs
@@ -1,44 +1,96 @@
-extern crate tower_service;
-#[macro_use]
-extern crate tracing;
-extern crate futures;
-extern crate tracing_futures;
-
 use std::fmt;
 use tower_service::Service;
 use tracing::Level;
-use tracing_futures::{Instrument, Instrumented};
 
-#[derive(Clone, Debug)]
-pub struct InstrumentedService<T> {
-    inner: T,
-    span: tracing::Span,
-}
+pub mod request_span;
+pub mod service_span;
 
-pub trait InstrumentableService<Request>: Service<Request> + Sized {
-    fn instrument(self, span: tracing::Span) -> InstrumentedService<Self> {
-        InstrumentedService { inner: self, span }
-    }
-}
+#[cfg(feature = "http")]
+pub mod http;
 
-impl<T: Service<Request>, Request> Service<Request> for InstrumentedService<T>
+pub type InstrumentedService<S, R> = service_span::Service<request_span::Service<S, R>>;
+
+pub trait InstrumentableService<Request>
 where
-    // TODO: it would be nice to do more for HTTP services...
-    Request: fmt::Debug + Clone + Send + Sync + 'static,
+    Self: Service<Request> + Sized,
 {
-    type Response = T::Response;
-    type Error = T::Error;
-    type Future = Instrumented<T::Future>;
-
-    fn poll_ready(&mut self) -> futures::Poll<(), Self::Error> {
-        let _enter = self.span.enter();
-        self.inner.poll_ready()
+    fn instrument<G>(self, svc_span: G) -> InstrumentedService<Self, Request>
+    where
+        G: GetSpan<Self>,
+        Request: fmt::Debug,
+    {
+        let req_span: fn(&Request) -> tracing::Span =
+            |request| tracing::span!(Level::TRACE, "request", ?request);
+        let svc_span = svc_span.span_for(&self);
+        self.trace_requests(req_span).trace_service(svc_span)
     }
 
-    fn call(&mut self, req: Request) -> Self::Future {
-        // TODO: custom `Value` impls for `http` types would be nice...
-        let span = span!(parent: &self.span, Level::TRACE, "request", request = ?req);
-        let _enter = span.enter();
-        self.inner.call(req).instrument(span.clone())
+    fn trace_requests<G>(self, get_span: G) -> request_span::Service<Self, Request, G>
+    where
+        G: GetSpan<Request> + Clone,
+    {
+        request_span::Service::new(self, get_span)
     }
+
+    fn trace_service<G>(self, get_span: G) -> service_span::Service<Self>
+    where
+        G: GetSpan<Self>,
+    {
+        let span = get_span.span_for(&self);
+        service_span::Service::new(self, span)
+    }
+}
+
+#[cfg(feature = "tower-util")]
+pub trait InstrumentMake<T, R>
+where
+    Self: tower_util::MakeService<T, R> + Sized,
+{
+    fn with_traced_service<G>(self, get_span: G) -> service_span::MakeService<Self, T, R, G>
+    where
+        G: GetSpan<T>,
+    {
+        service_span::MakeService::new(self, get_span)
+    }
+
+    fn with_traced_requests<G>(self, get_span: G) -> request_span::MakeService<Self, R, G>
+    where
+        G: GetSpan<R> + Clone,
+    {
+        request_span::MakeService::new(self, get_span)
+    }
+}
+
+impl<S, R> InstrumentableService<R> for S where S: Service<R> + Sized {}
+
+#[cfg(feature = "tower-util")]
+impl<M, T, R> InstrumentMake<T, R> for M where M: tower_util::MakeService<T, R> {}
+
+pub trait GetSpan<T>: crate::sealed::Sealed<T> {
+    fn span_for(&self, target: &T) -> tracing::Span;
+}
+
+impl<T, F> crate::sealed::Sealed<T> for F where F: Fn(&T) -> tracing::Span {}
+
+impl<T, F> GetSpan<T> for F
+where
+    F: Fn(&T) -> tracing::Span,
+{
+    #[inline]
+    fn span_for(&self, target: &T) -> tracing::Span {
+        (self)(target)
+    }
+}
+
+impl<T> crate::sealed::Sealed<T> for tracing::Span {}
+
+impl<T> GetSpan<T> for tracing::Span {
+    #[inline]
+    fn span_for(&self, _: &T) -> tracing::Span {
+        self.clone()
+    }
+}
+
+mod sealed {
+    pub trait Sealed<T = ()> {}
 }
