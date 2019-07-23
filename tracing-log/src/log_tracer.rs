d3ff7258ecec79e4e6149c08a680f151737c8286
Eliza Weisman <eliza@buoyant.io>
2019-07-23 14:14:37 -0700

    log: allow ignoring crates
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-log/src/log_tracer.rs
+++ tracing-log/src/log_tracer.rs
@@ -13,15 +13,42 @@ use tracing_core::dispatcher;
 /// [`init_with_filter`]: ../fn.init_with_filter.html
 #[derive(Debug)]
 pub struct LogTracer {
-    _p: (),
+    ignore_crates: Box<[String]>,
+}
+
+#[derive(Debug)]
+pub struct Builder {
+    ignore_crates: Vec<String>,
+    filter: log::LevelFilter,
 }
 
 // ===== impl LogTracer =====
 
-// Static logger for `LogTrace`
-static LOGGER: LogTracer = LogTracer { _p: () };
-
 impl LogTracer {
+    /// Returns a builder that allows customizing a `LogTracer` and setting it
+    /// the default logger.
+    ///
+    /// For example:
+    /// ```rust
+    /// # use std::error::Error;
+    /// use tracing_log::LogTracer;
+    /// use log;
+    ///
+    /// # fn main() -> Result<(), Box<Error>> {
+    /// LogTracer::builder()
+    ///     .ignore_crate("foo") // suppose the `foo` crate is using `tracing`'s log feature
+    ///     .with_max_level(log::Level::Info)
+    ///     .init()?;
+    ///
+    /// // will be available for Subscribers as a tracing Event
+    /// log::info!("an example info log");
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn builder() -> Builder {
+        Builder::default()
+    }
+
     /// Creates a new `LogTracer` that can then be used as a logger for the `log` crate.
     ///
     /// It is generally simpler to use the [`init`] or [`init_with_filter`] methods
@@ -48,17 +75,20 @@ impl LogTracer {
     /// [`init`]: #method.init
     /// [`init_with_filter`]: .#method.init_with_filter
     pub fn new() -> Self {
-        Self { _p: () }
+        Self { ignore_crates: Vec::new().into_boxed_slice() }
     }
 
     /// Sets up `LogTracer` as global logger for the `log` crate,
     /// with the given level as max level filter.
     ///
     /// Setting a global logger can only be done once.
+    ///
+    /// The [`builder`] function can be used to customize the `LogTracer` before
+    /// initializing it.
+    ///
+    /// [`builder`]: #method.builder
     pub fn init_with_filter(level: log::LevelFilter) -> Result<(), log::SetLoggerError> {
-        log::set_logger(&LOGGER)?;
-        log::set_max_level(level);
-        Ok(())
+        Self::builder().with_max_level(level).init()
     }
 
     /// Sets a `LogTracer` as the global logger for the `log` crate.
@@ -80,12 +110,18 @@ impl LogTracer {
     /// ```
     ///
     /// This will forward all logs to `tracing` and lets the current `Subscriber`
-    /// determine if they are enabled. If you know in advance you want to filter some log levels,
-    /// use [`init_with_filter`] instead.
+    /// determine if they are enabled.
+    ///
+    /// The [`builder`] function can be used to customize the `LogTracer` before
+    /// initializing it.
+    ///
+    /// If you know in advance you want to filter some log levels,
+    /// use [`builder`] or [`init_with_filter`] instead.
     ///
     /// [`init_with_filter`]: #method.init_with_filter
+    /// [`builder`]: #method.builder
     pub fn init() -> Result<(), log::SetLoggerError> {
-        Self::init_with_filter(log::LevelFilter::Trace)
+        Self::builder().init()
     }
 }
 
@@ -96,9 +132,15 @@ impl Default for LogTracer {
 }
 
 impl log::Log for LogTracer {
-    fn enabled(&self, _metadata: &log::Metadata) -> bool {
-        // Use log::set_max_level to filter LogTracing's input
-        true
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        if self.ignore_crates.is_empty() {
+            return true;
+        }
+
+        // If we are ignoring certain module paths, ensure that the metadata
+        // does not start with one of those paths.
+        let target = metadata.target();
+        !self.ignore_crates.iter().any(|ignored| target.starts_with(ignored))
     }
 
     fn log(&self, record: &log::Record) {
@@ -118,4 +160,68 @@ impl log::Log for LogTracer {
     }
 
     fn flush(&self) {}
+}
+
+// ===== impl Builder =====
+
+impl Builder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets a global maximum level for `log` records.
+    ///
+    /// Log records whose level is more verbose than the provided level will be
+    /// disabled.
+    ///
+    /// By default, all `log` records will be enabled.
+    pub fn with_max_level(self, filter: impl Into<log::LevelFilter>) -> Self {
+        let filter = filter.into();
+        Self { filter, ..self }
+    }
+
+    /// Configures the `LogTracer` to ignore all log records whose target
+    /// starts with the given string.
+    ///
+    /// This should be used when a crate enables the `tracing/log` feature to
+    /// emit log records for tracing events. Otherwise, those events will be
+    /// recorded twice.
+    pub fn ignore_crate(mut self, name: impl Into<String>) -> Self {
+        self.ignore_crates.push(name.into());
+        self
+    }
+
+    /// Configures the `LogTracer` to ignore all log records whose target
+    /// starts with any of the given the given strings.
+    ///
+    /// This should be used when a crate enables the `tracing/log` feature to
+    /// emit log records for tracing events. Otherwise, those events will be
+    /// recorded twice.
+    pub fn ignore_all<I>(self, crates: impl IntoIterator<Item = I>) -> Self
+    where
+        I: Into<String>,
+    {
+        crates.into_iter().fold(self, Self::ignore_crate)
+    }
+
+    /// Constructs a new `LogTracer` with the provided configuration and sets it
+    /// as the default logger.
+    ///
+    /// Setting a global logger can only be done once.
+    pub fn init(self) -> Result<(), log::SetLoggerError> {
+        let ignore_crates = self.ignore_crates.into_boxed_slice();
+        let logger = Box::new(LogTracer { ignore_crates });
+        log::set_boxed_logger(logger)?;
+        log::set_max_level(self.filter);
+        Ok(())
+    }
+}
+
+impl Default for Builder {
+    fn default() -> Self {
+        Self {
+            ignore_crates: Vec::new(),
+            filter: log::LevelFilter::max(),
+        }
+    }
 }
