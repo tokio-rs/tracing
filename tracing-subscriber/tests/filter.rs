aaebfc6e897c686ecddef31ebe16d02ee3c0cd0f
Eliza Weisman <eliza@buoyant.io>
2019-07-26 15:52:00 -0700

    more tests
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/tests/filter.rs
+++ tracing-subscriber/tests/filter.rs
@@ -117,11 +117,54 @@ fn field_filter_events() {
     let subscriber = subscriber.with(filter);
 
     with_default(subscriber, || {
-        tracing::trace!("this should be disabled");
+        tracing::trace!(disabled = true);
         tracing::info!("also disabled");
         tracing::info!(thing = 1);
         tracing::debug!(thing = 2);
         tracing::trace!(thing = 3);
+    });
+
+    finished.assert_finished();
+}
+
+#[test]
+fn field_filter_spans() {
+    let filter: Filter = "[{enabled=true}]=debug"
+        .parse()
+        .expect("filter should parse");
+    let (subscriber, finished) = subscriber::mock()
+        .enter(span::mock().named("span1"))
+        .event(
+            event::mock()
+                .at_level(Level::INFO)
+                .with_fields(field::mock("thing")),
+        )
+        .exit(span::mock().named("span1"))
+        .enter(span::mock().named("span2"))
+        .exit(span::mock().named("span2"))
+        .enter(span::mock().named("span3"))
+        .event(
+            event::mock()
+                .at_level(Level::DEBUG)
+                .with_fields(field::mock("thing")),
+        )
+        .exit(span::mock().named("span3"))
+        .done()
+        .run_with_handle();
+    let subscriber = subscriber.with(filter);
+
+    with_default(subscriber, || {
+        tracing::trace!("disabled");
+        tracing::info!("also disabled");
+        tracing::info_span!("span1", enabled = true).in_scope(|| {
+            tracing::info!(thing = 1);
+        });
+        tracing::debug_span!("span2", enabled = false, foo = "hi").in_scope(|| {
+            tracing::warn!(thing = 2);
+        });
+        tracing::trace_span!("span3", enabled = true, answer = 42).in_scope(|| {
+            tracing::debug!(thing = 2);
+        });
     });
 
     finished.assert_finished();
