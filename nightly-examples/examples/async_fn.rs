c739c7f2b4f367d97376b31ecced2ca8ff30b15e
Eliza Weisman <eliza@buoyant.io>
2019-08-07 10:54:58 -0700

    update examples
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- nightly-examples/examples/async_fn.rs
+++ nightly-examples/examples/async_fn.rs
@@ -1,5 +1,29 @@
+//! Demonstrates using the `trace` attribute macro to instrument `async`
+//! functions.
+//!
+//! This is based on the [`hello_world`] example from `tokio`.
+//!
+//! This server will create a TCP listener, accept connections in a loop, and
+//! write back everything that's read off of each TCP connection.
+//!
+//! Because the Tokio runtime uses a thread pool, each TCP connection is
+//! processed concurrently with all other TCP connections across multiple
+//! threads.
+//!
+//! To see this server in action, you can run this in one terminal:
+//!
+//!     cargo run --example echo
+//!
+//! and in another terminal you can run:
+//!
+//!     cargo run --example connect 127.0.0.1:8080
+//!
+//! Each line you type in to the `connect` terminal should be echo'd back to
+//! you! If you open up multiple terminals running the `connect` example you
+//! should be able to see them all make progress simultaneously.
+//!
+//! [`hello_world`]: https://github.com/tokio-rs/tokio/blob/132e9f1da5965530b63554d7a1c59824c3de4e30/tokio/examples/hello_world.rs
 #![feature(async_await)]
-
 use tokio;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
@@ -34,7 +58,5 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     // Note that this is the Tokio TcpStream, which is fully async.
     let mut stream = connect(&addr).await?;
 
-    write(&mut stream).await;
-
-    Ok(())
+    write(&mut stream).await
 }
