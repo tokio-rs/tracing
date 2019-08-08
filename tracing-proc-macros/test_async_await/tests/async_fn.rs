93565c5b9cf703a11a7a0ae92d936c9cd729660a
Eliza Weisman <eliza@buoyant.io>
2019-08-07 19:56:03 -0700

    proc-macros: add proper support for `async fn` (#244)
    ## Motivation
    
    The `tracing-proc-macros` crate provides an attribute macro that
    instruments a function. When an instrumented function is called, a span
    with the function's name and parameters is created and entered for the
    duration of the call. However, this does not currently handle 
    `async fn`s properly --- the correct behaviour would be to instrument
     the _future_ created by the async function.
    
    ## Solution
    
    This branch updates the attribute macro to properly handle async
    functions. This is done by expanding the function body inside of an
    async block, using the `tracing-futures::Instrument` combinator on that
    block, and `await`ing the instrumented block. This should function
    equivalently to the un-instrumented function, but with the addition that
    the `Instrumented` combinator future will ensure that the generated span
    is entered every time the async block's future is polled.
    
    Since we build and test `tracing` on stable Rust, a `nightly` feature
    flag is required to enable support for `async fn`s. If the attribute is
    added to an async fn without that feature enabled, we will generate a
    compiler error that indicates that this feature should be enabled.
    
    I've also added examples and tests for instrumenting `async fn`s. These
    are in their own separate workspaces so that they do not break 
    `cargo test --all` on stable.
    
    Closes #6 
    Fixes #162
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ tracing-proc-macros/test_async_await/tests/async_fn.rs
@@ -0,0 +1,36 @@
+#![feature(async_await)]
+
+use test_std_future::{
+    PollN,
+    block_on_future,
+    support::*,
+};
+
+use tokio_test::task::MockTask;
+use tracing::subscriber::with_default;
+
+#[tracing_proc_macros::trace]
+async fn test_async_fn(polls: usize) -> Result<(), ()> {
+    let future = PollN::new_ok(polls);
+    tracing::trace!(awaiting = true);
+    future.await
+}
+
+#[test]
+fn async_fn_only_enters_for_polls() {
+    let (subscriber, handle) = subscriber::mock()
+        .new_span(span::mock().named("test_async_fn"))
+        .enter(span::mock().named("test_async_fn"))
+        .event(event::mock().with_fields(field::mock("awaiting").with_value(&true)))
+        .exit(span::mock().named("test_async_fn"))
+        .enter(span::mock().named("test_async_fn"))
+        .exit(span::mock().named("test_async_fn"))
+        .drop_span(span::mock().named("test_async_fn"))
+        .done()
+        .run_with_handle();
+    let mut task = MockTask::new();
+    with_default(subscriber, || {
+        block_on_future(&mut task, async { test_async_fn(2).await }).unwrap();
+    });
+    handle.assert_finished();
+}
