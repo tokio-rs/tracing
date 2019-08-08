756e25d415535ed669085d3a8fb40c849ed72aaa
Eliza Weisman <eliza@buoyant.io>
2019-08-08 10:53:31 -0700

    proc-macros: add support for setting levels
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-proc-macros/src/lib.rs
+++ tracing-proc-macros/src/lib.rs
@@ -2,12 +2,16 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use quote::quote_spanned;
-use syn::{spanned::Spanned, ArgCaptured, FnArg, FnDecl, Ident, ItemFn, Pat, PatIdent};
+use quote::{quote, quote_spanned};
+use syn::{
+    spanned::Spanned, ArgCaptured, AttributeArgs, FnArg, FnDecl, Ident, ItemFn, Lit, Meta,
+    MetaNameValue, NestedMeta, Pat, PatIdent,
+};
 
 #[proc_macro_attribute]
-pub fn trace(_args: TokenStream, item: TokenStream) -> TokenStream {
+pub fn trace(args: TokenStream, item: TokenStream) -> TokenStream {
     let input: ItemFn = syn::parse_macro_input!(item as ItemFn);
+    let args = syn::parse_macro_input!(args as AttributeArgs);
     let call_site = Span::call_site();
 
     // these are needed ahead of time, as ItemFn contains the function body _and_
@@ -60,11 +64,13 @@ pub fn trace(_args: TokenStream, item: TokenStream) -> TokenStream {
         )
     };
 
+    let level = level(&args);
+
     quote_spanned!(call_site=>
         #(#attrs) *
         #vis #constness #unsafety #asyncness #abi fn #ident(#params) #return_type {
             let __tracing_attr_span = tracing::span!(
-                tracing::Level::TRACE,
+                #level,
                 #ident_str,
                 #(#param_names = tracing::field::debug(&#param_names_clone)),*
             );
@@ -72,6 +78,53 @@ pub fn trace(_args: TokenStream, item: TokenStream) -> TokenStream {
         }
     )
     .into()
+}
+
+fn level(args: &AttributeArgs) -> proc_macro2::TokenStream {
+    let mut levels = args.iter().filter_map(|arg| match arg {
+        NestedMeta::Meta(Meta::NameValue(MetaNameValue {
+            ref ident, ref lit, ..
+        })) if ident == "level" => Some(lit.clone()),
+        _ => None,
+    });
+    let level = levels.next();
+
+    // If we found more than one arg named "level", that's a syntax error...
+    if let Some(lit) = levels.next() {
+        return quote_spanned! {lit.span()=>
+            compile_error!("expected only a single `level` argument!")
+        };
+    }
+
+    match level {
+        Some(Lit::Str(ref lit)) if lit.value().eq_ignore_ascii_case("trace") => {
+            quote!(tracing::Level::TRACE)
+        }
+        Some(Lit::Str(ref lit)) if lit.value().eq_ignore_ascii_case("debug") => {
+            quote!(tracing::Level::DEBUG)
+        }
+        Some(Lit::Str(ref lit)) if lit.value().eq_ignore_ascii_case("info") => {
+            quote!(tracing::Level::INFO)
+        }
+        Some(Lit::Str(ref lit)) if lit.value().eq_ignore_ascii_case("warn") => {
+            quote!(tracing::Level::WARN)
+        }
+        Some(Lit::Str(ref lit)) if lit.value().eq_ignore_ascii_case("error") => {
+            quote!(tracing::Level::ERROR)
+        }
+        Some(Lit::Int(ref lit)) if lit.value() == 1 => quote!(tracing::Level::TRACE),
+        Some(Lit::Int(ref lit)) if lit.value() == 2 => quote!(tracing::Level::DEBUG),
+        Some(Lit::Int(ref lit)) if lit.value() == 3 => quote!(tracing::Level::INFO),
+        Some(Lit::Int(ref lit)) if lit.value() == 4 => quote!(tracing::Level::WARN),
+        Some(Lit::Int(ref lit)) if lit.value() == 5 => quote!(tracing::Level::ERROR),
+        Some(lit) => quote_spanned! {lit.span()=>
+            compile_error!(
+                "unknown verbosity level, expected one of \"trace\", \
+                 \"debug\", \"info\", \"warn\", or \"error\", or a number 1-5"
+            )
+        },
+        None => quote!(tracing::Level::INFO),
+    }
 }
 
 mod async_await {
