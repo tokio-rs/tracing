e6d9e8b4a105b9bc57d5b7f35d16cb23823aef59
Eliza Weisman <eliza@buoyant.io>
2019-08-07 19:56:03 -0700

    proc-macros: add proper support for `async fn` (#244)
    ## Motivation
    
    The `tracing-proc-macros` crate provides an attribute macro that
    instruments a function. When an instrumented function is called, a span
    with the function's name and parameters is created and entered for the
    duration of the call. However, this does not currently handle 
    `async fn`s properly --- the correct behaviour would be to instrument
     the _future_ created by the async function.
    
    ## Solution
    
    This branch updates the attribute macro to properly handle async
    functions. This is done by expanding the function body inside of an
    async block, using the `tracing-futures::Instrument` combinator on that
    block, and `await`ing the instrumented block. This should function
    equivalently to the un-instrumented function, but with the addition that
    the `Instrumented` combinator future will ensure that the generated span
    is entered every time the async block's future is polled.
    
    Since we build and test `tracing` on stable Rust, a `nightly` feature
    flag is required to enable support for `async fn`s. If the attribute is
    added to an async fn without that feature enabled, we will generate a
    compiler error that indicates that this feature should be enabled.
    
    I've also added examples and tests for instrumenting `async fn`s. These
    are in their own separate workspaces so that they do not break 
    `cargo test --all` on stable.
    
    Closes #6 
    Fixes #162
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ nightly-examples/examples/async_fn.rs
@@ -0,0 +1,58 @@
+//! Demonstrates using the `trace` attribute macro to instrument `async`
+//! functions.
+//!
+//! This is based on the [`hello_world`] example from `tokio`. and implements a
+//! simple client that opens a TCP stream, writes "hello world\n", and closes
+//! the connection.
+//!
+//! You can test this out by running:
+//!
+//!     ncat -l 6142
+//!
+//! And then in another terminal run:
+//!
+//!     cargo +nightly run --example async_fn
+//!
+//! [`hello_world`]: https://github.com/tokio-rs/tokio/blob/132e9f1da5965530b63554d7a1c59824c3de4e30/tokio/examples/hello_world.rs
+#![feature(async_await)]
+use tokio;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+use tracing::info;
+use tracing_proc_macros::trace;
+
+use std::{io, error::Error, net::SocketAddr};
+
+#[trace]
+async fn connect(addr: &SocketAddr) -> io::Result<TcpStream> {
+    let stream = TcpStream::connect(&addr).await;
+    tracing::info!("created stream");
+    stream
+}
+
+#[trace]
+async fn write(stream: &mut TcpStream) -> io::Result<usize> {
+    let result = stream.write(b"hello world\n").await;
+    info!("wrote to stream; success={:?}", result.is_ok());
+    result
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "127.0.0.1:6142".parse()?;
+
+    let subscriber = tracing_fmt::FmtSubscriber::builder()
+        .with_filter(tracing_fmt::filter::EnvFilter::from("async_fn=trace"))
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    // Open a TCP stream to the socket address.
+    //
+    // Note that this is the Tokio TcpStream, which is fully async.
+    let mut stream = connect(&addr).await?;
+
+    write(&mut stream).await?;
+
+    Ok(())
+}
---
+++ tracing-proc-macros/src/stable.rs
@@ -0,0 +1,18 @@
+/// Generates the instrumented body for an `async fn`.
+///
+/// # Arguments
+/// - `body`: the body of the `async fn`.
+/// - `err_span`: the callsite of the `trace` attribute macro, to emit global
+///   errors (if there are any).
+pub(crate) fn gen_async_body(
+    _body: Box<syn::Block>,
+    err_span: &proc_macro2::Span,
+) -> proc_macro2::TokenStream {
+    let span = err_span.clone();
+    quote_spanned! {span=>
+        compile_error!(
+            "to use `tracing-macros` with async functions, the \"nightly\" \
+             feature flag must be enabled."
+        )
+    }
+}
