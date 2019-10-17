1b3b798cf7d76226ee580917de7cb1578f6c8239
Eliza Weisman <eliza@buoyant.io>
2019-10-17 15:12:40 -0700

    subscriber: allow subscribers to expose stored metadata to layers (#382)
    ## Motivation
    
    Currently, when using `tracing-subscriber`'s `Layer` API, it is
    necessary for each layer to manage its own storage for per-span data.
    Since subscribers are shared concurrently, this means that `Layer`s must
    also manage synchronization on this storage. This is easy to get wrong,
    and even when it's implemented correctly, having every layer synchronize
    separately adds a lot of overhead. Ideally, it should be possible for
    state stored by the subscriber to be exposed to `Layer`s.
    
    ## Solution:
    
    This branch *starts* on the project of a general-purpose span registry
    by adding a new trait, `LookupMetadata`. Subscribers may implement this
    trait to allow looking up a span's `Metadata` by ID. If a subscriber
    implements this trait, the `layer::Context` type will then allow any
    layers wrapping that subscriber to look up metadata by ID. The
    `FmtSubscriber` implements this trait.
    
    Future work will be able to an interface for `Subscriber`s to expose
    more span data, such as parents, children, and arbitrarily-typed
    extensions, to `Layer`s as well.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ tracing-subscriber/src/registry/mod.rs
@@ -0,0 +1,47 @@
+//! **EXPERIMENTAL**: Storage for span data shared by multiple [`Layer`]s.
+//!
+//! This module is experimental. Although potential breaking changes will be
+//! avoided when possible, we reserve the right to make breaking changes to this
+//! module until it is no longer experimental.
+//!
+//! Add the `registry_unstable` feature to your `Cargo.toml` to enable
+//! this module:
+//!
+//! ```toml
+//! [dependencies.tracing-subscriber]
+//! features = ["registry_unstable"]
+//! ```
+//!
+//! [`Layer`]: ../layer/struct.Layer.html
+use tracing_core::{span::Id, Metadata};
+/// Provides access to stored span metadata.
+///
+/// Subscribers which store span metadata and associate it with span IDs should
+/// implement this trait; if they do, any [`Layer`]s wrapping them can look up
+/// metadata via the [`Context`] type's [`metadata()`] method.
+///
+/// [`Layer`]: ../layer/struct.Layer.html
+/// [`Context`]: ../layer/struct.Context.html
+/// [`metadata()`]: ../layer/struct.Context.html#method.metadata
+pub trait LookupMetadata {
+    /// Returns metadata for tne span with the given `id`, if it exists.
+    ///
+    /// If no span exists for the provided ID (e.g. the span has closed and been
+    /// removed from the registry, or the ID is invalid), this should return `None`.
+    fn metadata(&self, id: &Id) -> Option<&'static Metadata<'static>>;
+
+    /// Returns `true` if a span with the given `id` exists, false otherwise.
+    ///
+    /// **Note**: The default implementation of this method is simply:
+    ///```rust,ignore
+    /// fn exists(&self, id: &span::Id) -> bool {
+    ///     self.metadata(id).is_some()
+    /// }
+    ///```
+    /// If the subscriber has a faster way of determining whether a span exists
+    /// for a given ID (e.g., if the ID is greater than the current value of an
+    /// increasing ID counter, etc), this method may be overridden as an optimization.
+    fn exists(&self, id: &Id) -> bool {
+        self.metadata(id).is_some()
+    }
+}
