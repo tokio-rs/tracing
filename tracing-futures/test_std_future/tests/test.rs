aa058b31d06c4babf7cdf2cf8a7d68d6d8195d7f
Eliza Weisman <eliza@buoyant.io>
2019-11-04 10:39:38 -0800

    futures: update std future tests to track upstream API breakage
    A recent upstream change to `tokio-test` (tokio-rs/tokio#1728) removed
    the `MockTask` API in favour of a more ergonomic `task::spawn` API. This
    broke our tests for `tracing-futures` `std::future` compatibility, which
    depend on `tokio-test` from Git.
    
    This branch updates these tests to use the new API, which *is* much
    nicer. This should fix nightly CI.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-futures/test_std_future/tests/test.rs
+++ tracing-futures/test_std_future/tests/test.rs
@@ -4,7 +4,6 @@ extern crate tracing_core;
 
 use test_std_future::{block_on_future, support::*, PollN};
 
-use tokio_test::task::MockTask;
 use tracing::{subscriber::with_default, Level};
 use tracing_futures::Instrument;
 
@@ -18,10 +17,9 @@ fn enter_exit_is_reasonable() {
         .drop_span(span::mock().named("foo"))
         .done()
         .run_with_handle();
-    let mut task = MockTask::new();
     with_default(subscriber, || {
         let future = PollN::new_ok(2).instrument(span!(Level::TRACE, "foo"));
-        block_on_future(&mut task, future).unwrap();
+        block_on_future(future).unwrap();
     });
     handle.assert_finished();
 }
@@ -36,10 +34,9 @@ fn error_ends_span() {
         .drop_span(span::mock().named("foo"))
         .done()
         .run_with_handle();
-    let mut task = MockTask::new();
     with_default(subscriber, || {
         let future = PollN::new_err(2).instrument(span!(Level::TRACE, "foo"));
-        block_on_future(&mut task, future).unwrap_err();
+        block_on_future(future).unwrap_err();
     });
     handle.assert_finished();
 }
