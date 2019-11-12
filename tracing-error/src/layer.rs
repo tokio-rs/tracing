920f4ed67c67ed131494610e6dcd690b1241af25
Eliza Weisman <eliza@buoyant.io>
2019-11-12 15:52:44 -0800

    extremely bad, but works
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-error/src/layer.rs
+++ tracing-error/src/layer.rs
@@ -1,26 +1,33 @@
 use tracing_subscriber::{layer::{self, Layer}, fmt::{FormattedFields, format::{self, FormatFields}}, registry::LookupSpan};
 use tracing_core::{span, Subscriber};
 use std::marker::PhantomData;
-use super::Context;
+use std::sync::atomic::{AtomicPtr, Ordering};
+use std::ptr;
+use super::{Context, Span};
 
-#[derive(Debug)]
-pub struct ErrorLayer<S, F = format::DefaultFields> {
+pub struct ErrorLayer<F = format::DefaultFields> {
     format: F,
-    _s: PhantomData<fn(S)>,
+    get_context: AtomicPtr<()>
 }
 
-pub(crate) struct LayerMarker<F> {
-    _f: PhantomData<fn(F)>
-}
-
-impl<S, F> Layer<S> for ErrorLayer<S, F>
+impl<S, F> Layer<S> for ErrorLayer<F>
 where
     S: Subscriber + for<'span> LookupSpan<'span>,
     F: for<'writer> FormatFields<'writer> + 'static,
 {
+    fn register_callsite(&self, _: &'static tracing_core::Metadata<'static>) -> tracing_core::subscriber::Interest {
+        self.get_context.compare_exchange(
+            ptr::null_mut(),
+            (Self::get_context::<S>) as fn(&tracing_core::Dispatch) -> Option<Context<F>> as *const () as *mut _,
+            Ordering::AcqRel, Ordering::Acquire
+        );
+        tracing_core::subscriber::Interest::always()
+    }
+
     /// Notifies this layer that a new span was constructed with the given
     /// `Attributes` and `Id`.
     fn new_span(&self, attrs: &span::Attributes<'_>, id: &span::Id, ctx: layer::Context<'_, S>) {
+
         let span = ctx.span(id).expect("span must already exist!");
         if span.extensions().get::<FormattedFields<F>>().is_some() {
             return;
@@ -33,19 +40,43 @@ where
 
 }
 
-impl<S, F> ErrorLayer<S, F>
+impl<F> ErrorLayer<F>
 where
-    F: for<'writer> FormatFields<'writer>,
-    S: Subscriber + for<'span> LookupSpan<'span>,
+    F: for<'writer> FormatFields<'writer> + 'static,
 {
-    pub(crate) fn current_context(&self, dispatch: &tracing_core::Dispatch) -> Option<Context> {
-        let subscriber = dispatch.downcast_ref::<S>()?;
-        let curr_id = subscriber.current_span().id()?;
+    pub fn new(format: F) -> Self {
+        Self {
+            format,
+            get_context: AtomicPtr::new(ptr::null_mut()),
+        }
+    }
+
+    fn get_context<S>(dispatch: &tracing_core::Dispatch) -> Option<Context<F>>
+    where
+        S: Subscriber + for<'span> LookupSpan<'span>,
+    {
+        let subscriber = dispatch.downcast_ref::<S>().expect("subscriber should downcast to expected type; this is a bug!");
+        let curr_span = subscriber.current_span();
+        let curr_id = curr_span.id()?;
         let span = subscriber.span(curr_id).expect("registry should have a span for the current ID");
         let mut ctx = Context::new();
+        let fields = span.extensions().get::<FormattedFields<F>>().map(|f| f.fmt_fields.clone()).unwrap_or_default();
+        ctx.context.push(Span { metadata: span.metadata(), fields });
         for span in span.parents() {
-            unimplemented!("add to ctx")
+            let fields = span.extensions().get::<FormattedFields<F>>().map(|f| f.fmt_fields.clone()).unwrap_or_default();
+            ctx.context.push(Span { metadata: span.metadata(), fields });
         }
         Some(ctx)
+    }
+
+    pub(crate) fn current_context(&self, dispatch: &tracing_core::Dispatch) -> Option<Context<F>> {
+        let get_context = unsafe { self.get_context.load(Ordering::Acquire).cast::<fn(&tracing_core::Dispatch) -> Option<Context<F>>>().as_ref().expect("should have been set!") };
+        (get_context)(dispatch)
+    }
+}
+
+impl Default for ErrorLayer {
+    fn default() -> Self {
+        Self::new(format::DefaultFields::default())
     }
 }
--- tracing-error/src/lib.rs
+++ tracing-error/src/lib.rs
@@ -1,15 +1,21 @@
-use std::error::Error;
 mod layer;
 use tracing_core::{Metadata, dispatcher};
-#[derive(Debug)]
-pub struct ContextError {
+use tracing_subscriber::fmt::format;
+use std::marker::PhantomData;
+use std::error::Error;
+use std::fmt;
+
+pub use self::layer::ErrorLayer;
+
+pub struct ContextError<F = format::DefaultFields> {
     inner: Box<dyn Error + Send + Sync>,
-    context: Option<Context>,
+    context: Option<Context<F>>,
 }
 
-#[derive(Clone, Debug)]
-pub struct Context {
+#[derive(Clone)]
+pub struct Context<F = format::DefaultFields> {
     context: Vec<Span>,
+    _fmt: PhantomData<fn(F)>,
 }
 
 #[derive(Clone, Debug)]
@@ -18,35 +24,89 @@ struct Span {
     fields: String,
 }
 
-impl ContextError {
-    pub fn from_error<F>(error: Box<dyn Error + Send + Sync>) -> Self {
+impl<F> ContextError<F>
+where
+    F: for<'writer> format::FormatFields<'writer> + 'static
+{
+    pub fn from_error(error: Box<dyn Error + Send + Sync + 'static>) -> Self {
         ContextError {
             inner: error,
             context: Context::<F>::current(),
         }
     }
-}
 
-pub trait TraceError: Error + Send + Sync {
-    fn in_context<F>(self) -> ContextError
-    where
-        Self: Sized,
-    {
-        ContextError::from_error::<F>(Box::new(self))
+    pub fn context(&self) -> Option<&Context<F>> {
+        self.context.as_ref()
     }
 }
 
-impl<T> TraceError for T where T: Error + Send + Sync {}
+pub trait TraceError: Error + Sized + Send + Sync + 'static {
+    fn in_context(self) -> ContextError {
+        ContextError::from_error(Box::new(self))
+    }
+}
 
-impl Context {
-    fn current<F>() -> Option<Self> {
+impl<T> TraceError for T where T: Error + Sized + Send + Sync + 'static {}
+
+impl<F> Context<F>
+where
+    F: for<'writer> format::FormatFields<'writer> + 'static
+{
+    fn current() -> Option<Self> {
         dispatcher::get_default(|curr| {
-            curr.downcast_ref::<layer::LayerMarker<F>>()?
+            curr.downcast_ref::<ErrorLayer<F>>()?
                 .current_context(&curr)
         })
     }
 
     fn new() -> Self {
-        unimplemented!()
+        Self {
+            context: Vec::new(),
+            _fmt: PhantomData,
+        }
+    }
+}
+
+impl<F> fmt::Debug for Context<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut map = f.debug_map();
+        for Span { ref metadata, ref fields } in self.context.iter() {
+            map.entry(&metadata.name(), &format_args!("{}", fields));
+        }
+        map.finish()
+    }
+}
+
+impl<F> fmt::Display for ContextError<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.inner)?;
+        if let Some(ctx) = self.context.as_ref() {
+            writeln!(f, "");
+            for Span { ref metadata, ref fields } in ctx.context.iter() {
+                write!(f, "\tin {}::{}", metadata.target(), metadata.name())?;
+                if let Some((file, line)) = metadata.file().and_then(|f| metadata.line().map(|l| (f, l))) {
+                    writeln!(f, "({}:{})", file, line)?;
+                }
+                if fields.len() > 0 {
+                    writeln!(f, "\t   {}", fields)?
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<F> fmt::Debug for ContextError<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ContextError")
+         .field("inner", &self.inner)
+         .field("context", &self.context)
+         .finish()
+    }
+}
+
+impl<F> Error for ContextError<F> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.inner.as_ref())
     }
 }
