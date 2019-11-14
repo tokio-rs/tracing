3c933b6ec35b1bbe08a28cdbcc079c8388e3124a
Eliza Weisman <eliza@buoyant.io>
2019-11-14 13:34:11 -0800

    wip
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-error/src/lib.rs
+++ tracing-error/src/lib.rs
@@ -1,19 +1,18 @@
+pub mod fmt;
 mod layer;
-use tracing_core::{Metadata, dispatcher};
-use tracing_subscriber::fmt::format;
-use std::marker::PhantomData;
 use std::error::Error;
-use std::fmt;
+use std::marker::PhantomData;
+use tracing_core::{dispatcher, Metadata};
 
 pub use self::layer::ErrorLayer;
 
-pub struct ContextError<F = format::DefaultFields> {
+pub struct ContextError<F = fmt::DefaultFields> {
     inner: Box<dyn Error + Send + Sync>,
     context: Option<Context<F>>,
 }
 
 #[derive(Clone)]
-pub struct Context<F = format::DefaultFields> {
+pub struct Context<F = fmt::DefaultFields> {
     context: Vec<Span>,
     _fmt: PhantomData<fn(F)>,
 }
@@ -26,7 +25,7 @@ struct Span {
 
 impl<F> ContextError<F>
 where
-    F: for<'writer> format::FormatFields<'writer> + 'static
+    F: for<'writer> fmt::FormatFields<'writer> + 'static,
 {
     pub fn from_error(error: Box<dyn Error + Send + Sync + 'static>) -> Self {
         ContextError {
@@ -50,13 +49,10 @@ impl<T> TraceError for T where T: Error + Sized + Send + Sync + 'static {}
 
 impl<F> Context<F>
 where
-    F: for<'writer> format::FormatFields<'writer> + 'static
+    F: for<'writer> fmt::FormatFields<'writer> + 'static,
 {
     fn current() -> Option<Self> {
-        dispatcher::get_default(|curr| {
-            curr.downcast_ref::<ErrorLayer<F>>()?
-                .current_context(&curr)
-        })
+        dispatcher::get_default(|curr| curr.downcast_ref::<ErrorLayer<F>>()?.current_context(&curr))
     }
 
     fn new() -> Self {
@@ -67,28 +63,39 @@ where
     }
 }
 
-impl<F> fmt::Debug for Context<F> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl<F> std::fmt::Debug for Context<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut map = f.debug_map();
-        for Span { ref metadata, ref fields } in self.context.iter() {
-            map.entry(&metadata.name(), &format_args!("{}", fields));
+        for Span {
+            ref metadata,
+            ref fields,
+        } in self.context.iter()
+        {
+            map.entry(&metadata.name(), &fmt_args!("{}", fields));
         }
         map.finish()
     }
 }
 
-impl<F> fmt::Display for ContextError<F> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl<F> std::fmt::Display for ContextError<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.inner)?;
         if let Some(ctx) = self.context.as_ref() {
             writeln!(f, "");
-            for Span { ref metadata, ref fields } in ctx.context.iter() {
+            for Span {
+                ref metadata,
+                ref fields,
+            } in ctx.context.iter()
+            {
                 write!(f, "   in {}::{}", metadata.target(), metadata.name())?;
                 if fields.len() > 0 {
                     write!(f, ", {}", fields)?
                 }
                 writeln!(f, "");
-                if let Some((file, line)) = metadata.file().and_then(|f| metadata.line().map(|l| (f, l))) {
+                if let Some((file, line)) = metadata
+                    .file()
+                    .and_then(|f| metadata.line().map(|l| (f, l)))
+                {
                     writeln!(f, "\tat {}:{}", file, line)?;
                 }
             }
@@ -97,12 +104,12 @@ impl<F> fmt::Display for ContextError<F> {
     }
 }
 
-impl<F> fmt::Debug for ContextError<F> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl<F> std::fmt::Debug for ContextError<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ContextError")
-         .field("inner", &self.inner)
-         .field("context", &self.context)
-         .finish()
+            .field("inner", &self.inner)
+            .field("context", &self.context)
+            .finish()
     }
 }
 
