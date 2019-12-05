e2ce00dd90ad8f4b6b713eff873c9bd11e92a81a
David Barsky <me@davidbarsky.com>
2019-12-05 10:00:53 -0500

    fmt

--- nightly-examples/examples/tower.rs
+++ nightly-examples/examples/tower.rs
@@ -1,11 +1,11 @@
-use hyper::client::service::Connect;
-use hyper::client::conn::Builder;
-use hyper::client::connect::HttpConnector;
-use hyper::service::Service;
-use tracing_tower::InstrumentableService;
+use http::{Method, Request};
+use hyper::{
+    client::{conn::Builder, connect::HttpConnector, service::Connect},
+    service::Service,
+    Body,
+};
 use tracing::info;
-use hyper::Body;
-use http::{Request, Method};
+use tracing_tower::InstrumentableService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -32,7 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let mut mk_svc = Connect::new(HttpConnector::new(), Builder::new());
-    
+
     let uri = "http://httpbin.org".parse::<http::Uri>()?;
     let svc = mk_svc.call(uri.clone()).await?;
     let mut svc = svc.trace_requests(req_span);
@@ -41,7 +41,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let req = Request::builder()
         .method(Method::GET)
         .uri(uri)
-        .body(body)?;
+        .body(body)
+        .expect("Unable to build request; this is a bug.");
 
     let res = svc.call(req).await?;
     info!(message = "got a response", res = ?res.headers());
