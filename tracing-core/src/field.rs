d1eab2c30982ee4bf8bb1e79dfa60225dacac6cc
Oliver Scherer @ Cosmian <52913081+oli-cosmian@users.noreply.github.com>
2020-01-15 17:41:35 +0100

    core: support `Wrapping` and `NonZero` types as `Value`s (#538)
    ## Motivation
    
    For tracing there's no difference between a `NonZero*` or `Wrapping`
    type and their underlying type. This allows such types to directly be
    referenced by the tracing macros without requiring additional method
    calls or boilerplate.
    
    ## Solution
    
    Implemented `Value` for `Wrapping<T: Value>` and for all the `NonZero*`
    types from `std::num`.
    
    This required a slight refactoring of the macros generating the regular
    integer impls as to reduce the amount of generated code.

--- tracing-core/src/field.rs
+++ tracing-core/src/field.rs
@@ -42,6 +42,7 @@ use crate::stdlib::{
     borrow::Borrow,
     fmt,
     hash::{Hash, Hasher},
+    num,
     ops::Range,
 };
 
@@ -276,33 +277,88 @@ macro_rules! impl_values {
         )+
     }
 }
-macro_rules! impl_value {
-    ( $record:ident( $( $value_ty:ty ),+ ) ) => {
-        $(
-            impl $crate::sealed::Sealed for $value_ty {}
-            impl $crate::field::Value for $value_ty {
-                fn record(
-                    &self,
-                    key: &$crate::field::Field,
-                    visitor: &mut dyn $crate::field::Visit,
-                ) {
-                    visitor.$record(key, *self)
-                }
+
+macro_rules! ty_to_nonzero {
+    (u8) => {
+        NonZeroU8
+    };
+    (u16) => {
+        NonZeroU16
+    };
+    (u32) => {
+        NonZeroU32
+    };
+    (u64) => {
+        NonZeroU64
+    };
+    (u128) => {
+        NonZeroU128
+    };
+    (usize) => {
+        NonZeroUsize
+    };
+    (i8) => {
+        NonZeroI8
+    };
+    (i16) => {
+        NonZeroI16
+    };
+    (i32) => {
+        NonZeroI32
+    };
+    (i64) => {
+        NonZeroI64
+    };
+    (i128) => {
+        NonZeroI128
+    };
+    (isize) => {
+        NonZeroIsize
+    };
+}
+
+macro_rules! impl_one_value {
+    (bool, $op:expr, $record:ident) => {
+        impl_one_value!(normal, bool, $op, $record);
+    };
+    ($value_ty:tt, $op:expr, $record:ident) => {
+        impl_one_value!(normal, $value_ty, $op, $record);
+        impl_one_value!(nonzero, $value_ty, $op, $record);
+    };
+    (normal, $value_ty:tt, $op:expr, $record:ident) => {
+        impl $crate::sealed::Sealed for $value_ty {}
+        impl $crate::field::Value for $value_ty {
+            fn record(&self, key: &$crate::field::Field, visitor: &mut dyn $crate::field::Visit) {
+                visitor.$record(key, $op(*self))
             }
+        }
+    };
+    (nonzero, $value_ty:tt, $op:expr, $record:ident) => {
+        // This `use num::*;` is reported as unused because it gets emitted
+        // for every single invocation of this macro, so there are multiple `use`s.
+        // All but the first are useless indeed.
+        // We need this import because we can't write a path where one part is
+        // the `ty_to_nonzero!($value_ty)` invocation.
+        #[allow(clippy::useless_attribute, unused)]
+        use num::*;
+        impl $crate::sealed::Sealed for ty_to_nonzero!($value_ty) {}
+        impl $crate::field::Value for ty_to_nonzero!($value_ty) {
+            fn record(&self, key: &$crate::field::Field, visitor: &mut dyn $crate::field::Visit) {
+                visitor.$record(key, $op(self.get()))
+            }
+        }
+    };
+}
+
+macro_rules! impl_value {
+    ( $record:ident( $( $value_ty:tt ),+ ) ) => {
+        $(
+            impl_one_value!($value_ty, |this: $value_ty| this, $record);
         )+
     };
-    ( $record:ident( $( $value_ty:ty ),+ as $as_ty:ty) ) => {
+    ( $record:ident( $( $value_ty:tt ),+ as $as_ty:ty) ) => {
         $(
-            impl $crate::sealed::Sealed for $value_ty {}
-            impl Value for $value_ty {
-                fn record(
-                    &self,
-                    key: &$crate::field::Field,
-                    visitor: &mut dyn $crate::field::Visit,
-                ) {
-                    visitor.$record(key, *self as $as_ty)
-                }
-            }
+            impl_one_value!($value_ty, |this: $value_ty| this as $as_ty, $record);
         )+
     };
 }
@@ -315,6 +371,13 @@ impl_values! {
     record_i64(i64),
     record_i64(isize, i32, i16, i8 as i64),
     record_bool(bool)
+}
+
+impl<T: crate::sealed::Sealed> crate::sealed::Sealed for Wrapping<T> {}
+impl<T: crate::field::Value> crate::field::Value for Wrapping<T> {
+    fn record(&self, key: &crate::field::Field, visitor: &mut dyn crate::field::Visit) {
+        self.0.record(key, visitor)
+    }
 }
 
 impl crate::sealed::Sealed for str {}
