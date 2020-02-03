30a65561286bbdfda5d2cb88c5125f263225bb68
Eliza Weisman <eliza@buoyant.io>
2020-02-03 09:52:41 -0800

    error: introduce `tracing-error` crate (#556)
    ## Motivation
    
    A lot of folks have expressed interest in being able to capture the
    current `tracing` span context when an error type is _constructed_, so
    it can eventually be displayed when the error is handled. Since span
    entry is stackful, the error may not be formatted or otherwise handled
    within a span context that is in any way related to the one where the
    error occurred — and this is only made worse when "channels" are
    involved.
    
    ## Solution
    
    This branch introduces a new crate, `tracing-error`, which will contain
    integrations and types for enriching errors with `tracing` span contexts.
    
    Currently, this crate contains the following:
    
    - A `SpanTrace` type, which captures the current span and allows it to be
      inspected later.
    
    - A `Layer` implementation which may be added to a `Subscriber` to 
      enable capturing traces for errors. This layer will format fields and
      insert them into span extensions, **if no other layer has already 
      added such an extension** using the same formatter. This reuses the
      formatting traits from `tracing-subscriber`'s `fmt` module.
    
    ## Notes
    
    Note that we have not actually added any new error types or error
    wrapper types. I would like to provide these in the future, so that
    span capturing can be seamlessly integrated into the rest of the Rust
    error handling ecosystem. However, this will take some additional
    experimentation. For now, we want to release the `SpanTrace` type so
    that other libraries and applications may use _it_ as an integration point.
    
    We'll need to add more documentation and examples for these APIs, and
    can do that in follow-up branches.
    
    Closes #464
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    Co-authored-by: Jane Lusby <jlusby@yaah.dev>

---
+++ tracing-error/src/backtrace.rs
@@ -0,0 +1,171 @@
+use crate::layer::WithContext;
+use std::fmt;
+use tracing::{Metadata, Span};
+
+/// A captured trace of [`tracing`] spans.
+///
+/// This type can be thought of as a relative of
+/// [`std::backtrace::Backtrace`][`Backtrace`].
+/// However, rather than capturing the current call stack when it is
+/// constructed, a `SpanTrace` instead captures the current [span] and its
+/// [parents].
+///
+/// In many cases, span traces may be as useful as stack backtraces useful in
+/// pinpointing where an error occurred and why, if not moreso:
+///
+/// * A span trace captures only the user-defined, human-readable `tracing`
+///   spans, rather than _every_ frame in the call stack, often cutting out a
+///   lot of noise.
+/// * Span traces include the [fields] recorded by each span in the trace, as
+///   well as their names and source code location, so different invocations of
+///   a function can be distinguished,
+/// * In asynchronous code, backtraces for errors that occur in [futures] often
+///   consist not of the stack frames that _spawned_ a future, but the stack
+///   frames of the executor that is responsible for running that future. This
+///   means that if an `async fn` calls another `async fn` which generates an
+///   error, the calling async function will not appear in the stack trace (and
+///   often, the callee won't either!). On the other hand, when the
+///   [`tracing-futures`] crate is used to instrument async code, the span trace
+///   will represent the logical application context a future was running in,
+///   rather than the stack trace of the executor that was polling a future when
+///   an error occurred.
+///
+/// Finally, unlike stack [`Backtrace`]s, capturing a `SpanTrace` is fairly
+/// lightweight, and the resulting struct is not large. The `SpanTrace` struct
+/// is formatted lazily; instead, it simply stores a copy of the current span,
+/// and allows visiting the spans in that span's trace tree by calling the
+/// [`with_spans` method][`with_spans`].
+///
+/// # Formatting
+///
+/// The `SpanTrace` type implements `fmt::Display`, formatting the span trace
+/// similarly to how Rust formats panics. For example:
+///
+/// ```text
+/// span backtrace:
+///    0: custom_error::do_another_thing
+///         with answer=42 will_succeed=false
+///           at examples/examples/custom_error.rs:42
+///    1: custom_error::do_something
+///         with foo="hello world"
+///           at examples/examples/custom_error.rs:37
+/// ```
+///
+/// Additionally, if custom formatting is desired, the [`with_spans`] method can
+/// be used to visit each span in the trace, formatting them in order.
+///
+/// [`tracing`]: https://docs.rs/tracing
+/// [`Backtrace`]: https://doc.rust-lang.org/std/backtrace/struct.Backtrace.html
+/// [span]: https://docs.rs/tracing/latest/tracing/span/index.html
+/// [parents]: https://docs.rs/tracing/latest/tracing/span/index.html#span-relationships
+/// [fields]: https://docs.rs/tracing/latest/tracing/field/index.html
+/// [futures]: https://doc.rust-lang.org/std/future/trait.Future.html
+/// [`tracing-futures`]: https://docs.rs/tracing-futures/
+/// [`with_spans`] method: #method.with_spans.html
+#[derive(Clone)]
+pub struct SpanTrace {
+    span: Span,
+}
+
+// === impl SpanTrace ===
+
+impl SpanTrace {
+    /// Capture the current span trace.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use tracing_error::SpanTrace;
+    ///
+    /// pub struct MyError {
+    ///     span_trace: SpanTrace,
+    ///     // ...
+    /// }
+    ///
+    /// # fn some_error_condition() -> bool { true }
+    ///
+    /// #[tracing::instrument]
+    /// pub fn my_function(arg: &str) -> Result<(), MyError> {
+    ///     if some_error_condition() {
+    ///         return Err(MyError {
+    ///             span_trace: SpanTrace::capture(),
+    ///             // ...
+    ///         });
+    ///     }
+    ///
+    ///     // ...
+    /// #   Ok(())
+    /// }
+    /// ```
+    pub fn capture() -> Self {
+        SpanTrace {
+            span: Span::current(),
+        }
+    }
+
+    /// Apply a function to all captured spans in the trace until it returns
+    /// `false`.
+    ///
+    /// This will call the provided function with a reference to the
+    /// [`Metadata`] and a formatted representation of the [fields] of each span
+    /// captured in the trace, starting with the span that was current when the
+    /// trace was captured. The function may return `true` or `false` to
+    /// indicate whether to continue iterating over spans; if it returns
+    /// `false`, no additional spans will be visited.
+    ///
+    /// [fields]: https://docs.rs/tracing/latest/tracing/field/index.html
+    /// [`Metadata`]: https://docs.rs/tracing/latest/tracing/struct.Metadata.html
+    pub fn with_spans(&self, f: impl FnMut(&'static Metadata<'static>, &str) -> bool) {
+        self.span.with_subscriber(|(id, s)| {
+            if let Some(getcx) = s.downcast_ref::<WithContext>() {
+                getcx.with_context(s, id, f);
+            }
+        });
+    }
+}
+
+macro_rules! try_bool {
+    ($e:expr, $dest:ident) => {{
+        let ret = $e.unwrap_or_else(|e| $dest = Err(e));
+
+        if $dest.is_err() {
+            return false;
+        }
+
+        ret
+    }};
+}
+
+impl fmt::Display for SpanTrace {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut err = Ok(());
+        let mut span = 0;
+
+        writeln!(f, "span backtrace:")?;
+        self.with_spans(|metadata, fields| {
+            try_bool!(
+                writeln!(f, "{:>4}: {}::{}", span, metadata.target(), metadata.name()),
+                err
+            );
+            if !fields.is_empty() {
+                try_bool!(writeln!(f, "           with {}", fields), err);
+            }
+            if let Some((file, line)) = metadata
+                .file()
+                .and_then(|file| metadata.line().map(|line| (file, line)))
+            {
+                try_bool!(writeln!(f, "             at {}:{}", file, line), err);
+            }
+
+            span += 1;
+            true
+        });
+
+        err
+    }
+}
+
+impl fmt::Debug for SpanTrace {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
