5524f1d417e929ca8a6d4dd306566160eda94dfb
David Barsky <me@davidbarsky.com>
2020-02-09 15:32:38 -0500

    fix clippy lints on nightly

--- tracing-log/src/log_tracer.rs
+++ tracing-log/src/log_tracer.rs
@@ -27,7 +27,6 @@
 //! [builder]: struct.LogTracer.html#method.builder
 //! [ignore]: struct.Builder.html#method.ignore_crate
 use crate::{format_trace, AsTrace};
-use log;
 pub use log::SetLoggerError;
 use tracing_core::dispatcher;
 
@@ -54,16 +53,16 @@ impl LogTracer {
     /// ```rust
     /// # use std::error::Error;
     /// use tracing_log::LogTracer;
-    /// use log;
+    /// use log::{LevelFilter, info}
     ///
     /// # fn main() -> Result<(), Box<Error>> {
     /// LogTracer::builder()
     ///     .ignore_crate("foo") // suppose the `foo` crate is using `tracing`'s log feature
-    ///     .with_max_level(log::LevelFilter::Info)
+    ///     .with_max_level(LevelFilter::Info)
     ///     .init()?;
     ///
     /// // will be available for Subscribers as a tracing Event
-    /// log::info!("an example info log");
+    /// info!("an example info log");
     /// # Ok(())
     /// # }
     /// ```
