5f33d816c16e04c7342df937adf897a68f4d826d
Naja Melan <najamelan@autistici.org>
2020-02-11 17:25:43 +0000

    futures: make the executor/futures_03 module compile (#576)
    ## Motivation
    
    I tried to use `futures_03` module, but it is unusable in current state.
    The code has actually never compiled, due to a typo in the feature flag 
    in the `cfg` attribute on the `executor::futures_03` module. Because 
    the feature flag was wrong, the code was silently skipped by the compiler,
    and errors in it weren't caught. 
    
    ## Solution
    
    - use correct feature name
    - use correct paths to imported types
    - use correct trait names
    - pass correct types to Spawn and SpawnLocal traits
    - remove unused imports

--- tracing-futures/src/executor/mod.rs
+++ tracing-futures/src/executor/mod.rs
@@ -8,7 +8,7 @@ mod futures_preview;
 #[cfg(feature = "futures_preview")]
 pub use self::futures_preview::*;
 
-#[cfg(feature = "futures_03")]
+#[cfg(feature = "futures-03")]
 mod futures_03;
-#[cfg(feature = "futures_03")]
+#[cfg(feature = "futures-03")]
 pub use self::futures_03::*;
