828bb99edce106168771f580ce1760824ba3221b
Eliza Weisman <eliza@buoyant.io>
2020-04-27 11:43:08 -0700

    aghghghhghghhghg
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- examples/examples/sloggish/sloggish_subscriber.rs
+++ examples/examples/sloggish/sloggish_subscriber.rs
@@ -10,11 +10,8 @@
 //!
 //! [`slog-term`]: https://docs.rs/slog-term/2.4.0/slog_term/
 //! [`slog` README]: https://github.com/slog-rs/slog#terminal-output-example
-use self::ansi_term::{Color, Style};
-use ansi_term;
-use humantime;
+use ansi_term::{Color, Style};
 use tracing::{
-    self,
     field::{Field, Visit},
     Id, Level, Subscriber,
 };
