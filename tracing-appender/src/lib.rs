d8b6dbe77a537a5dc09764f4ebb6955c965d1d90
Zeki Sherif <zekshi@amazon.com>
2020-04-27 13:26:16 -0700

    doc updates

--- tracing-appender/src/lib.rs
+++ tracing-appender/src/lib.rs
@@ -6,7 +6,7 @@
 //! structured, event-based diagnostic information. This crate provides the ability
 //! for `tracing` events and spans to be recorded in a non-blocking manner by using a
 //! dedicated logging thread. It also provides a [`RollingFileAppender`][file_appender]
-//! that can be used with or without the non blocking writer.
+//! that can be used with or without the non-blocking writer.
 //!
 //! [file_appender]: ./rolling/struct.RollingFileAppender.html
 //! [tracing]: https://docs.rs/tracing/
@@ -28,9 +28,7 @@
 //!
 //! ```rust
 //! # fn docs() {
-//! use tracing_appender::rolling::{RollingFileAppender, Rotation};
-//!
-//! let file_appender = RollingFileAppender::new(Rotation::HOURLY, "/some/directory", "prefix.log");
+//! let file_appender = tracing_appender::rolling::hourly("/some/directory", "prefix.log");
 //! # }
 //! ```
 //! This creates an hourly rotating file appender which outputs to `/some/directory/prefix.log.YYYY-MM-DD-HH`.
@@ -54,12 +52,18 @@
 //! tracing::subscriber::set_global_default(subscriber.finish()).expect("Could not set global default");
 //! # }
 //! ```
+//! **Note:** `_guard` is a [`WorkerGuard`][guard] which is returned by `tracing_appender::non_blocking`
+//! to ensure buffered logs are flushed to their output in the case of abrupt terminations of a process.
+//! See [`WorkerGuard` module][guard] for more details.
 //!
-//! Alternatively, you can provide *any* type that implements `std::io::Write` to `tracing_appender::non_blocking`
+//! Alternatively, you can provide *any* type that implements [`std::io::Write`][write] to
+//! [`tracing_appender::non_blocking`][non_blocking]
 //!
 //! The [`non_blocking module`][non_blocking]'s documentation provides more detail on how to use `non_blocking`.
 //!
 //! [non_blocking]: ./non_blocking/index.html
+//! [write]: https://doc.rust-lang.org/std/io/trait.Write.html
+//! [guard]: ./non_blocking/struct.WorkerGuard.html
 //!
 //! ## Non-Blocking Rolling File Appender
 //!
@@ -78,79 +82,8 @@ use std::io::Write;
 
 mod inner;
 
-/// A non-blocking, off-thread writer.
-///
-/// This spawns a dedicated worker thread which is responsible for writing log
-/// lines to the provided writer. When a line is written using the returned
-/// `NonBlocking` struct's `make_writer` method, it will be enqueued to be
-/// written by the worker thread.
-///
-/// The queue has a fixed capacity, and if it becomes full, any logs written
-/// to it will be dropped until capacity is once again available. This may
-/// occur if logs are consistently produced faster than the worker thread can
-/// output them. The queue capacity and behavior when full (i.e., whether to
-/// drop logs or to exert backpressure to slow down senders) can be configured
-/// using [`NonBlockingBuilder::default()`][builder].
-/// This function returns the default configuration. It is equivalent to:
-///
-/// ```rust
-/// # use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
-/// # fn doc() -> (NonBlocking, WorkerGuard) {
-/// tracing_appender::non_blocking::NonBlocking::new(std::io::stdout())
-/// # }
-/// ```
-/// [builder]: ./struct.NonBlockingBuilder.html#method.default
-///
-/// <br/> This function returns a tuple of `NonBlocking` and `WorkerGuard`.
-/// `NonBlocking` implements [`MakeWriter`] which integrates with `tracing_subscriber`.
-/// `WorkerGuard` is a drop guard that is responsible for flushing any remaining logs when
-/// the program terminates.
-///
-/// Note that the `WorkerGuard` returned by `non_blocking` _must_ be assigned to a binding that
-/// is not `_`, as `_` will result in the `WorkerGuard` being dropped immediately.
-/// Unintentional drops of `WorkerGuard` remove the guarantee that logs will be flushed
-/// during a program's termination, in a panic or otherwise.
-///
-/// See [`WorkerGuard`][worker_guard] for examples of using the guard.
-///
-/// [worker_guard]: ./struct.WorkerGuard.html
-///
-/// # Examples
-///
-/// ``` rust
-/// # fn docs() {
-/// let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
-/// let subscriber = tracing_subscriber::fmt().with_writer(non_blocking);
-/// tracing::subscriber::with_default(subscriber.finish(), || {
-///    tracing::event!(tracing::Level::INFO, "Hello");
-/// });
-/// # }
-/// ```
 pub mod non_blocking;
 
-/// A rolling file appender.
-///
-/// Creates a log file at a specified directory and file name prefix which *may* be appended with
-/// the date and time.
-///
-/// The following helpers are available for creating a rolling file appender.
-///
-/// - [`Rotation::hourly()`][hourly]: This will result in log file located at `some_directory/log_file_name_prefix.YYYY-MM-DD-HH`
-/// - [`Rotation::daily()`][daily]: This will result in log file located at `some_directory/log_file_name_prefix.YYYY-MM-DD`
-/// - [`Rotation::never()`][never]: This will result in log file located at `some_directory/log_file_name`
-///
-/// [hourly]: fn.hourly.html
-/// [daily]: fn.daily.html
-/// [never]: fn.never.html
-///
-/// # Examples
-///
-/// ```rust
-/// # fn docs() {
-/// use tracing_appender::rolling::{RollingFileAppender, Rotation};
-/// let file_appender = RollingFileAppender::new(Rotation::HOURLY, "/some/directory", "prefix.log");
-/// # }
-/// ```
 pub mod rolling;
 
 mod worker;
--- tracing-appender/src/non_blocking.rs
+++ tracing-appender/src/non_blocking.rs
@@ -1,3 +1,51 @@
+//! A non-blocking, off-thread writer.
+//!
+//! This spawns a dedicated worker thread which is responsible for writing log
+//! lines to the provided writer. When a line is written using the returned
+//! `NonBlocking` struct's `make_writer` method, it will be enqueued to be
+//! written by the worker thread.
+//!
+//! The queue has a fixed capacity, and if it becomes full, any logs written
+//! to it will be dropped until capacity is once again available. This may
+//! occur if logs are consistently produced faster than the worker thread can
+//! output them. The queue capacity and behavior when full (i.e., whether to
+//! drop logs or to exert backpressure to slow down senders) can be configured
+//! using [`NonBlockingBuilder::default()`][builder].
+//! This function returns the default configuration. It is equivalent to:
+//!
+//! ```rust
+//! # use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
+//! # fn doc() -> (NonBlocking, WorkerGuard) {
+//! tracing_appender::non_blocking::NonBlocking::new(std::io::stdout())
+//! # }
+//! ```
+//! [builder]: ./struct.NonBlockingBuilder.html#method.default
+//!
+//! <br/> This function returns a tuple of `NonBlocking` and `WorkerGuard`.
+//! `NonBlocking` implements [`MakeWriter`] which integrates with `tracing_subscriber`.
+//! `WorkerGuard` is a drop guard that is responsible for flushing any remaining logs when
+//! the program terminates.
+//!
+//! Note that the `WorkerGuard` returned by `non_blocking` _must_ be assigned to a binding that
+//! is not `_`, as `_` will result in the `WorkerGuard` being dropped immediately.
+//! Unintentional drops of `WorkerGuard` remove the guarantee that logs will be flushed
+//! during a program's termination, in a panic or otherwise.
+//!
+//! See [`WorkerGuard`][worker_guard] for examples of using the guard.
+//!
+//! [worker_guard]: ./struct.WorkerGuard.html
+//!
+//! # Examples
+//!
+//! ``` rust
+//! # fn docs() {
+//! let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
+//! let subscriber = tracing_subscriber::fmt().with_writer(non_blocking);
+//! tracing::subscriber::with_default(subscriber.finish(), || {
+//!    tracing::event!(tracing::Level::INFO, "Hello");
+//! });
+//! # }
+//! ```
 use crate::worker::Worker;
 use crossbeam_channel::{bounded, Sender};
 use std::io;
