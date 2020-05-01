f4132a7802c2803667504244182c84a6015b1e93
Zeki Sherif <zekshi@amazon.com>
2020-05-01 14:40:32 -0700

    Add bench for synchronous writer so we can compare to non_blocking

--- tracing-appender/benches/bench.rs
+++ tracing-appender/benches/bench.rs
@@ -2,12 +2,51 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::thread;
 use std::thread::JoinHandle;
-use std::time::{Duration, Instant};
-use tempdir::TempDir;
+use std::time::Instant;
 use tracing::{event, Level};
-use tracing_appender::non_blocking::NonBlocking;
-use tracing_appender::{non_blocking, rolling};
+use tracing_appender::non_blocking;
 use tracing_subscriber::fmt::MakeWriter;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+struct SynchronousWriter {
+    writer: Arc<Mutex<Vec<u8>>>,
+}
+
+impl SynchronousWriter {
+    fn new() -> SynchronousWriter {
+        SynchronousWriter {
+            writer: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+}
+
+impl MakeWriter for SynchronousWriter {
+    type Writer = SynchronousWriter;
+
+    fn make_writer(&self) -> Self::Writer {
+        self.clone()
+    }
+}
+
+impl std::io::Write for SynchronousWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let buf_len = buf.len();
+        match self.writer.lock() {
+            Ok(mut guard) => {
+                guard.extend_from_slice(buf);
+            },
+            Err(e) => {
+                eprintln!("Failed to acquire lock: {:?}", e);
+            },
+        }
+        Ok(buf_len)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
 
 /// A cheap writer so that we don't spam console if we had used stdout
 #[derive(Clone)]
@@ -42,37 +81,49 @@ impl MakeWriter for SilentWriter {
     }
 }
 
-fn multi_threaded_bench(non_blocking: NonBlocking, iters: u64) -> Duration {
-    let mut handles: Vec<JoinHandle<()>> = Vec::new();
-
-    let start = Instant::now();
-
-    let cloned_make_writer = non_blocking;
-    let cloned_make_writer_2 = cloned_make_writer.clone();
-
-    handles.push(thread::spawn(move || {
-        let subscriber = tracing_subscriber::fmt().with_writer(cloned_make_writer);
+fn synchronous_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("synchronous");
+    group.bench_function("single_thread", |b| {
+        let subscriber = tracing_subscriber::fmt().with_writer(SynchronousWriter::new());
         tracing::subscriber::with_default(subscriber.finish(), || {
-            for _ in 0..iters {
-                event!(Level::INFO, "event");
-            }
+            b.iter(|| event!(Level::INFO, "event"))
         });
-    }));
+    });
 
-    handles.push(thread::spawn(move || {
-        let subscriber = tracing_subscriber::fmt().with_writer(cloned_make_writer_2);
-        tracing::subscriber::with_default(subscriber.finish(), || {
-            for _ in 0..iters {
-                event!(Level::INFO, "event");
+    group.bench_function("multiple_writers", |b| {
+        b.iter_custom(|iters| {
+            let mut handles: Vec<JoinHandle<()>> = Vec::new();
+
+            let start = Instant::now();
+
+            let make_writer = SynchronousWriter::new();
+            let cloned_make_writer = make_writer.clone();
+
+            handles.push(thread::spawn(move || {
+                let subscriber = tracing_subscriber::fmt().with_writer(make_writer);
+                tracing::subscriber::with_default(subscriber.finish(), || {
+                    for _ in 0..iters {
+                        event!(Level::INFO, "event");
+                    }
+                });
+            }));
+
+            handles.push(thread::spawn(move || {
+                let subscriber = tracing_subscriber::fmt().with_writer(cloned_make_writer);
+                tracing::subscriber::with_default(subscriber.finish(), || {
+                    for _ in 0..iters {
+                        event!(Level::INFO, "event");
+                    }
+                });
+            }));
+
+            for handle in handles {
+                let _ = handle.join();
             }
+
+            start.elapsed()
         });
-    }));
-
-    for handle in handles {
-        let _ = handle.join();
-    }
-
-    start.elapsed()
+    });
 }
 
 fn non_blocking_benchmark(c: &mut Criterion) {
@@ -93,42 +144,42 @@ fn non_blocking_benchmark(c: &mut Criterion) {
             let (silent_writer, _rx) = SilentWriter::new();
             let (non_blocking, _guard) = non_blocking(silent_writer);
 
-            multi_threaded_bench(non_blocking, iters)
-        });
-    });
-}
+            let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
-fn non_blocking_rolling_appender_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("non_blocking_rolling_file_appender");
+            let start = Instant::now();
 
-    group.bench_function("non_blocking", |b| {
-        let temp_dir = TempDir::new("rolling_file_appender").expect("Failed to create temp dir");
-        let file_appender = rolling::hourly(temp_dir.path(), "log");
-        let (non_blocking, _guard) = non_blocking(file_appender);
+            let cloned_make_writer = non_blocking.clone();
 
-        let subscriber = tracing_subscriber::fmt().with_writer(non_blocking);
-        tracing::subscriber::with_default(subscriber.finish(), || {
-            b.iter(|| event!(Level::INFO, "non_blocking event"))
-        });
+            handles.push(thread::spawn(move || {
+                let subscriber = tracing_subscriber::fmt().with_writer(non_blocking);
+                tracing::subscriber::with_default(subscriber.finish(), || {
+                    for _ in 0..iters {
+                        event!(Level::INFO, "event");
+                    }
+                });
+            }));
 
-        let _ = temp_dir.close();
-    });
+            handles.push(thread::spawn(move || {
+                let subscriber = tracing_subscriber::fmt().with_writer(cloned_make_writer);
+                tracing::subscriber::with_default(subscriber.finish(), || {
+                    for _ in 0..iters {
+                        event!(Level::INFO, "event");
+                    }
+                });
+            }));
 
-    group.bench_function("multiple_writers", |b| {
-        b.iter_custom(|iters| {
-            let temp_dir =
-                TempDir::new("rolling_file_appender").expect("Failed to create temp dir");
-            let file_appender = rolling::hourly(temp_dir.path(), "log");
-            let (non_blocking, _guard) = non_blocking(file_appender);
+            for handle in handles {
+                let _ = handle.join();
+            }
 
-            multi_threaded_bench(non_blocking, iters)
+            start.elapsed()
         });
     });
 }
 
 criterion_group!(
     benches,
-    non_blocking_benchmark,
-    non_blocking_rolling_appender_benchmark
+    synchronous_benchmark,
+    non_blocking_benchmark
 );
 criterion_main!(benches);
