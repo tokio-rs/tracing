c5faed2597dff2a9ed80bb470c1c20f9cdf75001
Eliza Weisman <eliza@buoyant.io>
2020-05-23 14:20:41 -0700

    attributes: support arbitrary field expressions (#672)
    ## Motivation
    
    Fields on spans generated by `#[instrument]` consist of the parameters
    to the `instrument`ed function, and a list of optional fields with
    literal values passed in the `fields` argument of the attribute. The
    current API for additional user-defined fields is pretty limited, since
    it only accepts literals (and doesn't accept dotted field names). It
    would be nice to have an API for including additional fields with values
    set by calling methods on `self` or on parameters, or by accessing
    values from parameters.
    
    ## Solution
    
    This branch extends the currently supported `fields` argument to allow
    providing arbitrary expressions which are invoked within the function's
    scope. This is a superset of the previous support for literal values
    only. In addition, field names passed to `fields` may now contain dotted
    Rust identifiers.
    
    Implementing this required rewriting how arguments to the
    `#[instrument]` attribute are parsed. Previously, we used `syn`'s
    `AttributeArgs` type, which parses a comma-separated of "nested meta"
    items, consisting of `$ident($nested meta)`, `$ident = $nested_meta`,
    paths, and literals. By replacing the use of `AttributeArgs` with our
    own types implementing `syn::parse::Parse`, we can accept more
    expressions in the attribute arguments position. This also lets us
    reject more invalid inputs at parse-time, which improves syntax error
    reporting a bit.
    
    One thing that's worth noting is that the current implementation will
    generate an _empty_ field when a field name is provided without an `=`
    and a value. This makes sense when only simple fields with literal
    values are permitted. However, if we accept arbitrary expressions in the
    macro, this is not the ideal behavior --- we would prefer to use the
    same local variable shorthand as the function-like `tracing` macros.
    However, changing this now is a breaking change. Any code which uses a
    name that doesn't exist in the current scope to declare an empty field
    would fail to compile, because it attempts to reference a name that
    doesn't exist. Instead, I left a comment noting that this is not the
    ideal behavior and it should be changed next time we're ready to break
    the proc macros.
    
    Fixes: #650
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-attributes/tests/fields.rs
+++ tracing-attributes/tests/fields.rs
@@ -15,6 +15,34 @@ fn fn_param(param: u32) {}
 #[instrument(fields(foo = "bar", empty))]
 fn fn_empty_field() {}
 
+#[instrument(fields(len = s.len()))]
+fn fn_expr_field(s: &str) {}
+
+#[instrument(fields(s.len = s.len(), s.is_empty = s.is_empty()))]
+fn fn_two_expr_fields(s: &str) {
+    let _ = s;
+}
+
+#[instrument(fields(%s, s.len = s.len()))]
+fn fn_clashy_expr_field(s: &str) {
+    let _ = s;
+}
+
+#[instrument(fields(s = "s"))]
+fn fn_clashy_expr_field2(s: &str) {
+    let _ = s;
+}
+
+#[derive(Debug)]
+struct HasField {
+    my_field: &'static str,
+}
+
+impl HasField {
+    #[instrument(fields(my_field = self.my_field), skip(self))]
+    fn self_expr_field(&self) {}
+}
+
 #[test]
 fn fields() {
     let span = span::mock().with_field(
@@ -26,6 +54,64 @@ fn fields() {
     );
     run_test(span, || {
         fn_no_param();
+    });
+}
+
+#[test]
+fn expr_field() {
+    let span = span::mock().with_field(
+        mock("s")
+            .with_value(&tracing::field::debug("hello world"))
+            .and(mock("len").with_value(&"hello world".len()))
+            .only(),
+    );
+    run_test(span, || {
+        fn_expr_field(&"hello world");
+    });
+}
+
+#[test]
+fn two_expr_fields() {
+    let span = span::mock().with_field(
+        mock("s")
+            .with_value(&tracing::field::debug("hello world"))
+            .and(mock("s.len").with_value(&"hello world".len()))
+            .and(mock("s.is_empty").with_value(&false))
+            .only(),
+    );
+    run_test(span, || {
+        fn_two_expr_fields(&"hello world");
+    });
+}
+
+#[test]
+fn clashy_expr_field() {
+    let span = span::mock().with_field(
+        // Overriding the `s` field should record `s` as a `Display` value,
+        // rather than as a `Debug` value.
+        mock("s")
+            .with_value(&tracing::field::display("hello world"))
+            .and(mock("s.len").with_value(&"hello world".len()))
+            .only(),
+    );
+    run_test(span, || {
+        fn_clashy_expr_field(&"hello world");
+    });
+
+    let span = span::mock().with_field(mock("s").with_value(&"s").only());
+    run_test(span, || {
+        fn_clashy_expr_field2(&"hello world");
+    });
+}
+
+#[test]
+fn self_expr_field() {
+    let span = span::mock().with_field(mock("my_field").with_value(&"hello world").only());
+    run_test(span, || {
+        let has_field = HasField {
+            my_field: "hello world",
+        };
+        has_field.self_expr_field();
     });
 }
 
