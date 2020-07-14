93c124341bfa294bb1ebf5c54801483d5fd0e59c
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-07-14 23:06:48 +0530

    move datetime into a new module
    This change creates datetime into a new mod, which is only included and
    used when crono is disabled.
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- tracing-subscriber/src/fmt/mod.rs
+++ tracing-subscriber/src/fmt/mod.rs
@@ -124,6 +124,8 @@
 use std::{any::TypeId, error::Error, io};
 use tracing_core::{span, subscriber::Interest, Event, Metadata};
 
+#[cfg(not(feature = "chrono"))]
+mod datetime;
 mod fmt_layer;
 pub mod format;
 pub mod time;
