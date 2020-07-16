75b0f93e27f9b040583c0983aadf10ee46b11b89
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-07-17 01:30:13 +0530

    add better docs for max_len
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- tracing-subscriber/src/fmt/format/mod.rs
+++ tracing-subscriber/src/fmt/format/mod.rs
@@ -848,20 +848,28 @@ impl<'a> fmt::Display for FmtThreadName<'a> {
             Ordering::{AcqRel, Acquire, Relaxed},
         };
 
-        // MAX_LEN stores the length of the longest thread name that we've seen
-        // so far and is persistent across fmt executions being a static field
-        // thread names are padded with this field to get formatted outputs.
+        // Track the longest thread name length we've seen so far in an atomic,
+        // so that it can be updated by any thread.
         static MAX_LEN: AtomicUsize = AtomicUsize::new(0);
         let len = self.name.len();
+        // Snapshot the current max thread name length.
         let mut max_len = MAX_LEN.load(Relaxed);
 
         while len > max_len {
+            // Try to set a new max length, if it is still the value we took a
+            // snapshot of.
             match MAX_LEN.compare_exchange(max_len, len, AcqRel, Acquire) {
+                // We successfully set the new max value
                 Ok(_) => break,
+                // Another thread set a new max value since we last observed
+                // it! It's possible that the new length is actually longer than
+                // ours, so we'll loop again and check whether our length is
+                // still the longest. If not, we'll just use the newer value.
                 Err(actual) => max_len = actual,
             }
         }
 
+        // pad thread name using `max_len`
         write!(f, "{:>width$}", self.name, width = max_len)
     }
 }
