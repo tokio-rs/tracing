793829be6e9fcefe457e331d483b70d75f0bf3a5
Eliza Weisman <eliza@buoyant.io>
2020-08-04 19:50:33 -0700

    wip

--- tracing-subscriber/src/extensions.rs
+++ tracing-subscriber/src/extensions.rs
@@ -1,12 +1,13 @@
 use crate::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
-use std::hash::Hasher;
+use std::hash::{BuildHasherDefault,Hasher};
 use std::marker::PhantomData;
+use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Default)]
 pub struct Directory {
-    known: HashMap<TypeId, usize, IdHasher>,
+    known: HashMap<TypeId, usize, BuildHasherDefault<IdHasher>>,
 }
 
 pub struct Key<T> {
@@ -15,24 +16,12 @@ pub struct Key<T> {
 }
 
 pub struct Extensions {
-    values: Vec<ExtensionValue>,
+    values: Vec<RwLock<ExtensionValue>>,
 }
 
-type ExtensionValue = RwLock<Option<Box<dyn Any + Send + Sync>>>;
+type ExtensionValue = Option<Box<dyn Any + Send + Sync>>;
 
-pub struct Extension<'a, T> {
-    // XXX(eliza): when `parking_lot` is enabled, this should just use a
-    // `MappedRwLockReadGuard`.
-    guard: RwLockReadGuard<'a, Option<Box<dyn Any + Send + Sync>>>,
-    _ty: PhantomData<T>,
-}
-
-pub struct ExtensionMut<'a, T> {
-    // XXX(eliza): when `parking_lot` is enabled, this should just use a
-    // `MappedRwLockReadGuard`.
-    guard: RwLockWriteGuard<'a, Option<Box<dyn Any + Send + Sync>>>,
-    _ty: PhantomData<T>,
-}
+pub use self::imp::*;
 
 /// With TypeIds as keys, there's no need to hash them. They are already hashes
 /// themselves, coming from the compiler. The IdHasher holds the u64 of
@@ -62,7 +51,11 @@ impl Directory {
 impl Extensions {
     pub fn get<'a, T>(&'a self, key: &Key<T>) -> Extension<'a, T> {
         let guard = self.values[key.index].read().unwrap();
-        Extension { guard, _ty }
+        Extension::from_guard(guard)
+
+    pub fn get_mut<'a, T>(&'a self, key: &Key<T>) -> ExtensionMut<'a, T> {
+        let guard = self.values[key.index].write().unwrap();
+        ExtensionMut::from_guard(guard)
     }
 }
 
@@ -79,5 +72,74 @@ impl Hasher for IdHasher {
     #[inline]
     fn finish(&self) -> u64 {
         self.0
+    }
+}
+
+#[cfg(feature = "parking_lot")]
+mod imp {
+    use super::*;
+    use parking_lot::{MappedRwLockReadGuard, MappedRwLockWriteGuard};
+    pub struct Extension<'a, T> {
+        guard: MappedRwLockReadGuard<'a, Option<T>>,
+    }
+
+    pub struct ExtensionMut<'a, T> {
+        guard: MappedRwLockWriteGuard<'a, Option<T>>,
+    }
+
+    impl Extensions {
+        pub fn get<'a, T>(&'a self, key: &Key<T>) -> Extension<'a, T> {
+            let guard = self.values[key.index].read().unwrap();
+            Extension { guard }
+        }
+    }
+
+
+    impl<'a, T> Extension<'a, T> {
+        #[inline]
+        pub(super) fn from_guard(guard: RwLockReadGuard<'a, ExtensionValue>) -> Self {
+            
+            let guard = RwLockReadGuard::map(|val| val.map(|v| v.downcast_ref::<T>().expect("must downcast")))
+            Extension { guard }
+        }
+
+    impl<'a, T> ExtensionMut<'a, T> {
+        #[inline]
+        pub(super) fn from_guard(guard: RwLockWriteGuard<'a, ExtensionValue>) -> Self {
+            let guard = RwLockWriteGuard::map(|val| val.map(|v| v.downcast_mut::<T>().expect("must downcast")))
+            Extension { guard, _ty: PhantomData }
+        }
+    }
+
+}
+
+#[cfg(not(feature = "parking_lot"))]
+mod imp {
+    use super::*;
+    pub struct Extension<'a, T> {
+        // XXX(eliza): when `parking_lot` is enabled, this should just use a
+        // `MappedRwLockReadGuard`.
+        guard: RwLockReadGuard<'a, ExtesionValue>,
+        _ty: PhantomData<T>,
+    }
+
+    pub struct ExtensionMut<'a, T> {
+        // XXX(eliza): when `parking_lot` is enabled, this should just use a
+        // `MappedRwLockReadGuard`.
+        guard: RwLockWriteGuard<'a, ExtesionValue>,
+        _ty: PhantomData<T>,
+    }
+
+    impl<'a, T> Extension<'a, T> {
+        #[inline]
+        pub(super) fn from_guard(guard: RwLockReadGuard<'a, ExtensionValue>) -> Self {
+            Extension { guard, _ty: PhantomData }
+        }
+
+    impl<'a, T> ExtensionMut<'a, T> {
+        #[inline]
+        pub(super) fn from_guard(guard: RwLockWriteGuard<'a, ExtensionValue>) -> Self {
+            Extension { guard, _ty: PhantomData }
+        }
     }
 }
