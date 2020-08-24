06b3fcbe413af0285af4beaf404c40e3a681fd0c
Eliza Weisman <eliza@buoyant.io>
2020-08-24 15:29:19 -0700

    add a version of the logo with text
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing/src/lib.rs
+++ tracing/src/lib.rs
@@ -843,7 +843,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(html_root_url = "https://docs.rs/tracing/0.1.19")]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo.svg",
+    html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
     issue_tracker_base_url = "https://github.com/tokio-rs/tracing/issues/"
 )]
 #![warn(
