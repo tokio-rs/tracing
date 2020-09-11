cb8834211da63bc95736f2ab2354743065bf5acc
Eliza Weisman <eliza@buoyant.io>
2020-09-11 14:13:47 -0700

    serde: prepare to release 0.1.2 (#973)
    ### Added
    
    - `SerdeMapVisitor::finish` to complete serializing the visited objects
      (#892)
    - `SerdeMapVisitor::take_serializer` to return the serializer wrapped by
      a `SerdeMapVisitor` (#892)
    
    Thanks to @bryanburgers for contributing to this release!

--- tracing-serde/src/lib.rs
+++ tracing-serde/src/lib.rs
@@ -125,7 +125,7 @@
 //!
 //! [`tracing`]: https://crates.io/crates/tracing
 //! [`serde`]: https://crates.io/crates/serde
-#![doc(html_root_url = "https://docs.rs/tracing-serde/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tracing-serde/0.1.2")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
     issue_tracker_base_url = "https://github.com/tokio-rs/tracing/issues/"
