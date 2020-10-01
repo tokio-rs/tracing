9c6dd2d58568a5dabee8425f8815b236d4c421fe
Ana Hobden <operator@hoverbear.org>
2020-10-01 16:05:02 -0700

    serde: allow tracing-serde to work on no_std. (#960)
    Allows `tracing-serde` to work on devices without `std`.
    
    *  Enable `#[no_std]` in `tracing-serde` when `cfg(not(feature = std))`
    *  No longer have unnecessary dependencies on std in serde/tracing.
    
    No behavior differences or actual code changes other than allowing
    for core/alloc fallbacks.
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- tracing-serde/src/lib.rs
+++ tracing-serde/src/lib.rs
@@ -109,6 +109,21 @@
 //! subscriber (`JsonSubscriber` in the above example) to record serialized
 //! trace data.
 //!
+//! ##  Crate Feature Flags
+//!
+//! The following crate feature flags are available:
+//!
+//! * `std`: Depend on the Rust standard library (enabled by default).
+//!
+//!   `no_std` users may disable this feature with `default-features = false`:
+//!
+//!   ```toml
+//!   [dependencies]
+//!   tracing-serde = { version = "0.2", default-features = false }
+//!   ```
+//
+//!   **Note**:`tracing-serde`'s `no_std` support requires `liballoc`.
+//!
 //! ## Supported Rust Versions
 //!
 //! Tracing is built against the latest stable release. The minimum supported
@@ -153,7 +168,10 @@
     unused_parens,
     while_true
 )]
-use std::fmt;
+// Support using tracing-serde without the standard library!
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use core::fmt;
 
 use serde::{
     ser::{SerializeMap, SerializeSeq, SerializeStruct, SerializeTupleStruct, Serializer},
