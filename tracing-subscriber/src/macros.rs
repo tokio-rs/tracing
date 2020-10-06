f37cf3970667312b2b9fa75626ae4b040373776e
nasa <htilcs1115@gmail.com>
2020-10-06 17:40:29 +0900

    Update tracing-subscriber/src/macros.rs
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/macros.rs
+++ tracing-subscriber/src/macros.rs
@@ -14,7 +14,7 @@ macro_rules! try_lock {
 }
 
 macro_rules! cfg_feature {
-    ($name:expr, { $($item:item)* }) => {
+    ($name:literal, { $($item:item)* }) => {
         $(
             #[cfg(feature = $name)]
             #[cfg_attr(docsrs, doc(cfg(feature = $name)))]
