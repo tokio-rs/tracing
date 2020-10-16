5b271a3a76d3bb1b7cf2b4feb6513b4f7688deaf
David Barsky <me@davidbarsky.com>
2020-10-16 14:43:50 -0400

    Update tracing-core/src/metadata.rs
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-core/src/metadata.rs
+++ tracing-core/src/metadata.rs
@@ -56,7 +56,7 @@ use core::{
 /// [file name]: Self::file
 /// [line number]: Self::line
 /// [module path]: Self::module_path
-/// [`Collector`]: super::collect::Collect
+/// [collector]: super::collect::Collect
 /// [callsite identifier]: super::callsite::Identifier
 pub struct Metadata<'a> {
     /// The name of the span described by this metadata.
