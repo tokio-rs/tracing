4d3cafa8ee0d8c1c513a81a02a9e66dd18d5be68
Eliza Weisman <eliza@buoyant.io>
2020-10-20 13:36:25 -0700

    subscriber: use a warm dispatcher in fmt benches
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/benches/fmt.rs
+++ tracing-subscriber/benches/fmt.rs
@@ -41,8 +41,9 @@ fn bench_new_span(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("multithreaded", i), i, |b, &i| {
             b.iter_custom(|iters| {
                 let mut total = Duration::from_secs(0);
+                let dispatch = mk_dispatch();
                 for _ in 0..iters {
-                    let bench = MultithreadedBench::new(mk_dispatch());
+                    let bench = MultithreadedBench::new(duspatch.clone());
                     let elapsed = bench
                         .thread(move || {
                             for n in 0..i {
@@ -95,7 +96,8 @@ fn mk_dispatch() -> tracing::Dispatch {
 fn bench_event(c: &mut Criterion) {
     bench_thrpt(c, "event", |group, i| {
         group.bench_with_input(BenchmarkId::new("root/single_threaded", i), i, |b, &i| {
-            tracing::dispatcher::with_default(&mk_dispatch(), || {
+            let dispatch = mk_dispatch();
+            tracing::dispatcher::with_default(&dispatch, || {
                 b.iter(|| {
                     for n in 0..i {
                         tracing::info!(n);
@@ -106,8 +108,9 @@ fn bench_event(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("root/multithreaded", i), i, |b, &i| {
             b.iter_custom(|iters| {
                 let mut total = Duration::from_secs(0);
+                let dispatch = mk_dispatch();
                 for _ in 0..iters {
-                    let bench = MultithreadedBench::new(mk_dispatch());
+                    let bench = MultithreadedBench::new(dispatch.clone());
                     let elapsed = bench
                         .thread(move || {
                             for n in 0..i {
@@ -156,8 +159,9 @@ fn bench_event(c: &mut Criterion) {
             |b, &i| {
                 b.iter_custom(|iters| {
                     let mut total = Duration::from_secs(0);
+                    let dispatch = mk_dispatch();
                     for _ in 0..iters {
-                        let bench = MultithreadedBench::new(mk_dispatch());
+                        let bench = MultithreadedBench::new(dispatch.clone());
                         let elapsed = bench
                             .thread_with_setup(move |start| {
                                 let span = tracing::info_span!("unique_parent", foo = false);
@@ -203,13 +207,13 @@ fn bench_event(c: &mut Criterion) {
             i,
             |b, &i| {
                 b.iter_custom(|iters| {
+                    let dispatch = mk_dispatch();
                     let mut total = Duration::from_secs(0);
                     for _ in 0..iters {
-                        let dispatch = mk_dispatch();
                         let parent = tracing::dispatcher::with_default(&dispatch, || {
                             tracing::info_span!("shared_parent", foo = "hello world")
                         });
-                        let bench = MultithreadedBench::new(dispatch);
+                        let bench = MultithreadedBench::new(dispatch.clone());
                         let parent2 = parent.clone();
                         bench.thread_with_setup(move |start| {
                             let _guard = parent2.enter();
