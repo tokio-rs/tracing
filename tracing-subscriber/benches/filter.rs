5eefac45a01bc4e45dc0c8e7356c99946fd7c8bd
David Palm <dvdplm@gmail.com>
2020-11-05 13:09:10 +0100

    Add benchmarks for a reloadable `Collector`

--- tracing-subscriber/benches/filter.rs
+++ tracing-subscriber/benches/filter.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::time::Duration;
 use tracing::{dispatch::Dispatch, span, Event, Id, Metadata};
-use tracing_subscriber::{prelude::*, EnvFilter};
+use tracing_subscriber::{prelude::*, EnvFilter, reload};
 
 mod support;
 use support::MultithreadedBench;
@@ -54,6 +54,7 @@ fn bench_static(c: &mut Criterion) {
             })
         });
     });
+
     group.bench_function("single_threaded", |b| {
         let filter = "static_filter=info"
             .parse::<EnvFilter>()
@@ -67,11 +68,45 @@ fn bench_static(c: &mut Criterion) {
             })
         });
     });
+
+    group.bench_function("single_threaded_reload", |b| {
+        let filter = "static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
+            b.iter(|| {
+                tracing::info!(target: "static_filter", "hi");
+                tracing::debug!(target: "static_filter", "hi");
+                tracing::warn!(target: "static_filter", "hi");
+                tracing::trace!(target: "foo", "hi");
+            })
+        });
+    });
+
     group.bench_function("enabled_one", |b| {
         let filter = "static_filter=info"
             .parse::<EnvFilter>()
             .expect("should parse");
         tracing::collect::with_default(EnabledSubscriber.with(filter), || {
+            b.iter(|| {
+                tracing::info!(target: "static_filter", "hi");
+            })
+        });
+    });
+
+    group.bench_function("enabled_one_reload", |b| {
+        let filter = "static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+
+        tracing::collect::with_default(collector, || {
             b.iter(|| {
                 tracing::info!(target: "static_filter", "hi");
             })
@@ -87,12 +122,38 @@ fn bench_static(c: &mut Criterion) {
             })
         });
     });
+    group.bench_function("enabled_many_reload", |b| {
+        let filter = "foo=debug,bar=trace,baz=error,quux=warn,static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
+            b.iter(|| {
+                tracing::info!(target: "static_filter", "hi");
+            })
+        });
+    });
     group.bench_function("disabled_level_one", |b| {
         let filter = "static_filter=info"
             .parse::<EnvFilter>()
             .expect("should parse");
         tracing::collect::with_default(EnabledSubscriber.with(filter), || {
             b.iter(|| {
+                tracing::debug!(target: "static_filter", "hi");
+            })
+        });
+    });
+    group.bench_function("disabled_level_one_reload", |b| {
+        let filter = "static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
+                b.iter(|| {
                 tracing::debug!(target: "static_filter", "hi");
             })
         });
@@ -107,9 +168,33 @@ fn bench_static(c: &mut Criterion) {
             })
         });
     });
+    group.bench_function("disabled_level_many_reload", |b| {
+        let filter = "foo=debug,bar=info,baz=error,quux=warn,static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
+                b.iter(|| {
+                tracing::trace!(target: "static_filter", "hi");
+            })
+        });
+    });
     group.bench_function("disabled_one", |b| {
         let filter = "foo=info".parse::<EnvFilter>().expect("should parse");
         tracing::collect::with_default(EnabledSubscriber.with(filter), || {
+            b.iter(|| {
+                tracing::info!(target: "static_filter", "hi");
+            })
+        });
+    });
+    group.bench_function("disabled_one_reload", |b| {
+        let filter = "foo=info".parse::<EnvFilter>().expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
             b.iter(|| {
                 tracing::info!(target: "static_filter", "hi");
             })
@@ -121,6 +206,19 @@ fn bench_static(c: &mut Criterion) {
             .expect("should parse");
         tracing::collect::with_default(EnabledSubscriber.with(filter), || {
             b.iter(|| {
+                tracing::info!(target: "static_filter", "hi");
+            })
+        });
+    });
+    group.bench_function("disabled_many_reload", |b| {
+        let filter = "foo=debug,bar=trace,baz=error,quux=warn,whibble=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        tracing::collect::with_default(collector, || {
+                b.iter(|| {
                 tracing::info!(target: "static_filter", "hi");
             })
         });
@@ -155,6 +253,39 @@ fn bench_static(c: &mut Criterion) {
             .parse::<EnvFilter>()
             .expect("should parse");
         let dispatch = Dispatch::new(EnabledSubscriber.with(filter));
+        b.iter_custom(|iters| {
+            let mut total = Duration::from_secs(0);
+            for _ in 0..iters {
+                let bench = MultithreadedBench::new(dispatch.clone());
+                let elapsed = bench
+                    .thread(|| {
+                        tracing::info!(target: "static_filter", "hi");
+                    })
+                    .thread(|| {
+                        tracing::debug!(target: "static_filter", "hi");
+                    })
+                    .thread(|| {
+                        tracing::warn!(target: "static_filter", "hi");
+                    })
+                    .thread(|| {
+                        tracing::warn!(target: "foo", "hi");
+                    })
+                    .run();
+                total += elapsed;
+            }
+            total
+        });
+    });
+    group.bench_function("multithreaded_reload", |b| {
+        let filter = "static_filter=info"
+            .parse::<EnvFilter>()
+            .expect("should parse");
+
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+
+        let dispatch = Dispatch::new(collector);
         b.iter_custom(|iters| {
             let mut total = Duration::from_secs(0);
             for _ in 0..iters {
@@ -213,6 +344,25 @@ fn bench_dynamic(c: &mut Criterion) {
             })
         });
     });
+    group.bench_function("single_threaded_reload", |b| {
+        let filter = "[foo]=trace".parse::<EnvFilter>().expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+
+        tracing::collect::with_default(collector, || {
+            b.iter(|| {
+                tracing::info_span!("foo").in_scope(|| {
+                    tracing::info!("hi");
+                    tracing::debug!("hi");
+                });
+                tracing::info_span!("bar").in_scope(|| {
+                    tracing::warn!("hi");
+                });
+                tracing::trace!("hi");
+            })
+        });
+    });
     group.bench_function("baseline_multithreaded", |b| {
         let dispatch = Dispatch::new(EnabledSubscriber);
         b.iter_custom(|iters| {
@@ -247,6 +397,42 @@ fn bench_dynamic(c: &mut Criterion) {
     group.bench_function("multithreaded", |b| {
         let filter = "[foo]=trace".parse::<EnvFilter>().expect("should parse");
         let dispatch = Dispatch::new(EnabledSubscriber.with(filter));
+        b.iter_custom(|iters| {
+            let mut total = Duration::from_secs(0);
+            for _ in 0..iters {
+                let bench = MultithreadedBench::new(dispatch.clone());
+                let elapsed = bench
+                    .thread(|| {
+                        let span = tracing::info_span!("foo");
+                        let _ = span.enter();
+                        tracing::info!("hi");
+                    })
+                    .thread(|| {
+                        let span = tracing::info_span!("foo");
+                        let _ = span.enter();
+                        tracing::debug!("hi");
+                    })
+                    .thread(|| {
+                        let span = tracing::info_span!("bar");
+                        let _ = span.enter();
+                        tracing::debug!("hi");
+                    })
+                    .thread(|| {
+                        tracing::trace!("hi");
+                    })
+                    .run();
+                total += elapsed;
+            }
+            total
+        })
+    });
+
+    group.bench_function("multithreaded_reload", |b| {
+        let filter = "[foo]=trace".parse::<EnvFilter>().expect("should parse");
+        let (reloadeable_filter, h) = reload::Subscriber::new(filter);
+        let collector = EnabledSubscriber.with(reloadeable_filter);
+        criterion::black_box(h);
+        let dispatch = Dispatch::new(collector);
         b.iter_custom(|iters| {
             let mut total = Duration::from_secs(0);
             for _ in 0..iters {
