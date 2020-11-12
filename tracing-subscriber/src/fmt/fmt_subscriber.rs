8e5101fc3b00f1ca00f2d14e7bb2b1a1b8cc9a71
David <dvdplm@gmail.com>
2020-11-12 21:47:54 +0100

    subscriber: use default values for all Format type params (#1087)
    `format::Format` already has default values for the log format (`Full`)
    as well as time measuring gizmo (`SystemTime`) so no need to plug those
    in.

--- tracing-subscriber/src/fmt/fmt_subscriber.rs
+++ tracing-subscriber/src/fmt/fmt_subscriber.rs
@@ -58,12 +58,7 @@ use tracing_core::{
 ///
 /// [`Subscriber`]: subscribe::Subscribe
 #[derive(Debug)]
-pub struct Subscriber<
-    S,
-    N = format::DefaultFields,
-    E = format::Format<format::Full>,
-    W = fn() -> io::Stdout,
-> {
+pub struct Subscriber<S, N = format::DefaultFields, E = format::Format, W = fn() -> io::Stdout> {
     make_writer: W,
     fmt_fields: N,
     fmt_event: E,
