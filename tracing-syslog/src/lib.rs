a5ba26dedcf79a5a8556fbe726b17c87a90ef94f
mxheller <max.a.heller@gmail.com>
2020-12-14 14:09:48 -0500

    point to Syslog for examples

--- tracing-syslog/src/lib.rs
+++ tracing-syslog/src/lib.rs
@@ -1,3 +1,8 @@
+//! `syslog` logging support for [`tracing`] backed by `libc`'s
+//! [`syslog()`](libc::syslog) function.
+//!
+//! See [`Syslog`] for examples.
+
 #[cfg(unix)]
 mod syslog;
 #[cfg(unix)]
