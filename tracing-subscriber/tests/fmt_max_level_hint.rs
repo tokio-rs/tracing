40e57086e57383481971c8638e2d27095a2a3264
Eliza Weisman <eliza@buoyant.io>
2021-02-18 16:38:54 -0800

    rustfmt
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/tests/fmt_max_level_hint.rs
+++ tracing-subscriber/tests/fmt_max_level_hint.rs
@@ -1,10 +1,9 @@
-
 use tracing_subscriber::filter::LevelFilter;
 
 #[test]
 fn fmt_sets_max_level_hint() {
     tracing_subscriber::fmt()
-       .with_max_level(LevelFilter::DEBUG)
-       .init();
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
     assert_eq!(LevelFilter::current(), LevelFilter::DEBUG);
 }
