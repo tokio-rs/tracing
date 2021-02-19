0cdd5e88ac3478ae91223e9c3b53a1c3598a7893
Eliza Weisman <eliza@buoyant.io>
2021-02-19 13:25:55 -0800

    log: forward `LogTracer::enabled` to the subscriber  (#1254)
    PRs #1247, #1248, and #1251 improve `tracing`'s behavior for the `log`
    crate's `log_enabled!` macro when a max-level hint is set. However, this
    *doesn't* get us correct behavior when a particular target is not
    enabled but the level is permitted by the max-level filter. In this
    case, `log_enabled!` will still return `true` when using `LogTracer`,
    because it doesn't currently call `Subscriber::enabled` on the current
    subscriber in its `Log::enabled` method. Instead, `Subscriber::enabled`
    is only checked when actually recording an event.
    
    This means that when a target is disabled by a target-specific filter
    but it's below the max level, `log::log_enabled!` will erroneously
    return `true`. This also means that skipping disabled `log` records in
    similar cases will construct the `log::Record` struct even when it isn't
    necessary to do so.
    
    This PR improves this behavior by adding a call to `Subscriber::enabled`
    in `LogTracer`'s `Log::enabled` method. I've also added to the existing
    tests for filtering `log` records to ensure that we also handle the
    `log_enabled!` macro correctly.
    
    While I was here, I noticed that the `Log::log` method for `LogTracer`
    is somewhat inefficient --- it gets the current dispatcher *three*
    times, and checks `enabled` twice. Currently, we check if the event
    would be enabled, and then call the`format_trace` function, which *also*
    checks if the event is enabled, and then dispatches it if it is. This is
    not great. :/ I fixed this by moving the check-and-dispatch inside of a
    single `dispatcher::get_default` block, and removing the duplicate
    check.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/tests/filter_log.rs
+++ tracing-subscriber/tests/filter_log.rs
@@ -1,21 +1,36 @@
 mod support;
 use self::support::*;
-use tracing::{self, subscriber::with_default, Level};
+use tracing::{self, Level};
 use tracing_subscriber::{filter::EnvFilter, prelude::*};
+
+mod my_module {
+    pub(crate) fn test_records() {
+        log::trace!("this should be disabled");
+        log::info!("this shouldn't be");
+        log::debug!("this should be disabled");
+        log::warn!("this should be enabled");
+        log::warn!(target: "something else", "this shouldn't be enabled");
+        log::error!("this should be enabled too");
+    }
+
+    pub(crate) fn test_log_enabled() {
+        assert!(
+            log::log_enabled!(log::Level::Info),
+            "info should be enabled inside `my_module`"
+        );
+        assert!(
+            !log::log_enabled!(log::Level::Debug),
+            "debug should not be enabled inside `my_module`"
+        );
+        assert!(
+            log::log_enabled!(log::Level::Warn),
+            "warn should be enabled inside `my_module`"
+        );
+    }
+}
 
 #[test]
 fn log_is_enabled() {
-    mod my_module {
-        pub(crate) fn do_test() {
-            log::trace!("this should be disabled");
-            log::info!("this shouldn't be");
-            log::debug!("this should be disabled");
-            log::warn!("this should be enabled");
-            log::warn!(target: "something else", "this shouldn't be enabled");
-            log::error!("this should be enabled too");
-        }
-    }
-    tracing_log::LogTracer::init().expect("logger should be unset");
     let filter: EnvFilter = "filter_log::my_module=info"
         .parse()
         .expect("filter should parse");
@@ -25,11 +40,23 @@ fn log_is_enabled() {
         .event(event::mock().at_level(Level::ERROR))
         .done()
         .run_with_handle();
-    let subscriber = subscriber.with(filter);
 
-    with_default(subscriber, || {
-        my_module::do_test();
-    });
+    // Note: we have to set the global default in order to set the `log` max
+    // level, which can only be set once.
+    subscriber.with(filter).init();
+
+    my_module::test_records();
+    log::info!("this is disabled");
+
+    my_module::test_log_enabled();
+    assert!(
+        !log::log_enabled!(log::Level::Info),
+        "info should not be enabled outside `my_module`"
+    );
+    assert!(
+        !log::log_enabled!(log::Level::Warn),
+        "warn should not be enabled outside `my_module`"
+    );
 
     finished.assert_finished();
 }
