d9e8eceafda5d4f8c36f556bd9c468edb79b8dd3
Eliza Weisman <eliza@buoyant.io>
2021-04-30 16:10:23 -0700

    tracing: prepare to release 0.1.26 (#1383)
    # 0.1.26 (April 30, 2021)
    
    ### Fixed
    
    - **attributes**: Compatibility between `#[instrument]` and `async-trait`
      v0.1.43 and newer ([#1228])
    - Several documentation fixes ([#1305], [#1344])
    ### Added
    
    - `Subscriber` impl for `Box<dyn Subscriber + Send + Sync + 'static>`
      ([#1358])
    - `Subscriber` impl for `Arc<dyn Subscriber + Send + Sync + 'static>`
      ([#1374])
    - Symmetric `From` impls for existing `Into` impls on `span::Current`,
      `Span`, and `Option<Id>` ([#1335], [#1338])
    - `From<EnteredSpan>` implementation for `Option<Id>`, allowing
      `EnteredSpan` to be used in a `span!` macro's `parent:` field ([#1325])
    - `Attributes::fields` accessor that returns the set of fields defined
      on a span's `Attributes` ([#1331])
    
    Thanks to @Folyd, @nightmared, and new contributors @rmsc and @Fishrock123 for
    contributing to this release!
    
    [#1227]: https://github.com/tokio-rs/tracing/pull/1228
    [#1305]: https://github.com/tokio-rs/tracing/pull/1305
    [#1325]: https://github.com/tokio-rs/tracing/pull/1325
    [#1338]: https://github.com/tokio-rs/tracing/pull/1338
    [#1344]: https://github.com/tokio-rs/tracing/pull/1344
    [#1358]: https://github.com/tokio-rs/tracing/pull/1358
    [#1374]: https://github.com/tokio-rs/tracing/pull/1374
    [#1335]: https://github.com/tokio-rs/tracing/pull/1335
    [#1331]: https://github.com/tokio-rs/tracing/pull/1331

--- tracing/src/lib.rs
+++ tracing/src/lib.rs
@@ -802,7 +802,7 @@
 //!
 //!   ```toml
 //!   [dependencies]
-//!   tracing = { version = "0.1.25", default-features = false }
+//!   tracing = { version = "0.1.26", default-features = false }
 //!   ```
 //!
 //! <div class="information">
@@ -858,7 +858,7 @@
 //! [flags]: #crate-feature-flags
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg), deny(broken_intra_doc_links))]
-#![doc(html_root_url = "https://docs.rs/tracing/0.1.25")]
+#![doc(html_root_url = "https://docs.rs/tracing/0.1.26")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
     issue_tracker_base_url = "https://github.com/tokio-rs/tracing/issues/"
