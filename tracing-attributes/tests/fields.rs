b6c07e7eba8530bf155f167c320fe9ead99acfce
Folyd <lyshuhow@gmail.com>
2021-05-07 07:38:09 +0800

    attributes: don't record primitive types of the function arguments as `fmt::Debug` (#1378)
    ## Motivation
    
    The default behavior of `tracing::instrument` attribution will record
    all of the function arguments as `fmt::Debug`, which is overwhelmed and
    unnecessary for those primitive types, such as `bool`, `u8`, `i8`,
    `u16`, `i16`, `u32`, `i32`, `u64`, `i64`, `usize`, and `isize`. Another
    concerning reason is that we‘ll lose the type info of those primitive
    types when record by a `Visitor`, while those type infos is essential to
    some people. For example, I need to show my spans in Jaeger UI.
    
    ## Solution
    
    Make the `tracing::instrument` records other function arguments as
    `fmt::Debug ` while not for those primitive types.
    
    However, I'm not good at naming. Maybe the `RecordType` enum and its
    variant aren't a good name? I'd love to seek suggestions. Thanks.

--- tracing-attributes/tests/fields.rs
+++ tracing-attributes/tests/fields.rs
@@ -61,7 +61,7 @@ fn fields() {
 fn expr_field() {
     let span = span::mock().with_field(
         mock("s")
-            .with_value(&tracing::field::debug("hello world"))
+            .with_value(&"hello world")
             .and(mock("len").with_value(&"hello world".len()))
             .only(),
     );
@@ -74,7 +74,7 @@ fn expr_field() {
 fn two_expr_fields() {
     let span = span::mock().with_field(
         mock("s")
-            .with_value(&tracing::field::debug("hello world"))
+            .with_value(&"hello world")
             .and(mock("s.len").with_value(&"hello world".len()))
             .and(mock("s.is_empty").with_value(&false))
             .only(),
@@ -120,7 +120,7 @@ fn parameters_with_fields() {
     let span = span::mock().with_field(
         mock("foo")
             .with_value(&"bar")
-            .and(mock("param").with_value(&format_args!("1")))
+            .and(mock("param").with_value(&1u32))
             .only(),
     );
     run_test(span, || {
