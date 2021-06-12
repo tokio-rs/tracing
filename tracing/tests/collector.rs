fa68f95b2aa22b31b3df7e65574040e914c47534
Ole Schönburg <ole.schoenburg@gmail.com>
2021-06-12 11:14:47 +0200

    tracing: Fix already imported´span::Id´

--- tracing/tests/collector.rs
+++ tracing/tests/collector.rs
@@ -53,7 +53,7 @@ fn event_macros_dont_infinite_loop() {
 
         fn enter(&self, _: &Id) {}
 
-        fn exit(&self, _: &span::Id) {}
+        fn exit(&self, _: &Id) {}
 
         fn current_span(&self) -> tracing_core::span::Current {
             tracing_core::span::Current::unknown()
