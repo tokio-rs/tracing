d6298cd3e5b3417e88a73255874e53e3581636d5
Eliza Weisman <eliza@buoyant.io>
2021-06-25 13:30:37 -0700

    subscriber: add `MakeWriter` combinators (#1274)
    Depends on #1141.
    
    This branch adds a `MakeWriterExt` trait which adds a number of
    combinators for composing multiple types implementing `MakeWriter`.
    `MakeWriter`s can now be teed together, filtered with minimum and
    maximum levels, filtered with a `Metadata` predicate, and combined with
    a fallback for when a writer is _not_ made for a particular `Metadata`.
    
    I've also added a `MakeWriter` impl for `std::fs::File`, because `&File`
    implements `Write`. Ideally, we'd have a default impl of `MakeWriter`
    for `T where &T: Write`, but that's not possible due to the impl of
    `MakeWriter` for `F: Fn() -> T: Write`. We could add a generic `by_ref`
    adapter for any `T where &T: Write`, though...
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/prelude.rs
+++ tracing-subscriber/src/prelude.rs
@@ -6,3 +6,7 @@
 pub use crate::field::{MakeExt as _, RecordFields as _};
 pub use crate::subscribe::{CollectExt as _, Subscribe as _};
 pub use crate::util::SubscriberInitExt as _;
+
+#[cfg(feature = "fmt")]
+#[cfg_attr(docsrs, doc(cfg(feature = "fmt")))]
+pub use crate::fmt::writer::MakeWriterExt as _;
