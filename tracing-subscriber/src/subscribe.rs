d8a136e34cc8d778d08c9d23aafe51e81de67148
Eliza Weisman <eliza@buoyant.io>
2021-08-13 12:13:40 -0700

    moar tests
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/subscribe.rs
+++ tracing-subscriber/src/subscribe.rs
@@ -1244,165 +1244,169 @@ impl Identity {
 
 #[cfg(test)]
 pub(crate) mod tests {
-    // use std::sync::{Arc, Mutex};
+    use std::sync::{Arc, Mutex};
 
-    // use super::*;
+    use super::*;
 
-    // pub(crate) struct NopCollector;
+    pub(crate) struct NopCollector;
 
-    // impl Collect for NopCollector {
-    //     fn register_callsite(&self, _: &'static Metadata<'static>) -> Interest {
-    //         Interest::never()
-    //     }
+    impl Collect for NopCollector {
+        fn register_callsite(&self, _: &'static Metadata<'static>) -> Interest {
+            Interest::never()
+        }
 
-    //     fn enabled(&self, _: &Metadata<'_>) -> bool {
-    //         false
-    //     }
+        fn enabled(&self, _: &Metadata<'_>) -> bool {
+            false
+        }
 
-    //     fn new_span(&self, _: &span::Attributes<'_>) -> span::Id {
-    //         span::Id::from_u64(1)
-    //     }
+        fn new_span(&self, _: &span::Attributes<'_>) -> span::Id {
+            span::Id::from_u64(1)
+        }
 
-    //     fn record(&self, _: &span::Id, _: &span::Record<'_>) {}
-    //     fn record_follows_from(&self, _: &span::Id, _: &span::Id) {}
-    //     fn event(&self, _: &Event<'_>) {}
-    //     fn enter(&self, _: &span::Id) {}
-    //     fn exit(&self, _: &span::Id) {}
-    //     fn current_span(&self) -> span::Current {
-    //         span::Current::unknown()
-    //     }
-    // }
+        fn record(&self, _: &span::Id, _: &span::Record<'_>) {}
+        fn record_follows_from(&self, _: &span::Id, _: &span::Id) {}
+        fn event(&self, _: &Event<'_>) {}
+        fn enter(&self, _: &span::Id) {}
+        fn exit(&self, _: &span::Id) {}
+        fn current_span(&self) -> span::Current {
+            span::Current::unknown()
+        }
+    }
 
-    // struct NopSubscriber;
-    // impl<C: Collect> Subscribe<C> for NopSubscriber {}
+    struct NopSubscriber;
+    impl<C: Collect> Subscribe<C> for NopSubscriber {}
 
-    // #[allow(dead_code)]
-    // struct NopSubscriber2;
-    // impl<C: Collect> Subscribe<C> for NopSubscriber2 {}
+    #[allow(dead_code)]
+    struct NopSubscriber2;
+    impl<C: Collect> Subscribe<C> for NopSubscriber2 {}
 
-    // /// A subscriber that holds a string.
-    // ///
-    // /// Used to test that pointers returned by downcasting are actually valid.
-    // struct StringSubscriber(String);
-    // impl<C: Collect> Subscribe<C> for StringSubscriber {}
-    // struct StringSubscriber2(String);
-    // impl<C: Collect> Subscribe<C> for StringSubscriber2 {}
+    /// A subscriber that holds a string.
+    ///
+    /// Used to test that pointers returned by downcasting are actually valid.
+    struct StringSubscriber(String);
+    impl<C: Collect> Subscribe<C> for StringSubscriber {}
+    struct StringSubscriber2(String);
+    impl<C: Collect> Subscribe<C> for StringSubscriber2 {}
 
-    // struct StringSubscriber3(String);
-    // impl<C: Collect> Subscribe<C> for StringSubscriber3 {}
+    struct StringSubscriber3(String);
+    impl<C: Collect> Subscribe<C> for StringSubscriber3 {}
 
-    // pub(crate) struct StringCollector(String);
+    pub(crate) struct StringCollector(String);
 
-    // impl Collect for StringCollector {
-    //     fn register_callsite(&self, _: &'static Metadata<'static>) -> Interest {
-    //         Interest::never()
-    //     }
+    impl Collect for StringCollector {
+        fn register_callsite(&self, _: &'static Metadata<'static>) -> Interest {
+            Interest::never()
+        }
 
-    //     fn enabled(&self, _: &Metadata<'_>) -> bool {
-    //         false
-    //     }
+        fn enabled(&self, _: &Metadata<'_>) -> bool {
+            false
+        }
 
-    //     fn new_span(&self, _: &span::Attributes<'_>) -> span::Id {
-    //         span::Id::from_u64(1)
-    //     }
+        fn new_span(&self, _: &span::Attributes<'_>) -> span::Id {
+            span::Id::from_u64(1)
+        }
 
-    //     fn record(&self, _: &span::Id, _: &span::Record<'_>) {}
-    //     fn record_follows_from(&self, _: &span::Id, _: &span::Id) {}
-    //     fn event(&self, _: &Event<'_>) {}
-    //     fn enter(&self, _: &span::Id) {}
-    //     fn exit(&self, _: &span::Id) {}
-    //     fn current_span(&self) -> span::Current {
-    //         span::Current::unknown()
-    //     }
-    // }
+        fn record(&self, _: &span::Id, _: &span::Record<'_>) {}
+        fn record_follows_from(&self, _: &span::Id, _: &span::Id) {}
+        fn event(&self, _: &Event<'_>) {}
+        fn enter(&self, _: &span::Id) {}
+        fn exit(&self, _: &span::Id) {}
+        fn current_span(&self) -> span::Current {
+            span::Current::unknown()
+        }
+    }
 
-    // fn assert_collector(_s: impl Collect) {}
+    fn assert_collector(_s: impl Collect) {}
 
-    // #[test]
-    // fn subscriber_is_collector() {
-    //     let s = NopSubscriber.with_collector(NopCollector);
-    //     assert_collector(s)
-    // }
+    #[test]
+    fn subscriber_is_collector() {
+        let s = NopSubscriber.with_collector(NopCollector);
+        assert_collector(s)
+    }
 
-    // #[test]
-    // fn two_subscribers_are_collector() {
-    //     let s = NopSubscriber
-    //         .and_then(NopSubscriber)
-    //         .with_collector(NopCollector);
-    //     assert_collector(s)
-    // }
+    #[test]
+    fn two_subscribers_are_collector() {
+        // let s = NopSubscriber
+        //     .and_then(NopSubscriber)
+        //     .with_collector(NopCollector);
+        // assert_collector(s)
 
-    // #[test]
-    // fn three_subscribers_are_collector() {
-    //     let s = NopSubscriber
-    //         .and_then(NopSubscriber)
-    //         .and_then(NopSubscriber)
-    //         .with_collector(NopCollector);
-    //     assert_collector(s)
-    // }
+        todo!("eliza")
+    }
 
-    // #[test]
-    // fn downcasts_to_collector() {
-    //     let s = NopSubscriber
-    //         .and_then(NopSubscriber)
-    //         .and_then(NopSubscriber)
-    //         .with_collector(StringCollector("collector".into()));
-    //     let collector =
-    //         <dyn Collect>::downcast_ref::<StringCollector>(&s).expect("collector should downcast");
-    //     assert_eq!(&collector.0, "collector");
-    // }
+    #[test]
+    fn three_subscribers_are_collector() {
+        // let s = NopSubscriber
+        //     .and_then(NopSubscriber)
+        //     .and_then(NopSubscriber)
+        //     .with_collector(NopCollector);
+        // assert_collector(s)
+        todo!("eliza")
+    }
 
-    // #[test]
-    // fn downcasts_to_subscriber() {
-    //     let s = StringSubscriber("subscriber_1".into())
-    //         .and_then(StringSubscriber2("subscriber_2".into()))
-    //         .and_then(StringSubscriber3("subscriber_3".into()))
-    //         .with_collector(NopCollector);
-    //     let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber>(&s)
-    //         .expect("subscriber 2 should downcast");
-    //     assert_eq!(&subscriber.0, "subscriber_1");
-    //     let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber2>(&s)
-    //         .expect("subscriber 2 should downcast");
-    //     assert_eq!(&subscriber.0, "subscriber_2");
-    //     let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber3>(&s)
-    //         .expect("subscriber 3 should downcast");
-    //     assert_eq!(&subscriber.0, "subscriber_3");
-    // }
+    #[test]
+    fn downcasts_to_collector() {
+        // let s = NopSubscriber
+        //     .and_then(NopSubscriber)
+        //     .and_then(NopSubscriber)
+        //     .with_collector(StringCollector("collector".into()));
+        // let collector =
+        //     <dyn Collect>::downcast_ref::<StringCollector>(&s).expect("collector should downcast");
+        // assert_eq!(&collector.0, "collector");
+        todo!("eliza")
+    }
 
-    // #[test]
-    // fn context_event_span() {
-    //     let last_event_span = Arc::new(Mutex::new(None));
+    #[test]
+    fn downcasts_to_subscriber() {
+        let s = crate::registry()
+            .with(StringSubscriber("subscriber_1".into()))
+            .with(StringSubscriber2("subscriber_2".into()))
+            .with(StringSubscriber3("subscriber_3".into()));
+        let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber>(&s)
+            .expect("subscriber 2 should downcast");
+        assert_eq!(&subscriber.0, "subscriber_1");
+        let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber2>(&s)
+            .expect("subscriber 2 should downcast");
+        assert_eq!(&subscriber.0, "subscriber_2");
+        let subscriber = <dyn Collect>::downcast_ref::<StringSubscriber3>(&s)
+            .expect("subscriber 3 should downcast");
+        assert_eq!(&subscriber.0, "subscriber_3");
+    }
 
-    //     struct RecordingSubscriber {
-    //         last_event_span: Arc<Mutex<Option<&'static str>>>,
-    //     }
+    #[test]
+    fn context_event_span() {
+        let last_event_span = Arc::new(Mutex::new(None));
 
-    //     impl<S> Subscribe<S> for RecordingSubscriber
-    //     where
-    //         S: Collect + for<'lookup> LookupSpan<'lookup>,
-    //     {
-    //         fn on_event(&self, event: &Event<'_>, ctx: Context<'_, S>) {
-    //             let span = ctx.event_span(event);
-    //             *self.last_event_span.lock().unwrap() = span.map(|s| s.name());
-    //         }
-    //     }
+        struct RecordingSubscriber {
+            last_event_span: Arc<Mutex<Option<&'static str>>>,
+        }
 
-    //     tracing::collect::with_default(
-    //         crate::registry().with(RecordingSubscriber {
-    //             last_event_span: last_event_span.clone(),
-    //         }),
-    //         || {
-    //             tracing::info!("no span");
-    //             assert_eq!(*last_event_span.lock().unwrap(), None);
+        impl<S> Subscribe<S> for RecordingSubscriber
+        where
+            S: Collect + for<'lookup> LookupSpan<'lookup>,
+        {
+            fn on_event(&self, event: &Event<'_>, ctx: Context<'_, S>) {
+                let span = ctx.event_span(event);
+                *self.last_event_span.lock().unwrap() = span.map(|s| s.name());
+            }
+        }
 
-    //             let parent = tracing::info_span!("explicit");
-    //             tracing::info!(parent: &parent, "explicit span");
-    //             assert_eq!(*last_event_span.lock().unwrap(), Some("explicit"));
+        tracing::collect::with_default(
+            crate::registry().with(RecordingSubscriber {
+                last_event_span: last_event_span.clone(),
+            }),
+            || {
+                tracing::info!("no span");
+                assert_eq!(*last_event_span.lock().unwrap(), None);
 
-    //             let _guard = tracing::info_span!("contextual").entered();
-    //             tracing::info!("contextual span");
-    //             assert_eq!(*last_event_span.lock().unwrap(), Some("contextual"));
-    //         },
-    //     );
-    // }
+                let parent = tracing::info_span!("explicit");
+                tracing::info!(parent: &parent, "explicit span");
+                assert_eq!(*last_event_span.lock().unwrap(), Some("explicit"));
+
+                let _guard = tracing::info_span!("contextual").entered();
+                tracing::info!("contextual span");
+                assert_eq!(*last_event_span.lock().unwrap(), Some("contextual"));
+            },
+        );
+    }
 }
