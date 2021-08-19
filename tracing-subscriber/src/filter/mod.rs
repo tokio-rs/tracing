00581341e321079ba68af6fe909d4ed2aac3c6fb
Eliza Weisman <eliza@buoyant.io>
2021-08-19 08:58:29 -0700

    wip

--- tracing-subscriber/src/filter/mod.rs
+++ tracing-subscriber/src/filter/mod.rs
@@ -11,3 +11,15 @@ pub use self::level::{LevelFilter, ParseError as LevelParseError};
 #[cfg(feature = "env-filter")]
 #[cfg_attr(docsrs, doc(cfg(feature = "env-filter")))]
 pub use self::env::*;
+
+use crate::layer::Context;
+use tracing_core::{subscriber::Interest, Metadata};
+
+/// A filter that determines whether a span or event is enabled.
+pub trait Filter<S> {
+    fn enabled(&self, meta: &Metadata<'_>, cx: Context<'_, S>) -> bool;
+    fn callsite_enabled(&self, meta: &'static Metadata<'static>, cx: Context<'_, S>) -> Interest;
+}
+
+#[derive(Clone, Debug)]
+pub struct FilterId(u64);
