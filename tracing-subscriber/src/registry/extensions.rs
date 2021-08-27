326d74f280a4394b4696a8137eb08f53ad626c8e
Eliza Weisman <eliza@buoyant.io>
2021-08-27 11:38:32 -0700

    subscriber: fix `Context`'s `LookupSpan` method feature flagging (#1525)
    The `layer::Context` type has additional methods that require the inner
    subscriber to implement `LookupSpan`. These currently are only present
    when the `registry` feature flag is enabled. However, this isn't
    actually necessary --- the `LookupSpan` trait always exists, and doesn't
    require the `registry` feature flag; that feature flag only controls
    whether the `Registry` _type_, a concrete implementation of
    `LookupSpan`, is enabled.
    
    This branch removes the `cfg` attributes from these methods.

--- tracing-subscriber/src/registry/extensions.rs
+++ tracing-subscriber/src/registry/extensions.rs
@@ -40,6 +40,7 @@ pub struct Extensions<'a> {
 }
 
 impl<'a> Extensions<'a> {
+    #[cfg(feature = "registry")]
     pub(crate) fn new(inner: RwLockReadGuard<'a, ExtensionsInner>) -> Self {
         Self { inner }
     }
@@ -57,6 +58,7 @@ pub struct ExtensionsMut<'a> {
 }
 
 impl<'a> ExtensionsMut<'a> {
+    #[cfg(feature = "registry")]
     pub(crate) fn new(inner: RwLockWriteGuard<'a, ExtensionsInner>) -> Self {
         Self { inner }
     }
@@ -117,6 +119,7 @@ pub(crate) struct ExtensionsInner {
 impl ExtensionsInner {
     /// Create an empty `Extensions`.
     #[inline]
+    #[cfg(any(test, feature = "registry"))]
     pub(crate) fn new() -> ExtensionsInner {
         ExtensionsInner {
             map: AnyMap::default(),
@@ -175,6 +178,7 @@ impl ExtensionsInner {
     ///
     /// This permits the hash map allocation to be pooled by the registry so
     /// that future spans will not need to allocate new hashmaps.
+    #[cfg(any(test, feature = "registry"))]
     pub(crate) fn clear(&mut self) {
         self.map.clear();
     }
