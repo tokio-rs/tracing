6c84f9755f5deefe1fc14f2136f85f50fe4f4778
Eliza Weisman <eliza@buoyant.io>
2021-08-29 12:47:38 -0700

    test for per layer filters on trees
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/tests/layer_filters.rs
+++ tracing-subscriber/tests/layer_filters.rs
@@ -313,6 +313,59 @@ fn filters_interleaved_span_scopes() {
     b_handle.assert_finished();
     all_handle.assert_finished();
 }
+
+#[test]
+fn basic_trees() {
+    let (with_target, with_target_handle) = layer::named("info_with_target")
+        .event(event::mock().at_level(Level::INFO).with_target("my_target"))
+        .done()
+        .run_with_handle();
+
+    let (info, info_handle) = layer::named("info")
+        .event(
+            event::mock()
+                .at_level(Level::INFO)
+                .with_target(module_path!()),
+        )
+        .event(event::mock().at_level(Level::INFO).with_target("my_target"))
+        .done()
+        .run_with_handle();
+
+    let (all, all_handle) = layer::named("all")
+        .event(
+            event::mock()
+                .at_level(Level::INFO)
+                .with_target(module_path!()),
+        )
+        .event(event::mock().at_level(Level::TRACE))
+        .event(event::mock().at_level(Level::INFO).with_target("my_target"))
+        .event(
+            event::mock()
+                .at_level(Level::TRACE)
+                .with_target("my_target"),
+        )
+        .done()
+        .run_with_handle();
+
+    let info_tree = info
+        .and_then(
+            with_target.with_filter(filter::filter_fn(|meta, _| meta.target() == "my_target")),
+        )
+        .with_filter(LevelFilter::INFO);
+    let _subscriber = tracing_subscriber::registry()
+        .with(info_tree)
+        .with(all)
+        .set_default();
+
+    tracing::info!("hello world");
+    tracing::trace!("hello trace");
+    tracing::info!(target: "my_target", "hi to my target");
+    tracing::trace!(target: "my_target", "hi to my target at trace");
+
+    with_target_handle.assert_finished();
+    info_handle.assert_finished();
+    all_handle.assert_finished();
+}
 mod dont_break_other_layers {
     use super::*;
 
