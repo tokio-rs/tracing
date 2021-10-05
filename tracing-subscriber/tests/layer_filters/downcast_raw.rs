fb2ffa11fedc5751d28fd5375e7edd49b4dfea9e
Bryan Burgers <bryan@burgers.io>
2021-10-05 14:51:10 -0500

    Simplify downcast_raw test and add basic test

--- tracing-subscriber/tests/layer_filters/downcast_raw.rs
+++ tracing-subscriber/tests/layer_filters/downcast_raw.rs
@@ -4,6 +4,31 @@ use tracing_subscriber::prelude::*;
 use tracing_subscriber::Layer;
 
 #[test]
+fn downcast_ref_to_inner_layer_and_filter() {
+    // Test that a filtered layer gives downcast_ref access to
+    // both the layer and the filter.
+
+    struct WrappedLayer;
+
+    impl<S> Layer<S> for WrappedLayer
+    where
+        S: Subscriber,
+        S: for<'lookup> tracing_subscriber::registry::LookupSpan<'lookup>,
+    {
+    }
+
+    let layer = WrappedLayer;
+    let filter = Targets::new().with_default(tracing::Level::INFO);
+    let registry = tracing_subscriber::registry().with(layer.with_filter(filter));
+    let dispatch = tracing::dispatcher::Dispatch::new(registry);
+
+    // The filter is available
+    assert!(dispatch.downcast_ref::<Targets>().is_some());
+    // The wrapped layer is available
+    assert!(dispatch.downcast_ref::<WrappedLayer>().is_some());
+}
+
+#[test]
 fn forward_downcast_raw_to_layer() {
     // Test that a filtered layer still gives its wrapped layer a chance to
     // return a custom struct from downcast_raw.
@@ -35,16 +60,9 @@ fn forward_downcast_raw_to_layer() {
         with_context: WithContext,
     };
     let filter = Targets::new().with_default(tracing::Level::INFO);
-    let _guard = tracing_subscriber::registry()
-        .with(layer.with_filter(filter))
-        .set_default();
+    let registry = tracing_subscriber::registry().with(layer.with_filter(filter));
+    let dispatch = tracing::dispatcher::Dispatch::new(registry);
 
-    let span = tracing::span!(tracing::Level::INFO, "span");
-    let _entered = span.enter();
-
-    let current_span = tracing::Span::current();
-    let found = current_span
-        .with_subscriber(|(_id, dispatch)| dispatch.downcast_ref::<WithContext>().is_some());
-
-    assert_eq!(found, Some(true));
+    // Types from a custom implementation of `downcast_raw` are available
+    assert!(dispatch.downcast_ref::<WithContext>().is_some());
 }
