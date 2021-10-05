7d2e431b90203e9f7eb00a2890a317d0e9f60384
xd009642 <danielmckenna93@gmail.com>
2021-10-05 21:14:32 +0100

    Make example work without valuable

--- examples/examples/valuable.rs
+++ examples/examples/valuable.rs
@@ -1,15 +1,24 @@
-#![cfg(tracing_unstable)]
-use tracing::{info_span, info, field::valuable};
+//! This example shows how a value may be recorded using valuable and as an alternative recording
+//! it without valuable but just using the debug printout. Valuable is more flexible however, as
+//! a user can implement a custom `tracing_core::Visit` and implement
+//! `tracing_core::Visit::record_value` to handle the valuable and extract the fields, whereas with
+//! the debug printout you'd need to parse the string to extract the values.
+use tracing::{info_span, info};
+#[cfg(tracing_unstable)]
 use valuable::Valuable;
+#[cfg(tracing_unstable)]
+use tracing::field::valuable;
 
-#[derive(Clone, Debug, Valuable)]
+#[cfg_attr(tracing_unstable, derive(Valuable))]
+#[derive(Clone, Debug)]
 struct User {
     name: String,
     age: u32,
     address: Address, 
 }
 
-#[derive(Clone, Debug, Valuable)]
+#[cfg_attr(tracing_unstable, derive(Valuable))]
+#[derive(Clone, Debug)]
 struct Address {
     country: String,
     city: String,
@@ -17,6 +26,7 @@ struct Address {
 }
 
 
+#[cfg(tracing_unstable)]
 fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::TRACE)
@@ -38,4 +48,23 @@ fn main() {
 }
 
 
+#[cfg(not(tracing_unstable))]
+fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
 
+    let user = User {
+        name: "Arwen Undomiel".to_string(),
+        age: 3000,
+        address: Address {
+            country: "Middle Earth".to_string(),
+            city: "Rivendell".to_string(),
+            street: "leafy lane".to_string(),
+        }
+    };
+
+    let span = info_span!("Processing", user = ?user); 
+    let _handle = span.enter();
+    info!("Nothing to do");
+}
