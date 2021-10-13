a22f6f858c6f64e37f5312a07e8b848bcad0a923
Ricky Han <github@rickyhan.com>
2021-10-13 13:46:50 -0700

    docs: add `tracing-etw` to Related Crates  (#1635)
    This PR adds tracing-etw which is a new crate.
    
    * Update README.md
    
    * Update lib.rs

--- tracing/src/lib.rs
+++ tracing/src/lib.rs
@@ -737,6 +737,7 @@
 //!  - [`tracing-tracy`] provides a way to collect [Tracy] profiles in instrumented
 //!    applications.
 //!  - [`tracing-elastic-apm`] provides a layer for reporting traces to [Elastic APM].
+//!  - [`tracing-etw`] provides a layer for emitting Windows [ETW] events.
 //!
 //! If you're the maintainer of a `tracing` ecosystem crate not listed above,
 //! please let us know! We'd love to add your project to the list!
@@ -765,6 +766,8 @@
 //! [Tracy]: https://github.com/wolfpld/tracy
 //! [`tracing-elastic-apm`]: https://crates.io/crates/tracing-elastic-apm
 //! [Elastic APM]: https://www.elastic.co/apm
+//! [`tracing-etw`]: https://github.com/microsoft/tracing-etw
+//! [ETW]: https://docs.microsoft.com/en-us/windows/win32/etw/about-event-tracing
 //!
 //! <pre class="ignore" style="white-space:normal;font:inherit;">
 //!     <strong>Note</strong>: Some of these ecosystem crates are currently
