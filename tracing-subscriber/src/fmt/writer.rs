6219cc7f2b09058d515e3ae9200773ac6989e5e6
Eliza Weisman <eliza@buoyant.io>
2021-10-18 13:42:44 -0700

    subscriber: replace `chrono` with `time` for timestamp formatting (#1646)
    ## Motivation
    
    Currently, `tracing-subscriber` supports the `chrono` crate for
    timestamp formatting, via a default-on feature flag. When this code was
    initially added to `tracing-subscriber`, the `time` crate did not have
    support for the timestamp formatting options we needed.
    
    Unfortunately, the `chrono` crate's maintainance status is now in
    question (see #1598). Furthermore, `chrono` depends on version 0.1 of
    the `time` crate, which contains a security vulnerability
    (https://rustsec.org/advisories/RUSTSEC-2020-0071.html). This
    vulnerability is fixed in more recent releases of `time`, but `chrono`
    still uses v0.1.
    
    ## Solution
    
    Fortunately, the `time` crate now has its own timestamp formatting
    support.
    
    This branch replaces the `ChronoLocal` and `ChronoUtc` timestamp
    formatters with new `LocalTime` and `UtcTime` formatters. These
    formatters use the `time` crate's formatting APIs rather than
    `chrono`'s. This removes the vulnerable dependency on `time` 0.1
    
    Additionally, the new `time` APIs are feature flagged as an _opt-in_
    feature, rather than as an _opt-out_ feature. This should make it easier
    to avoid accidentally depending on the `time` crate when more
    sophisticated timestamp formatting is _not_ required.
    
    In a follow-up branch, we could also add support for `humantime` as an
    option for timestamp formatting.
    
    Naturally, since this removes existing APIs, this is a breaking change,
    and will thus require publishing `tracing-subscriber` 0.3. We'll want to
    do some other breaking changes as well.
    
    Fixes #1598.

--- tracing-subscriber/src/fmt/writer.rs
+++ tracing-subscriber/src/fmt/writer.rs
@@ -2,7 +2,7 @@
 //!
 //! [`io::Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
 use std::{
-    fmt::Debug,
+    fmt::{self, Debug},
     io::{self, Write},
     sync::{Arc, Mutex, MutexGuard},
 };
@@ -659,6 +659,17 @@ pub struct MutexGuardWriter<'a, W>(MutexGuard<'a, W>);
 #[derive(Clone, Debug)]
 pub struct ArcWriter<W>(Arc<W>);
 
+/// A bridge between `fmt::Write` and `io::Write`.
+///
+/// This is used by the timestamp formatting implementation for the `time`
+/// crate and by the JSON formatter. In both cases, this is needed because
+/// `tracing-subscriber`'s `FormatEvent`/`FormatTime` traits expect a
+/// `fmt::Write` implementation, while `serde_json::Serializer` and `time`'s
+/// `format_into` methods expect an `io::Write`.
+pub(in crate::fmt) struct WriteAdaptor<'a> {
+    fmt_write: &'a mut dyn fmt::Write,
+}
+
 impl<'a, F, W> MakeWriter<'a> for F
 where
     F: Fn() -> W,
@@ -1146,6 +1157,36 @@ where
     }
 }
 
+// === impl WriteAdaptor ===
+
+impl<'a> WriteAdaptor<'a> {
+    pub(in crate::fmt) fn new(fmt_write: &'a mut dyn fmt::Write) -> Self {
+        Self { fmt_write }
+    }
+}
+
+impl<'a> io::Write for WriteAdaptor<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let s =
+            std::str::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+        self.fmt_write
+            .write_str(s)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        Ok(s.as_bytes().len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> fmt::Debug for WriteAdaptor<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad("WriteAdaptor { .. }")
+    }
+}
 // === blanket impls ===
 
 impl<'a, M> MakeWriterExt<'a> for M where M: MakeWriter<'a> {}
