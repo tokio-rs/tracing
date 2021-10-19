226f605900838d3a0e0db5d50359e667f530f33b
Eliza Weisman <eliza@buoyant.io>
2021-10-19 14:55:53 -0700

    subscriber: add minimal `#![no_std]` support (#1648)
    Depends on #1649 
    
    ## Motivation
    
    Presently, the `tracing-subscriber` crate requires the Rust standard
    library and doesn't build with `#![no_std]` targets. For the most part,
    this is fine, as much of `tracing-subscriber` inherently depends on
    `std` APIs.
    
    However, `tracing-subscriber` also contains some key abstractions that
    are necessary for interoperability: the `Subscriber` and `LookupSpan`
    traits. Since these traits are in `tracing-subscriber`, `no-std` users
    cannot currently access them.
    
    Some of the other utilities in this crate, such as the field visitor
    combinators, may also be useful for `#![no_std]` projects.
    
    ## Solution
    
    This branch adds "std" and "alloc" feature flags to
    `tracing-subscriber`, for conditionally enabling `libstd` and
    `liballoc`, respectively. The `registry`, `fmt`, `EnvFilter`, and
    `reload` APIs all require libstd, and cannot be implemented without it,
    but the core `Subscribe` and `LookupSpan` traits are now available with
    `#![no_std]`.
    
    Fixes #999 
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/lib.rs
+++ tracing-subscriber/src/lib.rs
@@ -22,6 +22,9 @@
 //!
 //! ## Feature Flags
 //!
+//! - `std`: Enables APIs that depend on the on the Rust standard library
+//!   (enabled by default).
+//! - `alloc`: Depend on [`liballoc`] (enabled by "std").
 //! - `env-filter`: Enables the [`EnvFilter`] type, which implements filtering
 //!   similar to the [`env_logger` crate].
 //! - `fmt`: Enables the [`fmt`] module, which provides a subscriber
@@ -46,6 +49,34 @@
 //! - [`parking_lot`]: Use the `parking_lot` crate's `RwLock` implementation
 //!   rather than the Rust standard library's implementation.
 //!
+//! ### `no_std` Support
+//!
+//! In embedded systems and other bare-metal applications, `tracing` can be
+//! used without requiring the Rust standard library, although some features are
+//! disabled. Although most of the APIs provided by `tracing-subscriber`, such
+//! as [`fmt`] and [`EnvFilter`], require the standard library, some
+//! functionality, such as the [`Subscriber`] trait, can still be used in
+//! `no_std` environments.
+//!
+//! The dependency on the standard library is controlled by two crate feature
+//! flags, "std", which enables the dependency on [`libstd`], and "alloc", which
+//! enables the dependency on [`liballoc`] (and is enabled by the "std"
+//! feature). These features are enabled by default, but `no_std` users can
+//! disable them using:
+//!
+//! ```toml
+//! # Cargo.toml
+//! tracing-subscriber = { version = "0.3", default-features = false }
+//! ```
+//!
+//! Additional APIs are available when [`liballoc`] is available. To enable
+//! `liballoc` but not `std`, use:
+//!
+//! ```toml
+//! # Cargo.toml
+//! tracing-subscriber = { version = "0.3", default-features = false, features = ["alloc"] }
+//! ```
+//!
 //! ## Supported Rust Versions
 //!
 //! Tracing is built against the latest stable release. The minimum supported
@@ -69,6 +100,7 @@
 //! [`env_logger` crate]: https://crates.io/crates/env_logger
 //! [`parking_lot`]: https://crates.io/crates/parking_lot
 //! [`time` crate]: https://crates.io/crates/time
+//! [`liballoc`]: https://doc.rust-lang.org/alloc/index.html
 #![doc(html_root_url = "https://docs.rs/tracing-subscriber/0.2.12")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
@@ -103,43 +135,51 @@
 // future, reducing diff noise. Allow this even though clippy considers it
 // "needless".
 #![allow(clippy::needless_update)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
 
 #[macro_use]
 mod macros;
 
 pub mod field;
 pub mod filter;
-#[cfg(feature = "fmt")]
-#[cfg_attr(docsrs, doc(cfg(feature = "fmt")))]
-pub mod fmt;
 pub mod prelude;
 pub mod registry;
-pub mod reload;
+
 pub mod subscribe;
-pub(crate) mod sync;
 pub mod util;
 
-#[cfg(feature = "env-filter")]
-#[cfg_attr(docsrs, doc(cfg(feature = "env-filter")))]
-pub use filter::EnvFilter;
+feature! {
+    #![feature = "std"]
+    pub mod reload;
+    pub(crate) mod sync;
+}
+
+feature! {
+    #![all(feature = "fmt", feature = "std")]
+    pub mod fmt;
+    pub use fmt::fmt;
+    pub use fmt::Subscriber as FmtSubscriber;
+}
+
+feature! {
+    #![all(feature = "env-filter", feature = "std")]
+    pub use filter::EnvFilter;
+}
 
 pub use subscribe::Subscribe;
 
-cfg_feature!("fmt", {
-    pub use fmt::fmt;
-    pub use fmt::Subscriber as FmtSubscriber;
-});
-
-cfg_feature!("registry", {
+feature! {
+    #![all(feature = "registry", feature = "std")]
     pub use registry::Registry;
 
     ///
     pub fn registry() -> Registry {
         Registry::default()
     }
-});
-
-use std::default::Default;
+}
 
 mod sealed {
     pub trait Sealed<A = ()> {}
