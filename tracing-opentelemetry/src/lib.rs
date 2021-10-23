367b4bd90830733298a2c25a3a77e4c96bf42ae8
Eliza Weisman <eliza@buoyant.io>
2021-10-23 11:47:08 -0700

    opentelemetry: prepare to release v0.16.0
    # 0.16.0 (October 23, 2021)
    
    ### Breaking Changes
    
    - Upgrade to `v0.3.0` of `tracing-subscriber` ([#1677]) For list of
      breaking changes in `tracing-subscriber`, see the [v0.3.0 changelog].
    
    ### Added
    
    - `OpenTelemetrySpanExt::add_link` method for adding a link between a
      `tracing` span and a provided OpenTelemetry `Context` ([#1516])
    
    Thanks to @LehMaxence for contributing to this release!
    
    [v0.3.0 changelog]: https://github.com/tokio-rs/tracing/releases/tag/tracing-subscriber-0.3.0
    [#1516]: https://github.com/tokio-rs/tracing/pull/1516
    [#1677]: https://github.com/tokio-rs/tracing/pull/1677

--- tracing-opentelemetry/src/lib.rs
+++ tracing-opentelemetry/src/lib.rs
@@ -92,7 +92,7 @@
 //!
 #![deny(unreachable_pub)]
 #![cfg_attr(test, deny(warnings))]
-#![doc(html_root_url = "https://docs.rs/tracing-opentelemetry/0.15.0")]
+#![doc(html_root_url = "https://docs.rs/tracing-opentelemetry/0.16.0")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
     issue_tracker_base_url = "https://github.com/tokio-rs/tracing/issues/"
