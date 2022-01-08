b91ee934f8134e7e8bfe500c74908a27e9eaaab8
Tamme Schichler <tamme@schichler.dev>
2022-01-08 21:10:26 +0100

    add tests for remaining `#[instrument(tracing)]` code paths

--- tracing-attributes/tests/tracing_override.rs
+++ tracing-attributes/tests/tracing_override.rs
@@ -14,20 +14,59 @@ pub fn plain() {}
 #[instrument(tracing = ::tracing)]
 pub async fn async_fn() {}
 
+#[derive(Debug)]
+pub struct Custom;
+
+#[instrument(tracing = ::tracing)]
+pub fn implicit_field_debug(custom: Custom) {}
+
 #[instrument(tracing = ::tracing, level = "error")]
 pub fn level_error() {}
-
 #[instrument(tracing = ::tracing, level = "warn")]
 pub fn level_warn() {}
-
 #[instrument(tracing = ::tracing, level = "info")]
 pub fn level_info() {}
-
 #[instrument(tracing = ::tracing, level = "debug")]
 pub fn level_debug() {}
-
 #[instrument(tracing = ::tracing, level = "trace")]
 pub fn level_trace() {}
 
-#[instrument(tracing = ::tracing, fields(empty))]
+#[instrument(tracing = ::tracing, level = 5)]
+pub fn level_5() {}
+#[instrument(tracing = ::tracing, level = 4)]
+pub fn level_4() {}
+#[instrument(tracing = ::tracing, level = 3)]
+pub fn level_3() {}
+#[instrument(tracing = ::tracing, level = 2)]
+pub fn level_2() {}
+#[instrument(tracing = ::tracing, level = 1)]
+pub fn level_1() {}
+
+#[::tracing::instrument(tracing = ::tracing, fields(empty))]
 pub fn empty() {}
+
+#[::tracing::instrument(tracing = ::tracing, ret)]
+pub fn ret() {}
+
+#[instrument(tracing = ::tracing, ret(Debug))]
+pub fn ret_debug() {}
+
+#[instrument(tracing = ::tracing, ret(Display))]
+pub fn ret_display() -> &'static str {
+    ""
+}
+
+#[instrument(tracing = ::tracing, err)]
+pub fn err() -> Result<(), &'static str> {
+    Ok(())
+}
+
+#[instrument(tracing = ::tracing, err(Debug))]
+pub fn err_debug() -> Result<(), &'static str> {
+    Ok(())
+}
+
+#[instrument(tracing = ::tracing, err(Display))]
+pub fn err_display() -> Result<(), &'static str> {
+    Ok(())
+}
