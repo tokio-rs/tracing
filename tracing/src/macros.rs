55e6c9841dbad6bc753ce969b4d82f90613d5b7c
nicolaasg <87317085+nicolaasg@users.noreply.github.com>
2022-02-16 21:58:22 +0200

    tracing: fix wrong log level in `warn!` macro (#1930)
    ## Motivation
    
    I was scrolling through the code in `tracing/src/macros.rs` and noticed
    a possibly wrong log level in the `warn!` macro:
    
    https://github.com/tokio-rs/tracing/blob/4e65750b13721fee7a7ac05b053e1b9c3d21244f/tracing/src/macros.rs#L1698
    
    ## Solution
    
    This branch changes the level to `Level::WARN` in the incorrect macro
     arm.
    
    Fixes: #1929
    
    Co-authored-by: Nicolaas Geldenhuys <nicolaas.geldenhuys@cartrack.com>

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -1695,7 +1695,7 @@ macro_rules! warn {
     (%$($k:ident).+ = $($field:tt)*) => (
         $crate::event!(
             target: module_path!(),
-            $crate::Level::TRACE,
+            $crate::Level::WARN,
             { %$($k).+ = $($field)*}
         )
     );
