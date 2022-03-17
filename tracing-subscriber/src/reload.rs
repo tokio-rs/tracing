cd12dae73b863d2ad8c3f0d2d5e2c27e8164b7f5
Timo Freiberg <timo.freiberg@gmail.com>
2022-03-17 22:18:07 +0100

    subscriber: add example for reloading a global filtering layer (#1971)
    Missing example was noticed in
    https://github.com/tokio-rs/tracing/pull/1959#discussion_r816248186
    
    Should the descriptions of the examples distinguish global filtering and
    per-layer filtering a bit more explicitly?
    
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/reload.rs
+++ tracing-subscriber/src/reload.rs
@@ -11,24 +11,45 @@
 //!
 //! # Examples
 //!
-//! Reloading a [`Filtered`](crate::filter::Filtered) layer to change the filter at runtime.
+//! Reloading a [global filtering](crate::layer#global-filtering) layer:
 //!
-//! ```
+//! ```rust
 //! # use tracing::info;
-//! # use tracing_subscriber::{filter,fmt,reload,Registry,prelude::*};
-//! # fn main() {
+//! use tracing_subscriber::{filter, fmt, reload, prelude::*};
+//! let filter = filter::LevelFilter::WARN;
+//! let (filter, reload_handle) = reload::Layer::new(filter);
+//! tracing_subscriber::registry()
+//!   .with(filter)
+//!   .with(fmt::Layer::default())
+//!   .init();
+//! #
+//! # // specifying the Registry type is required
+//! # let _: &reload::Handle<filter::LevelFilter, tracing_subscriber::Registry> = &reload_handle;
+//! #
+//! info!("This will be ignored");
+//! reload_handle.modify(|filter| *filter = filter::LevelFilter::INFO);
+//! info!("This will be logged");
+//! ```
+//!
+//! Reloading a [`Filtered`](crate::filter::Filtered) layer:
+//!
+//! ```rust
+//! # use tracing::info;
+//! use tracing_subscriber::{filter, fmt, reload, prelude::*};
 //! let filtered_layer = fmt::Layer::default().with_filter(filter::LevelFilter::WARN);
 //! let (filtered_layer, reload_handle) = reload::Layer::new(filtered_layer);
 //! #
 //! # // specifying the Registry type is required
-//! # let _: &reload::Handle<filter::Filtered<fmt::Layer<Registry>,
-//! # filter::LevelFilter, Registry>,Registry>
+//! # let _: &reload::Handle<filter::Filtered<fmt::Layer<tracing_subscriber::Registry>,
+//! # filter::LevelFilter, tracing_subscriber::Registry>,tracing_subscriber::Registry>
 //! # = &reload_handle;
 //! #
+//! tracing_subscriber::registry()
+//!   .with(filtered_layer)
+//!   .init();
 //! info!("This will be ignored");
 //! reload_handle.modify(|layer| *layer.filter_mut() = filter::LevelFilter::INFO);
 //! info!("This will be logged");
-//! # }
 //! ```
 //!
 //! [`Layer` type]: struct.Layer.html
