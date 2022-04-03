c001fea1b4dafd116af21683b67bf237e32087c9
CAD97 <cad97@cad97.com>
2022-04-03 07:05:22 -0500

    Remove 'static requirement from Metadata::name
    Yes, I'm as surprised as you are that this causes 0 issues.

--- tracing-core/src/metadata.rs
+++ tracing-core/src/metadata.rs
@@ -56,7 +56,7 @@ use core::{
 /// [callsite identifiers]: Self::callsite
 pub struct Metadata<'a> {
     /// The name of the span described by this metadata.
-    name: &'static str,
+    name: &'a str,
 
     /// The part of the system that the span that this metadata describes
     /// occurred in.
@@ -252,7 +252,7 @@ impl<'a> Metadata<'a> {
     /// Construct new metadata for a span or event, with a name, target, level, field
     /// names, and optional source code location.
     pub const fn new(
-        name: &'static str,
+        name: &'a str,
         target: &'a str,
         level: Level,
         file: Option<&'a str>,
@@ -284,7 +284,7 @@ impl<'a> Metadata<'a> {
     }
 
     /// Returns the name of the span.
-    pub fn name(&self) -> &'static str {
+    pub fn name(&self) -> &'a str {
         self.name
     }
 
