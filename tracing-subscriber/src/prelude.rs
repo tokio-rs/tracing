647c6ae25251a636a314c25d9beae4e819344299
Eliza Weisman <eliza@buoyant.io>
2022-04-07 09:36:25 -0700

    chore: fix apparently-wrong unreachable_pub lints
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/prelude.rs
+++ tracing-subscriber/src/prelude.rs
@@ -3,6 +3,9 @@
 //! This brings into scope a number of extension traits that define methods on
 //! types defined here and in other crates.
 
+// apparently `as _` reexpoorts now generate `unreachable_pub` linting? which
+// seems wrong to me...
+#![allow(unreachable_pub)]
 pub use crate::field::{MakeExt as _, RecordFields as _};
 pub use crate::subscribe::{CollectExt as _, Subscribe as _};
 pub use crate::util::SubscriberInitExt as _;
