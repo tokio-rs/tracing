82c5672f879de8d04f51fdbd2788cf0dff9b46f9
David Barsky <me@davidbarsky.com>
2022-05-17 15:57:04 -0400

    switch to no-op writers in benchmarks

--- tracing-appender/benches/bench.rs
+++ tracing-appender/benches/bench.rs
@@ -1,90 +1,45 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use crossbeam_channel::{unbounded, Receiver, Sender};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::thread::JoinHandle;
-use std::time::Instant;
+use std::{
+    thread::{self, JoinHandle},
+    time::Instant,
+};
 use tracing::{event, Level};
 use tracing_appender::non_blocking;
 use tracing_subscriber::fmt::MakeWriter;
 
+// a no-op writer is used in order to measure the overhead incurred by
+// tracing-subscriber.
 #[derive(Clone)]
-struct SynchronousWriter {
-    writer: Arc<Mutex<Vec<u8>>>,
-}
+struct NoOpWriter;
 
-impl SynchronousWriter {
-    fn new() -> SynchronousWriter {
-        SynchronousWriter {
-            writer: Arc::new(Mutex::new(Vec::new())),
-        }
+impl NoOpWriter {
+    fn new() -> NoOpWriter {
+        NoOpWriter
     }
 }
 
-impl<'a> MakeWriter<'a> for SynchronousWriter {
-    type Writer = SynchronousWriter;
+impl<'a> MakeWriter<'a> for NoOpWriter {
+    type Writer = NoOpWriter;
 
     fn make_writer(&self) -> Self::Writer {
         self.clone()
     }
 }
 
-impl std::io::Write for SynchronousWriter {
+impl std::io::Write for NoOpWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let buf_len = buf.len();
-        match self.writer.lock() {
-            Ok(mut guard) => {
-                guard.extend_from_slice(buf);
-            }
-            Err(e) => {
-                eprintln!("Failed to acquire lock: {:?}", e);
-            }
-        }
-        Ok(buf_len)
+        Ok(buf.len())
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
-    }
-}
-
-/// A cheap writer so that we don't spam console if we had used stdout
-#[derive(Clone)]
-struct SilentWriter {
-    tx: Sender<String>,
-}
-
-impl SilentWriter {
-    fn new() -> (Self, Receiver<String>) {
-        let (tx, rx) = unbounded();
-        (Self { tx }, rx)
-    }
-}
-
-impl std::io::Write for SilentWriter {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let buf_len = buf.len();
-        let _ = self.tx.send(String::from_utf8_lossy(buf).to_string());
-        Ok(buf_len)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        Ok(())
-    }
-}
-
-impl<'a> MakeWriter<'a> for SilentWriter {
-    type Writer = SilentWriter;
-
-    fn make_writer(&self) -> Self::Writer {
-        self.clone()
     }
 }
 
 fn synchronous_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("synchronous");
     group.bench_function("single_thread", |b| {
-        let subscriber = tracing_subscriber::fmt().with_writer(SynchronousWriter::new());
+        let subscriber = tracing_subscriber::fmt().with_writer(NoOpWriter::new());
         tracing::collect::with_default(subscriber.finish(), || {
             b.iter(|| event!(Level::INFO, "event"))
         });
@@ -96,7 +51,7 @@ fn synchronous_benchmark(c: &mut Criterion) {
 
             let start = Instant::now();
 
-            let make_writer = SynchronousWriter::new();
+            let make_writer = NoOpWriter::new();
             let cloned_make_writer = make_writer.clone();
 
             handles.push(thread::spawn(move || {
@@ -130,8 +85,7 @@ fn non_blocking_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("non_blocking");
 
     group.bench_function("single_thread", |b| {
-        let (silent_writer, _rx) = SilentWriter::new();
-        let (non_blocking, _guard) = non_blocking(silent_writer);
+        let (non_blocking, _guard) = non_blocking(NoOpWriter::new());
         let subscriber = tracing_subscriber::fmt().with_writer(non_blocking);
 
         tracing::collect::with_default(subscriber.finish(), || {
@@ -141,8 +95,7 @@ fn non_blocking_benchmark(c: &mut Criterion) {
 
     group.bench_function("multiple_writers", |b| {
         b.iter_custom(|iters| {
-            let (silent_writer, _rx) = SilentWriter::new();
-            let (non_blocking, _guard) = non_blocking(silent_writer);
+            let (non_blocking, _guard) = non_blocking(NoOpWriter::new());
 
             let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
