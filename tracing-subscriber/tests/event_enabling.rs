952538f0af8b81a427ecb6a3e286db7c1b042180
Christopher Durham <cad97@cad97.com>
2022-06-14 20:45:39 -0400

    Ensure event_enabled is not called extra times

---
+++ tracing-subscriber/tests/event_enabling.rs
@@ -0,0 +1,35 @@
+use std::sync::{Arc, Mutex};
+use tracing::{collect::with_default, Collect};
+use tracing_subscriber::{prelude::*, registry, Subscribe};
+
+struct TrackingLayer {
+    event_enabled_count: Arc<Mutex<usize>>,
+}
+
+impl<C> Subscribe<C> for TrackingLayer
+where
+    C: Collect + Send + Sync + 'static,
+{
+    fn event_enabled(
+        &self,
+        _event: &tracing::Event<'_>,
+        _ctx: tracing_subscriber::subscribe::Context<'_, C>,
+    ) -> bool {
+        *self.event_enabled_count.lock().unwrap() += 1;
+        true
+    }
+}
+
+#[test]
+fn event_enabled_is_only_called_once() {
+    let event_enabled_count = Arc::new(Mutex::default());
+    let count = event_enabled_count.clone();
+    let collector = registry().with(TrackingLayer {
+        event_enabled_count,
+    });
+    with_default(collector, || {
+        tracing::error!("hiya!");
+    });
+
+    assert_eq!(1, *count.lock().unwrap());
+}
