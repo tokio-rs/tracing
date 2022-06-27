83d009872f8e1de6fc75a7436f525e774b63cc39
Christopher Durham <cad97@cad97.com>
2022-06-27 19:46:38 -0400

    ... but actually

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -829,6 +829,7 @@ macro_rules! event {
 /// }
 /// ```
 ///
+/// [`Collect::event_enabled`]: crate::Collect::event_enabled
 /// [`enabled!`]: crate::enabled
 /// [`span_enabled!`]: crate::span_enabled
 #[macro_export]
@@ -958,7 +959,6 @@ macro_rules! span_enabled {
 ///
 ///
 /// [`Metadata`]: crate::Metadata
-/// [`Collect::event_enabled`]: crate::Collect::event_enabled
 /// [`is_event`]: crate::Metadata::is_event
 /// [`is_span`]: crate::Metadata::is_span
 /// [`enabled!`]: crate::enabled
