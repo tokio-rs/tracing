7a9e1e4569971859849cfd546cc8412d762386a8
Christopher Durham <cad97@cad97.com>
2022-06-28 14:04:10 -0400

    tracing: document that `event_enabled!` does not call `Collect::event_enabled` (#2179)
    Although it shares the name, `event_enabled!` *does not* call
    `Collect::event_enabled`. `Collect::event_enabled` is only called once
    the event fields are available.

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -806,6 +806,9 @@ macro_rules! event {
 /// This is similar to [`enabled!`], but queries the current collector specifically for
 /// an event, whereas [`enabled!`] queries for an event _or_ span.
 ///
+/// Although it shares the name, this *does not* call [`Collect::event_enabled`].
+/// `Collect::event_enabled` is only called once the event fields are available.
+///
 /// See the documentation for [`enabled!]` for more details on using this macro.
 /// See also [`span_enabled!`].
 ///
@@ -826,6 +829,7 @@ macro_rules! event {
 /// }
 /// ```
 ///
+/// [`Collect::event_enabled`]: crate::Collect::event_enabled
 /// [`enabled!`]: crate::enabled
 /// [`span_enabled!`]: crate::span_enabled
 #[macro_export]
