d3e9f6b3d6b3554f1290a613e06ba85ed4a5c4a7
Gus Wynn <guswynn@gmail.com>
2022-06-30 15:44:11 -0700

    dont depend on with_filter

--- tracing-subscriber/tests/option.rs
+++ tracing-subscriber/tests/option.rs
@@ -1,63 +1,42 @@
 mod support;
 use self::support::*;
 use tracing::Collect;
-use tracing_subscriber::{filter, prelude::*, Subscribe};
 use tracing::{level_filters::LevelFilter, Level};
+use tracing_subscriber::{filter, prelude::*, Subscribe};
 
 // This test is just used to compare to the tests below
 #[test]
 fn just_subscriber() {
-    let info = subscriber::named("info")
-        .run()
-        .with_filter(LevelFilter::INFO)
-        .boxed();
-
-    let collector = tracing_subscriber::registry().with(info);
+    let collector = tracing_subscriber::registry().with(LevelFilter::INFO);
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::INFO));
 }
 
 #[test]
 fn subscriber_and_option_some_subscriber() {
-    let info = subscriber::named("info")
-        .run()
-        .with_filter(LevelFilter::INFO)
-        .boxed();
-
-    let debug = subscriber::named("debug")
-        .run()
-        .with_filter(LevelFilter::DEBUG)
-        .boxed();
-
-    let collector = tracing_subscriber::registry().with(info).with(Some(debug));
+    let collector = tracing_subscriber::registry()
+        .with(LevelFilter::INFO)
+        .with(Some(LevelFilter::DEBUG));
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::DEBUG));
 }
 
 #[test]
 fn subscriber_and_option_none_subscriber() {
-    let error = subscriber::named("error")
-        .run()
-        .with_filter(LevelFilter::ERROR)
-        .boxed();
     // None means the other layer takes control
     let collector = tracing_subscriber::registry()
-        .with(error)
-        .with(None::<ExpectSubscriber>);
+        .with(LevelFilter::ERROR)
+        .with(None::<LevelFilter>);
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::ERROR));
 }
 
 #[test]
 fn just_option_some_subscriber() {
     // Just a None means everything is off
-    let collector = tracing_subscriber::registry().with(None::<ExpectSubscriber>);
+    let collector = tracing_subscriber::registry().with(None::<LevelFilter>);
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::OFF));
 }
 
 #[test]
 fn just_option_none_subscriber() {
-    let error = subscriber::named("error")
-        .run()
-        .with_filter(LevelFilter::ERROR)
-        .boxed();
-    let collector = tracing_subscriber::registry().with(Some(error));
+    let collector = tracing_subscriber::registry().with(Some(LevelFilter::ERROR));
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::ERROR));
 }
--- tracing-subscriber/tests/vec.rs
+++ tracing-subscriber/tests/vec.rs
@@ -7,19 +7,14 @@ use tracing_subscriber::{filter, prelude::*, Subscribe};
 #[test]
 fn just_empty_vec() {
     // Just a None means everything is off
-    let collector = tracing_subscriber::registry().with(Vec::<ExpectSubscriber>::new());
+    let collector = tracing_subscriber::registry().with(Vec::<LevelFilter>::new());
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::OFF));
 }
 
 #[test]
 fn subscriber_and_empty_vec() {
-    let info = subscriber::named("info")
-        .run()
-        .with_filter(LevelFilter::INFO)
-        .boxed();
-
     let collector = tracing_subscriber::registry()
-        .with(info)
-        .with(Vec::<ExpectSubscriber>::new());
+        .with(LevelFilter::INFO)
+        .with(Vec::<LevelFilter>::new());
     assert_eq!(collector.max_level_hint(), Some(LevelFilter::INFO));
 }
