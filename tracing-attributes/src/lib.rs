f15165bddcb7f01146cbe8767fbfe16ab423de46
Eliza Weisman <eliza@buoyant.io>
2022-07-01 10:59:16 -0700

    attributes: prepare to release v0.1.22
    # 0.1.22 (July 1, 2022)
    
    This release fixes an issue where using the `err` or `ret` arguments to
    `#[instrument]` along with an overridden target, such as
    
    ```rust
    #[instrument(target = "...", err, ret)]
    ```
    
    would not propagate the overridden target to the events generated for
    errors/return values.
    
    ### Fixed
    
    - Error and return value events generated by `#[instrument(err)]` or
      `#[instrument(ret)]` not inheriting an overridden target (#2184)
    - Incorrect default level in documentation (#2119)
    
    Thanks to new contributor @tbraun96 for contributing to this release!

--- tracing-attributes/src/lib.rs
+++ tracing-attributes/src/lib.rs
@@ -16,7 +16,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! tracing-attributes = "0.1.21"
+//! tracing-attributes = "0.1.22"
 //! ```
 //!
 //! The [`#[instrument]`][instrument] attribute can now be added to a function
@@ -52,7 +52,7 @@
 //! supported compiler version is not considered a semver breaking change as
 //! long as doing so complies with this policy.
 //!
-#![doc(html_root_url = "https://docs.rs/tracing-attributes/0.1.21")]
+#![doc(html_root_url = "https://docs.rs/tracing-attributes/0.1.22")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/tracing/master/assets/logo-type.png",
     issue_tracker_base_url = "https://github.com/tokio-rs/tracing/issues/"
