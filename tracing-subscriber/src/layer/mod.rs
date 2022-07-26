a0824d398aa2511de28371d30dda9203360a6cf5
Jack Wrenn <jack@wrenn.fyi>
2022-07-26 18:24:01 -0400

    subscriber: impl `LookupSpan` for `Box<LS>` and `Arc<LS>` (#2247)
    These implementations mirror those provided by tracing-core for
    `Subscriber` on `Box<S>` and `Arc<S>`.

--- tracing-subscriber/src/layer/mod.rs
+++ tracing-subscriber/src/layer/mod.rs
@@ -685,7 +685,7 @@ use core::any::TypeId;
 
 feature! {
     #![feature = "alloc"]
-    use alloc::boxed::Box;
+    use alloc::{vec::Vec, boxed::Box};
     use core::ops::{Deref, DerefMut};
 }
 
@@ -1568,8 +1568,6 @@ where
 
 feature! {
     #![any(feature = "std", feature = "alloc")]
-    #[cfg(not(feature = "std"))]
-    use alloc::vec::Vec;
 
     macro_rules! layer_impl_body {
         () => {
