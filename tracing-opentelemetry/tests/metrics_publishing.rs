ff4ca28e550e2ff7976c197f8467bc65996916a5
Julian Tescher <jatescher@gmail.com>
2022-09-15 16:54:07 -0700

    opentelemetry: update otel to 0.18.0 (#2303)
    ## Motivation
    
    Support the latest OpenTelemetry specification.
    
    ## Solution
    
    Update `opentelemetry` to the latest `0.18.x` release. Breaking changes
    in the metrics spec have removed value recorders and added histograms so
    the metrics layer's `value.` prefix has been changed to `histogram.` and
    behaves accordingly. Additionally the `PushController` configuration for
    the metrics layer has been simplified to accept a `BasicController` that
    can act in either push or pull modes. Finally trace sampling in the
    sdk's `PreSampledTracer` impl has been updated to match the sampling
    logic in https://github.com/open-telemetry/opentelemetry-rust/pull/839.
    
    * Update MSRV to 1.56
    * Update examples
    * Fix async-trait dep
    # Conflicts:
    #	.github/workflows/CI.yml
    #	tracing-opentelemetry/Cargo.toml
    #	tracing-opentelemetry/src/layer.rs
    #	tracing-opentelemetry/src/metrics.rs
    #	tracing-opentelemetry/tests/metrics_publishing.rs

--- tracing-opentelemetry/tests/metrics_publishing.rs
+++ tracing-opentelemetry/tests/metrics_publishing.rs
@@ -1,26 +1,21 @@
-#![cfg(feature = "metrics")]
-use async_trait::async_trait;
-use futures_util::{Stream, StreamExt as _};
 use opentelemetry::{
-    metrics::{Descriptor, InstrumentKind},
-    metrics::{Number, NumberKind},
+    metrics::MetricsError,
     sdk::{
-        export::{
-            metrics::{
-                CheckpointSet, ExportKind, ExportKindFor, ExportKindSelector,
-                Exporter as MetricsExporter, Points, Sum,
-            },
-            trace::{SpanData, SpanExporter},
+        export::metrics::{
+            aggregation::{self, Histogram, Sum, TemporalitySelector},
+            InstrumentationLibraryReader,
         },
         metrics::{
-            aggregators::{ArrayAggregator, SumAggregator},
-            selectors::simple::Selector,
+            aggregators::{HistogramAggregator, SumAggregator},
+            controllers::BasicController,
+            processors,
+            sdk_api::{Descriptor, InstrumentKind, Number, NumberKind},
+            selectors,
         },
     },
-    Key, Value,
+    Context,
 };
 use std::cmp::Ordering;
-use std::time::Duration;
 use tracing::Subscriber;
 use tracing_opentelemetry::MetricsLayer;
 use tracing_subscriber::prelude::*;
@@ -30,7 +25,7 @@ const INSTRUMENTATION_LIBRARY_NAME: &str = "tracing/tracing-opentelemetry";
 
 #[tokio::test]
 async fn u64_counter_is_exported() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "hello_world".to_string(),
         InstrumentKind::Counter,
         NumberKind::U64,
@@ -40,11 +35,13 @@ async fn u64_counter_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(monotonic_counter.hello_world = 1_u64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
 async fn u64_counter_is_exported_i64_at_instrumentation_point() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "hello_world2".to_string(),
         InstrumentKind::Counter,
         NumberKind::U64,
@@ -54,11 +51,13 @@ async fn u64_counter_is_exported_i64_at_instrumentation_point() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(monotonic_counter.hello_world2 = 1_i64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
 async fn f64_counter_is_exported() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "float_hello_world".to_string(),
         InstrumentKind::Counter,
         NumberKind::F64,
@@ -68,11 +67,13 @@ async fn f64_counter_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(monotonic_counter.float_hello_world = 1.000000123_f64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
 async fn i64_up_down_counter_is_exported() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "pebcak".to_string(),
         InstrumentKind::UpDownCounter,
         NumberKind::I64,
@@ -82,11 +83,13 @@ async fn i64_up_down_counter_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(counter.pebcak = -5_i64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
 async fn i64_up_down_counter_is_exported_u64_at_instrumentation_point() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "pebcak2".to_string(),
         InstrumentKind::UpDownCounter,
         NumberKind::I64,
@@ -96,11 +99,13 @@ async fn i64_up_down_counter_is_exported_u64_at_instrumentation_point() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(counter.pebcak2 = 5_u64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
 async fn f64_up_down_counter_is_exported() {
-    let subscriber = init_subscriber(
+    let (subscriber, exporter) = init_subscriber(
         "pebcak_blah".to_string(),
         InstrumentKind::UpDownCounter,
         NumberKind::F64,
@@ -110,13 +115,15 @@ async fn f64_up_down_counter_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(counter.pebcak_blah = 99.123_f64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
-async fn u64_value_is_exported() {
-    let subscriber = init_subscriber(
+async fn u64_histogram_is_exported() {
+    let (subscriber, exporter) = init_subscriber(
         "abcdefg".to_string(),
-        InstrumentKind::ValueRecorder,
+        InstrumentKind::Histogram,
         NumberKind::U64,
         Number::from(9_u64),
     );
@@ -124,13 +131,15 @@ async fn u64_value_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(value.abcdefg = 9_u64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
-async fn i64_value_is_exported() {
-    let subscriber = init_subscriber(
+async fn i64_histogram_is_exported() {
+    let (subscriber, exporter) = init_subscriber(
         "abcdefg_auenatsou".to_string(),
-        InstrumentKind::ValueRecorder,
+        InstrumentKind::Histogram,
         NumberKind::I64,
         Number::from(-19_i64),
     );
@@ -138,13 +147,15 @@ async fn i64_value_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(value.abcdefg_auenatsou = -19_i64);
     });
+
+    exporter.export().unwrap();
 }
 
 #[tokio::test]
-async fn f64_value_is_exported() {
-    let subscriber = init_subscriber(
+async fn f64_histogram_is_exported() {
+    let (subscriber, exporter) = init_subscriber(
         "abcdefg_racecar".to_string(),
-        InstrumentKind::ValueRecorder,
+        InstrumentKind::Histogram,
         NumberKind::F64,
         Number::from(777.0012_f64),
     );
@@ -152,6 +163,8 @@ async fn f64_value_is_exported() {
     tracing::subscriber::with_default(subscriber, || {
         tracing::info!(value.abcdefg_racecar = 777.0012_f64);
     });
+
+    exporter.export().unwrap();
 }
 
 fn init_subscriber(
@@ -159,24 +172,25 @@ fn init_subscriber(
     expected_instrument_kind: InstrumentKind,
     expected_number_kind: NumberKind,
     expected_value: Number,
-) -> impl Subscriber + 'static {
+) -> (impl Subscriber + 'static, TestExporter) {
+    let controller = opentelemetry::sdk::metrics::controllers::basic(processors::factory(
+        selectors::simple::histogram(vec![-10.0, 100.0]),
+        aggregation::cumulative_temporality_selector(),
+    ))
+    .build();
+
     let exporter = TestExporter {
         expected_metric_name,
         expected_instrument_kind,
         expected_number_kind,
         expected_value,
+        controller: controller.clone(),
     };
 
-    let push_controller = opentelemetry::sdk::metrics::controllers::push(
-        Selector::Exact,
-        ExportKindSelector::Stateless,
+    (
+        tracing_subscriber::registry().with(MetricsLayer::new(controller)),
         exporter,
-        tokio::spawn,
-        delayed_interval,
     )
-    .build();
-
-    tracing_subscriber::registry().with(MetricsLayer::new(push_controller))
 }
 
 #[derive(Clone, Debug)]
@@ -185,100 +199,84 @@ struct TestExporter {
     expected_instrument_kind: InstrumentKind,
     expected_number_kind: NumberKind,
     expected_value: Number,
+    controller: BasicController,
 }
 
-#[async_trait]
-impl SpanExporter for TestExporter {
-    async fn export(
-        &mut self,
-        mut _batch: Vec<SpanData>,
-    ) -> opentelemetry::sdk::export::trace::ExportResult {
-        Ok(())
-    }
-}
+impl TestExporter {
+    fn export(&self) -> Result<(), MetricsError> {
+        self.controller.collect(&Context::current())?;
+        self.controller.try_for_each(&mut |library, reader| {
+            reader.try_for_each(self, &mut |record| {
+                assert_eq!(self.expected_metric_name, record.descriptor().name());
+                assert_eq!(
+                    self.expected_instrument_kind,
+                    *record.descriptor().instrument_kind()
+                );
+                assert_eq!(
+                    self.expected_number_kind,
+                    *record.descriptor().number_kind()
+                );
+                match self.expected_instrument_kind {
+                    InstrumentKind::Counter | InstrumentKind::UpDownCounter => {
+                        let number = record
+                            .aggregator()
+                            .unwrap()
+                            .as_any()
+                            .downcast_ref::<SumAggregator>()
+                            .unwrap()
+                            .sum()
+                            .unwrap();
 
-impl MetricsExporter for TestExporter {
-    fn export(&self, checkpoint_set: &mut dyn CheckpointSet) -> opentelemetry::metrics::Result<()> {
-        checkpoint_set.try_for_each(self, &mut |record| {
-            assert_eq!(self.expected_metric_name, record.descriptor().name());
-            assert_eq!(
-                self.expected_instrument_kind,
-                *record.descriptor().instrument_kind()
-            );
-            assert_eq!(
-                self.expected_number_kind,
-                *record.descriptor().number_kind()
-            );
-            let number = match self.expected_instrument_kind {
-                InstrumentKind::Counter | InstrumentKind::UpDownCounter => record
-                    .aggregator()
-                    .unwrap()
-                    .as_any()
-                    .downcast_ref::<SumAggregator>()
-                    .unwrap()
-                    .sum()
-                    .unwrap(),
-                InstrumentKind::ValueRecorder => record
-                    .aggregator()
-                    .unwrap()
-                    .as_any()
-                    .downcast_ref::<ArrayAggregator>()
-                    .unwrap()
-                    .points()
-                    .unwrap()[0]
-                    .clone(),
-                _ => panic!(
-                    "InstrumentKind {:?} not currently supported!",
-                    self.expected_instrument_kind
-                ),
-            };
-            assert_eq!(
-                Ordering::Equal,
-                number
-                    .partial_cmp(&NumberKind::U64, &self.expected_value)
-                    .unwrap()
-            );
+                        assert_eq!(
+                            Ordering::Equal,
+                            number
+                                .partial_cmp(&NumberKind::U64, &self.expected_value)
+                                .unwrap()
+                        );
+                    }
+                    InstrumentKind::Histogram => {
+                        let histogram = record
+                            .aggregator()
+                            .unwrap()
+                            .as_any()
+                            .downcast_ref::<HistogramAggregator>()
+                            .unwrap()
+                            .histogram()
+                            .unwrap();
 
-            // The following are the same regardless of the individual metric.
-            assert_eq!(
-                INSTRUMENTATION_LIBRARY_NAME,
-                record.descriptor().instrumentation_library().name
-            );
-            assert_eq!(
-                CARGO_PKG_VERSION,
-                record.descriptor().instrumentation_version().unwrap()
-            );
-            assert_eq!(
-                Value::String("unknown_service".into()),
-                record
-                    .resource()
-                    .get(Key::new("service.name".to_string()))
-                    .unwrap()
-            );
+                        let counts = histogram.counts();
+                        if dbg!(self.expected_value.to_i64(&self.expected_number_kind)) > 100 {
+                            assert_eq!(counts, &[0.0, 0.0, 1.0]);
+                        } else if self.expected_value.to_i64(&self.expected_number_kind) > 0 {
+                            assert_eq!(counts, &[0.0, 1.0, 0.0]);
+                        } else {
+                            assert_eq!(counts, &[1.0, 0.0, 0.0]);
+                        }
+                    }
+                    _ => panic!(
+                        "InstrumentKind {:?} not currently supported!",
+                        self.expected_instrument_kind
+                    ),
+                };
 
-            opentelemetry::metrics::Result::Ok(())
+                // The following are the same regardless of the individual metric.
+                assert_eq!(INSTRUMENTATION_LIBRARY_NAME, library.name);
+                assert_eq!(CARGO_PKG_VERSION, library.version.as_ref().unwrap());
+
+                Ok(())
+            })
         })
     }
 }
 
-impl ExportKindFor for TestExporter {
-    fn export_kind_for(&self, _descriptor: &Descriptor) -> ExportKind {
+impl TemporalitySelector for TestExporter {
+    fn temporality_for(
+        &self,
+        _descriptor: &Descriptor,
+        _kind: &aggregation::AggregationKind,
+    ) -> aggregation::Temporality {
         // I don't think the value here makes a difference since
         // we are just testing a single metric.
-        ExportKind::Cumulative
+        aggregation::Temporality::Cumulative
     }
-}
-
-// From opentelemetry::sdk::util::
-// For some reason I can't pull it in from the other crate, it gives
-//   could not find `util` in `sdk`
-/// Helper which wraps `tokio::time::interval` and makes it return a stream
-fn tokio_interval_stream(period: std::time::Duration) -> tokio_stream::wrappers::IntervalStream {
-    tokio_stream::wrappers::IntervalStream::new(tokio::time::interval(period))
-}
-
-// https://github.com/open-telemetry/opentelemetry-rust/blob/2585d109bf90d53d57c91e19c758dca8c36f5512/examples/basic-otlp/src/main.rs#L34-L37
-// Skip first immediate tick from tokio, not needed for async_std.
-fn delayed_interval(duration: Duration) -> impl Stream<Item = tokio::time::Instant> {
-    tokio_interval_stream(duration).skip(0)
 }
