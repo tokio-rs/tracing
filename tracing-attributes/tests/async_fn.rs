4848d7dc0976d2d29e002aa6545daf444920c2ee
Eashwar Ranganathan <eranganathan@lyft.com>
2022-10-06 11:01:43 -0700

    attributes: fix handling of inner attributes (#2307)
    ## Motivation
    
    When the `instrument` attribute is used on a function with inner
    attributes, the proc macro generates code above the attributes within
    the function block that causes compilation errors. These should be
    parsed out separately and handled.
    
    Fixes #2294
    
    ## Solution
    
    I updated `MaybeItemFn` and `MaybeItemFnRef` to so they hold both the
    outer and inner attributes for the instrumented function and updated the
    codegen to inlcude them in the appropriate locations.
    
    I couldn't preserve the existing implementation of
    `From<&'_ ItemFn> for MaybeItemFnRef<'_, Box<Block>>`, because it is
    now necessary to separate the inner and outer attributes of the
    `ItemFn` into two separate `Vec`s. That implementation was replaced
    with a `From<ItemFn> for MaybeItemFn`, which uses `Iterator::partition`
    to separate out the inner and outer attributes.
    
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-attributes/tests/async_fn.rs
+++ tracing-attributes/tests/async_fn.rs
@@ -32,6 +32,15 @@ async fn test_ret_impl_trait_err(n: i32) -> Result<impl Iterator<Item = i32>, &'
 #[instrument]
 async fn test_async_fn_empty() {}
 
+// Reproduces a compile error when an instrumented function body contains inner
+// attributes (https://github.com/tokio-rs/tracing/issues/2294).
+#[deny(unused_variables)]
+#[instrument]
+async fn repro_async_2294() {
+    #![allow(unused_variables)]
+    let i = 42;
+}
+
 // Reproduces https://github.com/tokio-rs/tracing/issues/1613
 #[instrument]
 // LOAD-BEARING `#[rustfmt::skip]`! This is necessary to reproduce the bug;
