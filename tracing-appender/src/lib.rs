19ab4fd23cb815880cd3f06897c98d6ea75964af
AnthonyMikh <anthony.mikh@yandex.ru>
2023-10-15 22:48:38 +0400

    appender: remove `Sync` bound from writer for `NonBlocking` (#2607)
    ## Motivation
    
    `NonBlocking` from `tracing-appender` wraps a writer and requires that
    writer to implement `Sync` (among other bounds). However it is not clear
    why this bound is necessary in first place: this writer is sent to a
    dedicated thread and never used directly concurrently.
    
    #1934 demonstrates that it is a real problem for some people. Also at my
    current work we hit this issue as well when a third-party writer did not
    implement `Sync`. Our workaround was to wrap that writer in our own type
    and manually implement `Send` and `Sync` for it. Needless to say that it
    is more a hack than a proper solution.
    
    ## Solution
    
    Remove `Sync` bound in relevant places. Yep, that simple. Probably
    having it in first place was just an oversight.
    
    Closes #1934

--- tracing-appender/src/lib.rs
+++ tracing-appender/src/lib.rs
@@ -190,7 +190,7 @@ pub(crate) mod sync;
 /// });
 /// # }
 /// ```
-pub fn non_blocking<T: Write + Send + Sync + 'static>(writer: T) -> (NonBlocking, WorkerGuard) {
+pub fn non_blocking<T: Write + Send + 'static>(writer: T) -> (NonBlocking, WorkerGuard) {
     NonBlocking::new(writer)
 }
 
