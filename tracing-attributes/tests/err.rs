c22b35913e37ddd5be9febfe6b6a405b3cbb542c
Hayden Stainsby <hds@caffeineconcepts.com>
2023-11-07 11:36:18 +0100

    test: add `tracing-test` crate for non-publishable test utils (#2466)
    ## Motivation
    
    There has been interest around publishing tracing-mock to crates.io
    for some time. In order to make this possible, it needs to be cleaned up.
    
    ## Solution
    
    There are some test utils in the `tracing-mock` crate which wouldn't
    make sense to publish. They provide test futures that are needed in
    multiple `tracing-*` crates, but would likely not be needed outside that
    context.
    
    This change moves that functionality into a separate `tracing-test`
    crate, which should never be published to crates.io.
    
    Refs: #539
    
    Co-authored-by: David Barsky <me@davidbarsky.com>

--- tracing-attributes/tests/err.rs
+++ tracing-attributes/tests/err.rs
@@ -4,6 +4,7 @@ use tracing_attributes::instrument;
 use tracing_mock::*;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::subscribe::CollectExt;
+use tracing_test::{block_on_future, PollN};
 
 use std::convert::TryFrom;
 use std::num::TryFromIntError;
