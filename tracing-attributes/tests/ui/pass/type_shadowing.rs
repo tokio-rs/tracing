1ae0e0079920d2f74333a558f80db5f65341bea8
Hayden Stainsby <hds@caffeineconcepts.com>
2025-06-13 11:08:40 +0200

    Update tracing-attributes/tests/ui/pass/type_shadowing.rs
    Documents test.

--- tracing-attributes/tests/ui/pass/type_shadowing.rs
+++ tracing-attributes/tests/ui/pass/type_shadowing.rs
@@ -1,3 +1,8 @@
+//! This program is a regression test for [#3306], where shadowing
+//! caused compilation failure in certain cases due to the original
+//! function body not getting its own scope.
+//!
+//! [#3306]: https://github.com/tokio-rs/tracing/issues/3306
 type Foo = ();
 enum Bar {
     Foo,
